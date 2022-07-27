6f4dc0150a87b2a2e4a010f13d2aee599df85199
Aswin Asok <aswinasokofficial@gmail.com>
2022-07-27 19:00:18 +0530

    Setting up first view

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-
-import './App.css';
+import "./App.css";
+import Home from "./Pages/Home/Home";
 
 function App() {
   return (
     <div className="App">
-      
+      <Home />
     </div>
   );
 }
---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import styles from "../Home/Home.module.css";
+import illustration from "./assets/illustration.png";
+
+const Home = () => {
+  return (
+    <div className={styles.main_container}>
+      <div className={styles.firstview_container}>
+        <div className={styles.first_view}>
+          <div className={styles.fv_texts}>
+            <p className={styles.fv_heading}>
+              Let's <span>break the Echo Chambers</span> Together.
+            </p>
+            <p className={styles.fv_tagline}>
+              µLearn is a synergic philosophy of education, with a culture of
+              mutual learning through micro groups of peers. µLearn is here to
+              assist you in breaking through the echo chambers and free you from
+              the shackles that have you grounded.
+            </p>
+          </div>
+          <button className={styles.primary}>Join Us</button>
+        </div>
+        <img className={styles.fv_image} src={illustration} alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
