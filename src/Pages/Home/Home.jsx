194245a24964c1c5f22692120acff55b9848bb11
Aswin Asok <aswinasokofficial@gmail.com>
2022-07-27 19:28:25 +0530

    Writing

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,25 +4,37 @@ import illustration from "./assets/illustration.png";
 
 const Home = () => {
   return (
-    <div className={styles.firstviewmain_container}>
-      <div className={styles.firstview_container}>
-        <div className={styles.first_view}>
-          <div className={styles.fv_texts}>
-            <p className={styles.fv_heading}>
-              Let's <span>break the Echo Chambers</span> Together.
-            </p>
-            <p className={styles.fv_tagline}>
-              µLearn is a synergic philosophy of education, with a culture of
-              mutual learning through micro groups of peers. µLearn is here to
-              assist you in breaking through the echo chambers and free you from
-              the shackles that have you grounded.
-            </p>
+    <>
+      <div className={styles.firstviewmain_container}>
+        <div className={styles.firstview_container}>
+          <div className={styles.first_view}>
+            <div className={styles.fv_texts}>
+              <p className={styles.fv_heading}>
+                Let's <span>break the Echo Chambers</span> Together.
+              </p>
+              <p className={styles.fv_tagline}>
+                µLearn is a synergic philosophy of education, with a culture of
+                mutual learning through micro groups of peers. µLearn is here to
+                assist you in breaking through the echo chambers and free you
+                from the shackles that have you grounded.
+              </p>
+            </div>
+            <button className={styles.primary}>Join Us</button>
           </div>
-          <button className={styles.primary}>Join Us</button>
+          <img className={styles.fv_image} src={illustration} alt="" />
         </div>
-        <img className={styles.fv_image} src={illustration} alt="" />
       </div>
-    </div>
+
+      <div className={styles.secondviewmain_container}>
+        <div className={styles.secondview_container}>
+          <div className={styles.second_view}>
+            <div className={styles.sv_mainheading}>
+              What is <span>Mulearn?</span>{" "}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
