10be43c1d1041e638822b70dc6320e6a76742363
Aswin Asok <aswinasokofficial@gmail.com>
2022-07-31 09:31:08 +0530

    Navbar changes

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,15 +1,120 @@
 import React from "react";
-import styles from "./Navbar.module.css";
+import "./Navbar.css";
+import Mulearn from "./assets/Mulearn.png";
 
 const Navbar = () => {
   return (
-    <div className={styles.navbar_container}>
-      <div className={styles.navbar}>
-        <p className={styles.link}>Career Labs</p>
-        <p className={styles.headertext}>µLearn</p>
-        <p className={styles.link}>Our Team</p>
+    <nav>
+      <div class="navbar navbar-expand-lg navbar-light ">
+        <div class="container">
+          <a class="navbar-brand" href="/">
+            <img className="mulearn-logo" src={Mulearn} alt="Mulearn" />
+          </a>
+
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavDropdown"
+            aria-controls="navbarNavDropdown"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <div
+            class="navbar-styles collapse navbar-collapse"
+            id="navbarNavDropdown"
+          >
+            <div class="navbar-dropdowns">
+              <ul class="navbar-nav">
+                <li class="nav-item dropdown">
+                  <a
+                    class="nav-link dropdown-toggle"
+                    href="#"
+                    target="_blank" rel="noopener noreferrer" 
+                    id="navbarDropdownMenuLink"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    Program
+                  </a>
+                  <ul
+                    class="dropdown-menu"
+                    aria-labelledby="navbarDropdownMenuLink"
+                  >
+                    <li>
+
+                      <a class="dropdown-item" target="_blank" rel="noopener noreferrer" href="https://yip.mulearn.org">
+                        YIP
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        class="dropdown-item"
+                        target="_blank" rel="noopener noreferrer" 
+                        href="https://mulearn.org/events"
+                      >
+                        Events
+                      </a>
+                    </li>
+                  </ul>
+                </li>
+
+                <li class="nav-item dropdown">
+                  <a
+                    class="nav-link"
+                    target="_blank" rel="noopener noreferrer" 
+                    href="https://atfg.gtechindia.org/participate/"
+                  >
+                    Participate
+                  </a>
+                </li>
+
+                <li class="nav-item dropdown">
+                  <a class="nav-link"  target="_blank" rel="noopener noreferrer"  href="https://mulearn.org/careers/">
+                    {" "}
+                    Careers{" "}
+                  </a>
+                </li>
+
+                <li class="nav-item dropdown">
+                  <a
+                    class="nav-link dropdown-toggle"
+                    href="#"
+                    target="_blank" rel="noopener noreferrer" 
+                    id="navbarDropdownMenuLink"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    About
+                  </a>
+                  <ul
+                    class="dropdown-menu"
+                    aria-labelledby="navbarDropdownMenuLink"
+                  >
+                    <li>
+                      <a class="dropdown-item"  target="_blank" rel="noopener noreferrer"  href="https://mulearn.org/team">
+                        Our Team
+                      </a>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
+
+            <div class="nav-barbtn">
+              <a href="https://discord.com/invite/Jt7sv3chZP">
+                <button class="join-discord">Join Discord Server</button>
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+    </nav>
   );
 };
 
