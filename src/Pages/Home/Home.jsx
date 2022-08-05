072b648479ebba1b5efdcf79a7d3e4967565e57a
Aswin Asok <aswinasokofficial@gmail.com>
2022-08-05 13:05:26 +0530

    Link and Logos Updation

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -11,14 +11,15 @@ import Navbar from "../../Components/Navbar/Navbar";
 
 import asap from "./assets/partners/asap.png";
 import blockchain from "./assets/partners/blockchain.png";
-import fof from "./assets/partners/fof.png";
 import foxlab from "./assets/partners/foxlab.png";
 import ieee from "./assets/partners/ieee.png";
 import kites from "./assets/partners/kites.png";
 import pygrammers from "./assets/partners/pygrammers.png";
 import xtrudar from "./assets/partners/xtrudar.png";
 import ksum from "./assets/partners/ksum.png";
-import kdisc from "./assets/partners/kdisc.png"
+import kdisc from "./assets/partners/kdisc.png";
+import ether_logo from "./assets/partners/ether_logo.png";
+import productpack from "./assets/partners/productpack.png";
 
 import CountUp from "react-countup";
 
@@ -58,16 +59,45 @@ const Home = () => {
             Community Partners at <span>µLearn.</span>{" "}
           </p>
           <div className={styles.supporters}>
-            <img src={asap} alt="" className={styles.supporter} />
-            <img src={blockchain} alt="" className={styles.supporter} />
-            <img src={fof} alt="" className={styles.supporter} />
-            <img src={foxlab} alt="" className={styles.supporter} />
-            <img src={ieee} alt="" className={styles.supporter} />
-            <img src={kites} alt="" className={styles.supporter} />
-            <img src={pygrammers} alt="" className={styles.supporter} />
-            <img src={xtrudar} alt="" className={styles.supporter} />
-            <img src={ksum} alt="" className={styles.supporter} />
-            <img src={kdisc} alt="" className={styles.supporter} />
+            <a href="https://kdisc.kerala.gov.in/" target="_blank" rel="noopener noreferrer">
+              <img src={kdisc} alt="" className={styles.supporter} />
+            </a>
+            <a href="https://iedc.startupmission.in/" target="_blank" rel="noopener noreferrer">
+              <img src={ksum} alt="" className={styles.supporter} />
+            </a>
+            <a href="https://asapkerala.gov.in/" target="_blank" rel="noopener noreferrer">
+              <img src={asap} alt="" className={styles.supporter} />
+            </a>
+            <a href="https://ieeekerala.org/" target="_blank" rel="noopener noreferrer">
+              <img src={ieee} alt="" className={styles.supporter} />
+            </a>
+            <a href="https://kba.ai/" target="_blank" rel="noopener noreferrer">
+              <img src={blockchain} alt="" className={styles.supporter} />
+            </a>
+            <a href="https://kitesfoundation.org/" target="_blank" rel="noopener noreferrer">
+              <img src={kites} alt="" className={styles.supporter} />
+            </a>
+            <a href="http://" target="_blank" rel="noopener noreferrer">
+              {" "}
+              <img src={foxlab} alt="" className={styles.supporter} />
+            </a>
+            <a href="https://etherindia.org/" target="_blank" rel="noopener noreferrer">
+              <img src={ether_logo} alt="ether" className={styles.supporter} />
+            </a>
+            <a href="http://pygrammers.org/" target="_blank" rel="noopener noreferrer">
+              <img src={pygrammers} alt="" className={styles.supporter} />
+            </a>
+            <a href="http://" target="_blank" rel="noopener noreferrer">
+              {" "}
+              <img src={xtrudar} alt="" className={styles.supporter} />
+            </a>
+            <a href="https://www.productpack.in/" target="_blank" rel="noopener noreferrer">
+              <img
+                src={productpack}
+                alt="product pack"
+                className={styles.supporter}
+              />
+            </a>
           </div>
         </div>
       </div>
@@ -142,7 +172,7 @@ const Home = () => {
                   <span>Let's Chill</span> and make the most out of it.
                 </div>
               </div>
-              
+
               <div className={styles.sv_point}>
                 <img src={interactive} alt="" className={styles.svp_image} />
                 <div className={styles.svp_heading}>Interact</div>
