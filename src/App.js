f151a14daf4828fe53e65cad508833589aec39a0
Aswin Asok <aswinasokofficial@gmail.com>
2022-09-23 22:07:49 +0530

    Mulearn Calendar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Home from "./Pages/Home/Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import News from "./Pages/News/News";
+import Calendar from "./Pages/Calendar/Calendar";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/news" element={<News />} />
+          <Route path="/calendar" element={<Calendar />} />
         </Routes>
       </Router>
     </div>
