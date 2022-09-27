348e0130bb8919215f7b42dd33c26c1382c5b146
Aswin Asok <aswinasokofficial@gmail.com>
2022-09-28 01:29:47 +0530

    Gallary Loaded with Data

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Home from "./Pages/Home/Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import News from "./Pages/News/News";
 import Calendar from "./Pages/Calendar/Calendar";
-import Gallary from "./Pages/Gallary/Gallary";
+import Gallery from "./Pages/Gallery/Gallery";
 import MuAnnouncements from "./Pages/Events/MuAnnouncements/MuAnnouncements";
 import ISR from "./Pages/Events/ISR/ISR";
 import Company from "./Pages/Company/Company";
@@ -24,7 +24,7 @@ function App() {
 					<Route path="/careers" element={<Career />} />
 					<Route path="/news" element={<News />} />
 					<Route path="/calendar" element={<Calendar />} />
-					<Route path="/gallary" element={<Gallary />} />
+					<Route path="/gallery" element={<Gallery />} />
 					<Route path="/announcements" element={<MuAnnouncements />} />
 					<Route path="/isr" element={<ISR />} />
 					<Route path="/company-partners" element={<Company />} />
--- src/Pages/Gallery/Gallery.jsx
+++ src/Pages/Gallery/Gallery.jsx
@@ -1,15 +1,17 @@
 import React, { useState } from "react";
 import Footer from "../../Components/Footer/Footer";
 import Navbar from "../../Components/Navbar/Navbar";
-import styles from "./Gallary.module.css";
+import styles from "./Gallery.module.css";
 
 import Box from "@mui/material/Box";
 import Tabs, { tabsClasses } from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
 
+import Events from "./data/Events";
+
 import fvimg from "./assets/fvimg.png";
 
-const Gallary = () => {
+const Gallery = () => {
   const [evalue, setEValue] = React.useState(0);
 
   const ehandleChange = (event, newValue) => {
@@ -22,7 +24,7 @@ const Gallary = () => {
     setNValue(newValue);
   };
 
-  const [program, setProgram] = useState(1);
+  const [program, setProgram] = useState(Events[0]);
 
   return (
     <>
@@ -32,10 +34,10 @@ const Gallary = () => {
           <div className={styles.first_view}>
             <div className={styles.fv_texts}>
               <p className={styles.fv_heading}>
-                Welcome to <span>µLearn </span> Gallary
+                Welcome to <span>µLearn </span> Gallery
               </p>
               <p className={styles.fv_tagline}>
-                Welcome, to µLearn Gallary, Listed below are the key pictures of
+                Welcome, to µLearn Gallery, Listed below are the key pictures of
                 various milestones accomplished by µLearn in the past one year.
                 Together, we learned and witness a lot of things and going back
                 and taking a look at those wonderful memeories again is a always
@@ -52,11 +54,11 @@ const Gallary = () => {
             <div className={styles.sv_texts}>
               <p className={styles.sv_heading}>
                 {" "}
-                <span>µLearn</span> Community Event Gallary.
+                <span>µLearn</span> Community Event Gallery.
               </p>
               <p className={styles.sv_tagline}>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Modi
-                maxime ea quisquam sequi.impedit hic!
+                Beautiful memories often need to document, so that you could
+                revisit them. These are some of the pictures from our events.
               </p>
             </div>
             <Box
@@ -78,81 +80,73 @@ const Gallary = () => {
                   },
                 }}
               >
-                <Tab
-                  onClick={() => {
-                    setProgram(1);
-                  }}
-                  label="Launch of MuLearn"
-                />
-                <Tab
-                  onClick={() => {
-                    setProgram(2);
-                  }}
-                  label="MuOnam"
-                />
-                <Tab label="CTF Award Ceremony at KTU" />
-                <Tab label="Session on skill gap at KTU Fresher's Induction Program" />
-                <Tab label="YIP events" />
-                <Tab label="MoU with IEEE Kerala Section" />
-                <Tab label="MoU with Ether India" />
+                {Events.map((event) => (
+                  <Tab
+                    onClick={() => {
+                      setProgram(event);
+                      console.log(event);
+                    }}
+                    label={event.Event_Name}
+                  />
+                ))}
               </Tabs>
             </Box>
             <div className={styles.cards_view_container}>
-              {program == 1 && (
+              {program && (
                 <div className={styles.cards_view}>
-                  <div className={styles.card_container}>
-                    <img
-                      src="https://drive.google.com/uc?export=view&id=1aa5OxeKsmHevobh6-eS1eHDc6Dcr1ab7"
-                      alt=""
-                      className={styles.card_img}
-                    />
-                  </div>
-                  <div className={styles.card_container}>
-                    <img
-                      src="https://drive.google.com/uc?export=view&id=1aF7AfZfzch3MOfkbdoDiPJ7MqQlc0fIE"
-                      alt=""
-                      className={styles.card_img}
-                    />
-                  </div>
-                  <div className={styles.card_container}>
-                    <img
-                      src="https://drive.google.com/uc?export=view&id=1-5eAtkEWG8ERms-QbYf595dgOyut3ugR"
-                      alt=""
-                      className={styles.card_img}
-                    />
-                  </div>
-                </div>
-              )}
-              {program == 2 && (
-                <div className={styles.cards_view}>
-                  <div className={styles.card_container}>
-                    <img
-                      src="https://drive.google.com/uc?export=view&id=1o81t_EcS8I_oQCEIPfiR52wGsr-SiJPu"
-                      alt=""
-                      className={styles.card_img}
-                    />
-                  </div>
-                  <div className={styles.card_container}>
-                    <img
-                      src="https://drive.google.com/uc?export=view&id=1KkOM77XPvk4tSaDbPQNK_tHp8Kl5eGbq"
-                      alt=""
-                      className={styles.card_img}
-                    />
-                  </div>
-                  <div className={styles.card_container}>
-                    <img
-                      src="https://drive.google.com/uc?export=view&id=1l-KGFUjC57e5zRqoaOL3iy5Eb3BvtXXp"
-                      alt=""
-                      className={styles.card_img}
-                    />
-                  </div>
+                  {program.Photo_1 && (
+                    <div className={styles.card_container}>
+                      <img
+                        src={program.Photo_1}
+                        alt=""
+                        className={styles.card_img}
+                      />
+                    </div>
+                  )}
+
+                  {program.Photo_2 && (
+                    <div className={styles.card_container}>
+                      <img
+                        src={program.Photo_2}
+                        alt=""
+                        className={styles.card_img}
+                      />
+                    </div>
+                  )}
+                  {program.Photo_3 && (
+                    <div className={styles.card_container}>
+                      <img
+                        src={program.Photo_3}
+                        alt=""
+                        className={styles.card_img}
+                      />
+                    </div>
+                  )}
+                  {program.Photo_4 && (
+                    <div className={styles.card_container}>
+                      <img
+                        src={program.Photo_4}
+                        alt=""
+                        className={styles.card_img}
+                      />
+                    </div>
+                  )}
+                  {program.Photo_5 && (
+                    <div className={styles.card_container}>
+                      <img
+                        src={program.Photo_5}
+                        alt=""
+                        className={styles.card_img}
+                      />
+                    </div>
+                  )}
                 </div>
               )}
             </div>
           </div>
         </div>
 
-        <div className={styles.second_view_container}>
+        {/* <div className={styles.second_view_container}>
           <div className={styles.second_view}>
             <div className={styles.sv_texts}>
               <p className={styles.sv_heading}>
@@ -262,11 +256,11 @@ const Gallary = () => {
               )}
             </div>
           </div>
-        </div>
+        </div> */}
       </div>
       <Footer />
     </>
   );
 };
 
-export default Gallary;
+export default Gallery;
