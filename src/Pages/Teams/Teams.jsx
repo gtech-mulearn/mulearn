cd1465581d57100836a2b808c841b1c65cf92e8e
VinuBalagopalAP <vinubalagopalap@gmail.com>
2022-09-28 15:59:41 +0530

    team

--- src/Pages/Teams/Teams.jsx
+++ src/Pages/Teams/Teams.jsx
@@ -35,24 +35,35 @@ const Teams = () => {
         <h2 className={styles.team_heading}>
           View The <span>Team Members</span>
         </h2>
+        <div></div>
         <div className={styles.team_list}>
           <span className={styles.team_card}>
             <h5>Executive Committee</h5>
-            
+            <p>
+              The Executive Members are those who serve as the community's
+              skeleton and propel it forward from the rear.
+            </p>
             <Link to="/team/execom">
               <button className={styles.join_button}>View</button>
             </Link>
           </span>
           <span className={styles.team_card}>
             <h5>YIP Team</h5>
-            
+            <p>
+              Here are the members of the crew and interns who helped to make
+              the Yip a big success.
+            </p>
             <Link to="/team/yip">
               <button className={styles.join_button}>View</button>
             </Link>
           </span>
           <span className={styles.team_card}>
             <h5>Community Team</h5>
-            
+            <p>
+              The Community Team was the one who brought the achievements at the
+              most; it links industry and academia and forges connections
+              between students, faculty, mentors, and others.
+            </p>
             <Link to="/team/community">
               <button className={styles.join_button}>View</button>
             </Link>
