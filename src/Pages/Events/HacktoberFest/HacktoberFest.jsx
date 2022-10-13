44079df48ade26399aa933ce38b0f91ccd0caf1a
Aswin Asok <aswinasokofficial@gmail.com>
2022-10-13 21:11:54 +0530

    Working on Hacktober Page

--- src/Pages/Events/HacktoberFest/HacktoberFest.jsx
+++ src/Pages/Events/HacktoberFest/HacktoberFest.jsx
@@ -19,8 +19,59 @@ const HacktoberFest = () => {
                 a website for a store or buiness online.
               </p>
               <button className={styles.primary_btn}>Contribute Now!</button>
+
+              <a
+                href="https://hacktoberfest.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <button className={styles.signup}>Join Hacktoberfest</button>
+              </a>
             </div>
-            <img src="/assets/events/hacktoberfest/fvimg.gif"/>
+            <img src="/assets/events/hacktoberfest/fvimg.gif" />
+          </div>
+        </div>
+
+        <div className={styles.second_view_container}>
+          <div className={styles.second_view}>
+            <div className={styles.sv_texts}>
+              <p className={styles.svheader}>About HacktoberFest</p>
+              <p className={styles.svtagline}>
+                Put simply, Hacktoberfest is Digital Ocean’s annual event that
+                seeks to encourage people to make open-source contributions
+                throughout the month of October.It wouldn’t be wrong to say that
+                the Hacktoberfest has played an integral role in building the
+                open-source community over the years.
+              </p>
+            </div>
+            <div className={styles.sv_points}>
+              <div className={styles.sv_point}>
+                <div className={styles.svp_heading}>New Season</div>
+                <div className={styles.svp_text}>
+                  This time around, Hacktoberfest isn’t just about code!For the
+                  first time in its history, Hacktoberfest has opened its doors
+                  to no-code or low-code contributions.
+                </div>
+              </div>
+              <div className={styles.sv_point}>
+                <div className={styles.svp_heading}>To Participate</div>
+                <div className={styles.svp_text}>
+                  To register authorize access to your GitHub or GitLab account,
+                  before 31st October.To successfully complete Hacktoberfest,
+                  one has to have 4 pull/merge requests accepted between October
+                  1 and October 31.
+                </div>
+              </div>
+
+              <div className={styles.sv_point}>
+                <div className={styles.svp_heading}>New Surprises</div>
+                <div className={styles.svp_text}>
+                  Stay tuned to our social media handles for some cool updates.
+                  We know you’re excited to get started on this journey. So are
+                  we!
+                </div>
+              </div>
+            </div>
           </div>
         </div>
 
@@ -29,9 +80,9 @@ const HacktoberFest = () => {
             <div className={styles.sv_texts}>
               <p className={styles.svheader}>How to Contribute?</p>
               <p className={styles.svtagline}>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui
-                totam blanditiis ipsam omnis laboriosam, nam sequi atque quod?
-                Provident, quis natus asperiores atque quibusdam quasi.
+                You can build a webpage for a local businees that you know, or
+                you frequently visit using simple HTML, CSS and JS. It could
+                even be your favourite chayakkada(Tea Shop).
               </p>
             </div>
             <div className={styles.sv_points}>
@@ -70,10 +121,8 @@ const HacktoberFest = () => {
             <div className={styles.tv_texts}>
               <p className={styles.tvheader}>Minimum Requirements</p>
               <p className={styles.tvtagline}>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam,
-                alias numquam nam nobis voluptatibus ratione, provident sequi
-                consectetur quos reprehenderit iusto esse nesciunt libero.
-                Suscipit.
+                While making the pull request to our repository make sure there
+                are no duplicates.
               </p>
             </div>
             <div className={styles.tvpoints}>
