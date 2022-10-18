97bb177f9f81d0708c1a60d081213b55e830851a
Basith P <basithpofficial@gmail.com>
2022-10-18 19:46:27 +0530

    Filter option added in team page

--- src/Pages/Teams/Teams.jsx
+++ src/Pages/Teams/Teams.jsx
@@ -1,4 +1,6 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
+
 import Footer from "../../Components/Footer/Footer";
 import Navbar from "../../Components/Navbar/Navbar";
 import styles from "./Teams.module.css";
@@ -13,6 +15,11 @@ import districtHeads from "./data/community/district.js";
 import techTeam from "./data/tech-contributors.js";
 
 const Teams = () => {
+  const [teamName, setTeamfilter] = useState("all");
+
+  const handleFilterChange = (e) => {
+    setTeamfilter(e.target.value);
+  };
   return (
     <>
       <Navbar />
@@ -41,120 +48,145 @@ const Teams = () => {
         </div>
       </div>
 
-      <section id={styles.execom} className={styles.team_group}>
-        <p className={styles.team_title}>Executive Committee</p>
-        <p className={styles.team_desc}>The Executive Members are those who serve as the community's skeleton and propel it forward from the rear.</p>
-        <div className={styles.members_list}>
-          {execom.map((member) => {
-            return (
-              <MentorCard
-                key={member.name}
-                name={member.name}
-                designation={member.designation}
-                image={member.image}
-                linkedIn={member.linkedin ? member.linkedin : ""}
-              />
-            );
-          })}
-        </div>
-      </section>
+      <div className={styles.select_wrapper}>
+        <select
+          className={styles.select}
+          value={teamName}
+          onChange={handleFilterChange}
+        >
+          <option value="all" selected>All</option>
+          <option value="execom">Execom</option>
+          <option value="yip">YIP Team</option>
+          <option value="community">Community Team</option>
+          <option value="tech">Tech Team</option>
+        </select>
+      </div>
 
-      <section className={styles.team_group}>
-        <p className={styles.team_title}>YIP Organization Team</p>
-        <p className={styles.team_desc}>Here are the members of the crew and interns who helped to make the Yip a big success.</p>
-        <div className={styles.members_list}>
-          {yipteam.map((member) => {
-            return (
-              <MentorCard
-                key={member.name}
-                name={member.name}
-                designation={member.designation}
-                image={member.image}
-                linkedIn={member.linkedin ? member.linkedin : ""}
-              />
-            );
-          })}
-        </div>
-      </section>
+      {
+        (teamName === "all" || teamName == "execom") && (
+          <section id={styles.execom} className={styles.team_group}>
+            <p className={styles.team_title}>Executive Committee</p>
+            <p className={styles.team_desc}>The Executive Members are those who serve as the community's skeleton and propel it forward from the rear.</p>
+            <div className={styles.members_list}>
+              {execom.map((member) => {
+                return (
+                  <MentorCard
+                    key={member.name}
+                    name={member.name}
+                    designation={member.designation}
+                    image={member.image}
+                    linkedIn={member.linkedin ? member.linkedin : ""}
+                  />
+                );
+              })}
+            </div>
+          </section>)
+      }
 
-      <section className={styles.team_group}>
-        <p className={styles.team_title}>Community Team</p>
-        <p className={styles.team_desc}>The Community Team was the one who brought the achievements at the most; it links industry and academia and forges connections between students, faculty, mentors, and others.</p>
-        <p className={styles.sub_team_title}>Core Team</p>
-        <div className={styles.members_list}>
-          {communitycoreTeam.map((member) => {
-            return (
-              <MentorCard
-                key={member.name}
-                name={member.name}
-                designation={member.designation}
-                image={member.image}
-                linkedIn={member.linkedin ? member.linkedin : ""}
-              />
-            );
-          })}
-        </div>
-        <p className={styles.sub_team_title}>Zonal Heads</p>
-        <div className={styles.members_list}>
-          {zonalHeads.map((member) => {
-            return (
-              <MentorCard
-                key={member.name}
-                name={member.name}
-                designation={member.designation}
-                image={member.image}
-                linkedIn={member.linkedin ? member.linkedin : ""}
-              />
-            );
-          })}
-        </div>
-        <p className={styles.sub_team_title}>District Heads</p>
-        <div className={styles.members_list}>
-          {districtHeads.map((member) => {
-            return (
-              <MentorCard
-                key={member.name}
-                name={member.name}
-                designation={member.designation}
-                image={member.image}
-                linkedIn={member.linkedin ? member.linkedin : ""}
-              />
-            );
-          })}
-        </div>
-        <p className={styles.sub_team_title}>Campus Ambassadors</p>
-        <div className={styles.members_list}>
-          {campusAmbassadors.map((member) => {
-            return (
-              <MentorCard
-                key={member.name}
-                name={member.name}
-                designation={member.designation}
-                image={member.image}
-                linkedIn={member.linkedin ? member.linkedin : ""}
-              />
-            );
-          })}
-        </div>
-      </section>
 
-      <section className={styles.team_group}>
-        <p className={styles.team_title}>Tech Team</p>
-        <p className={styles.team_desc}>Here are the members of the crew who contributed developing the website and bot, collected resources and gave suggestions about UX.</p>
-        <div className={styles.members_list}>
-          {techTeam.map((member) => {
-            return (
-              <MentorCard
-                key={member.name}
-                name={member.name}
-                image={member.image}
-                interest={member.team ? member.team : ""}
-                linkedIn={member.linkedin ? member.linkedin : ""}
-              />
-            );
-          })}
-        </div>
-      </section>
+
+      {(teamName === "all" || teamName == "yip") && (
+        <section className={styles.team_group}>
+          <p className={styles.team_title}>YIP Organization Team</p>
+          <p className={styles.team_desc}>Here are the members of the crew and interns who helped to make the YIP a big success.</p>
+          <div className={styles.members_list}>
+            {yipteam.map((member) => {
+              return (
+                <MentorCard
+                  key={member.name}
+                  name={member.name}
+                  designation={member.designation}
+                  image={member.image}
+                  linkedIn={member.linkedin ? member.linkedin : ""}
+                />
+              );
+            })}
+          </div>
+        </section>
+      )}
+
+      {(teamName === "all" || teamName == "community") && (
+        <section className={styles.team_group}>
+          <p className={styles.team_title}>Community Team</p>
+          <p className={styles.team_desc}>The Community Team was the one who brought the achievements at the most; it links industry and academia and forges connections between students, faculty, mentors, and others.</p>
+          <p className={styles.sub_team_title}>Core Team</p>
+          <div className={styles.members_list}>
+            {communitycoreTeam.map((member) => {
+              return (
+                <MentorCard
+                  key={member.name}
+                  name={member.name}
+                  designation={member.designation}
+                  image={member.image}
+                  linkedIn={member.linkedin ? member.linkedin : ""}
+                />
+              );
+            })}
+          </div>
+          <p className={styles.sub_team_title}>Zonal Heads</p>
+          <div className={styles.members_list}>
+            {zonalHeads.map((member) => {
+              return (
+                <MentorCard
+                  key={member.name}
+                  name={member.name}
+                  designation={member.designation}
+                  image={member.image}
+                  linkedIn={member.linkedin ? member.linkedin : ""}
+                />
+              );
+            })}
+          </div>
+          <p className={styles.sub_team_title}>District Heads</p>
+          <div className={styles.members_list}>
+            {districtHeads.map((member) => {
+              return (
+                <MentorCard
+                  key={member.name}
+                  name={member.name}
+                  designation={member.designation}
+                  image={member.image}
+                  linkedIn={member.linkedin ? member.linkedin : ""}
+                />
+              );
+            })}
+          </div>
+          <p className={styles.sub_team_title}>Campus Ambassadors</p>
+          <div className={styles.members_list}>
+            {campusAmbassadors.map((member) => {
+              return (
+                <MentorCard
+                  key={member.name}
+                  name={member.name}
+                  designation={member.designation}
+                  image={member.image}
+                  linkedIn={member.linkedin ? member.linkedin : ""}
+                />
+              );
+            })}
+          </div>
+        </section>
+      )}
+
+      {(teamName === "all" || teamName == "tech") && (
+        <section className={styles.team_group}>
+          <p className={styles.team_title}>Tech Team</p>
+          <p className={styles.team_desc}>Here are the members of the crew who contributed developing the website and bot, collected resources and gave suggestions about UX.</p>
+          <div className={styles.members_list}>
+            {techTeam.map((member) => {
+              return (
+                <MentorCard
+                  key={member.name}
+                  name={member.name}
+                  image={member.image}
+                  interest={member.team ? member.team : ""}
+                  linkedIn={member.linkedin ? member.linkedin : ""}
+                />
+              );
+            })}
+          </div>
+        </section>
+      )}
 
       <Footer />
     </>
