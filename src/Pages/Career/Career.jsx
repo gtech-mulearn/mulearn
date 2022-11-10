24696aab29b344c2fa532c1935ba9da6897f63cf
Aswin Asok <aswinasokofficial@gmail.com>
2022-11-10 17:24:53 +0530

    carrerspage-data-updations

--- src/Pages/Career/Career.jsx
+++ src/Pages/Career/Career.jsx
@@ -48,23 +48,53 @@ const Career = () => {
               applylink="https://airtable.com/shr5Wy2PVLHFVOgkA"
             />
             <CareersCard
-              title="Program Manager"
+              title="Operations Coordinator"
               duration="1 Year"
-              payment="INR 60K - 80K"
+              payment="INR 20K - 40K"
               criteria="Click View More Button"
               vacancy="01"
               lastdate="21st November"
-              jdlink="https://mulearn.notion.site/Program-Manager-6aba62240dd34826b37f44afb24d95f4"
+              jdlink="https://mulearn.notion.site/Operations-Coordinator-67535a48b8974ba1ba3e77151f4ae938"
               applylink="https://airtable.com/shr5Wy2PVLHFVOgkA"
             />
             <CareersCard
-              title="Program Manager"
+              title="Zonal Coordinator"
               duration="1 Year"
-              payment="INR 60K - 80K"
+              payment="INR 20K - 40K"
+              criteria="Click View More Button"
+              vacancy="03"
+              lastdate="21st November"
+              jdlink="https://mulearn.notion.site/Zonal-Coordinator-edbed256b1d04703b7a444b6c070bfbb"
+              applylink="https://airtable.com/shr5Wy2PVLHFVOgkA"
+            />
+            <CareersCard
+              title="District Coordinator"
+              duration="1 Year"
+              payment="INR 15K - 30K"
+              criteria="Click View More Button"
+              vacancy="14"
+              lastdate="21st November"
+              jdlink="https://mulearn.notion.site/District-Coordinator-4f10c75f117a49a3a9507a87f8ce2318"
+              applylink="https://airtable.com/shr5Wy2PVLHFVOgkA"
+            />
+            <CareersCard
+              title="Creative Coordinator"
+              duration="1 Year"
+              payment="INR 20K - 40K"
               criteria="Click View More Button"
               vacancy="01"
               lastdate="21st November"
-              jdlink="https://mulearn.notion.site/Program-Manager-6aba62240dd34826b37f44afb24d95f4"
+              jdlink="https://mulearn.notion.site/Creative-Coordinator-a5083823e02a48c0a023dc86828b74b9"
+              applylink="https://airtable.com/shr5Wy2PVLHFVOgkA"
+            />
+            <CareersCard
+              title="Zonal Coordinator"
+              duration="As per requirement"
+              payment="INR 500/Day+TA"
+              criteria="Click View More Button"
+              vacancy="140"
+              lastdate="21st November"
+              jdlink="https://mulearn.notion.site/Temporary-Intern-b413ce986e7b410d918a7b2aba3ae879"
               applylink="https://airtable.com/shr5Wy2PVLHFVOgkA"
             />
           </div>
