8c3f98833d981be658b97597cbd299b5ecf9110d
Bijo Mathew Jose <73273702+JackRiddlers@users.noreply.github.com>
2022-11-21 18:57:37 +0530

    new-navbar: notification-added

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,13 +1,16 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-
+import ReactTimeAgo from "react-time-ago";
+import TimeAgo from "javascript-time-ago";
+import en from "javascript-time-ago/locale/en";
 import NavLinks from "./NavLinks";
 const Navbar = () => {
   const [open, setOpen] = useState(false);
+  const [notificationOpen, setNotificationOpen] = useState(false);
   return (
     <nav className="bg-white ">
       <div className="flex items-center font-medium justify-around">
-        <div className={`z-30 p-5 mt-1 lg:w-auto w-full flex justify-between ${open ? "fixed top-1" : ""}`}>
+        <div className={`z-30 p-5 mt-1 bg-white lg:w-auto w-full flex justify-between ${open ? "fixed top-1" : ""}`}>
           <Link to="/">
             <img
               src="/assets/navbar/µLearn.webp"
@@ -15,8 +18,28 @@ const Navbar = () => {
               className="lg:cursor-pointer h-8"
             />
           </Link>
-          <div className="text-3xl lg:hidden" onClick={() => setOpen(!open)}>
-            <ion-icon name={`${open ? "close" : "menu"}`}></ion-icon>
+          <div className="flex justify-between">
+            {/* Notification */}
+            <div className={`group lg:hidden text-black  inline-block  pr-5 `}
+              onClick={() => {
+                setNotificationOpen(!notificationOpen);
+                setOpen(false);
+              }}>
+
+              <div className={`text-3xl ${notificationOpen ? ' text-orange-400' : 'text-black'}`}>
+                <ion-icon name="notifications-circle-outline"></ion-icon>
+              </div>
+              {/* Notification menu */}
+              <div className={` absolute w-[300px] bg-[#3c3e48]  
+              border-0 rounded-md overflow-y-scroll text-xs  left-5 mt-3  h-4/5 special ${notificationOpen ? 'block' : 'hidden'}`}>
+                <NotificationNav />
+              </div>
+
+            </div >
+            {/* Menu */}
+            <div className="text-3xl lg:hidden" onClick={() => { setOpen(!open); setNotificationOpen(false) }}>
+              <ion-icon name={`${open ? "close" : "menu"}`}></ion-icon>
+            </div>
           </div>
         </div>
         <ul className="lg:flex hidden uppercase items-center gap-8 font-[Poppins]">
@@ -28,6 +51,15 @@ const Navbar = () => {
           >
             Careers
           </Link>
+          <div className="group text-black py-7 px-3 inline-block hover:text-orange-500">
+            <div className="text-3xl ">
+              <ion-icon name="notifications-circle-outline"></ion-icon>
+            </div>
+            <div className={`hidden group-hover:lg:block hover:lg:block absolute w-[300px] bg-[#3c3e48]  
+              border-0 rounded-md overflow-y-scroll text-xs  mt-3  h-4/5 special ${notificationOpen ? 'block' : ''}`}>
+              <NotificationNav />
+            </div>
+          </div >
         </ul>
         <div className="lg:block hidden">
           <a
@@ -65,15 +97,54 @@ const Navbar = () => {
               rel="noopener noreferrer"
               className="py-7 px-28 "
             >
-              <button className="bg-orange-400 text-white  px-6 py-2 rounded-md ">
+              <button className="bg-orange-400 text-white  px-6 py-2 rounded-md " >
                 Join Discord
               </button>
             </a>
           </div>
         </ul>
       </div>
-    </nav>
+    </nav >
   );
 };
 
+const NotificationNav = () => {
+  TimeAgo.setDefaultLocale(en.locale);
+  TimeAgo.addLocale(en);
+  // ./data/notifications.json
+  let notifications = require("../../Pages/Notifications/data/notifications.json");
+  return (
+    <>
+      <div className=" px-3 py-4 text-white  bg-[#4c4f57] text-sm">This Week</div>
+      <div className=" px-3 py-2 text-white bg-[rgba(0,0,0,.2)] text-[9px] text-center capitalize select-none ">
+        <li>Mark all as seen</li>
+      </div>
+      <div className=" overflow-y-hidden">
+        {notifications && notifications.new.map((notification) => (
+          <div className=' px-5 py-2 capitalize text-white border-b '>
+            <a href={notification.url} >
+              <div className="py-2 ">{notification.title}</div>
+              <p className="py-2 text-justify disabled:decoration-current text-[10px] ">{notification.description}</p>
+              <div className="text-right text-[8px]"><ReactTimeAgo date={notification.date} locale="en-US" /></div>
+            </a>
+          </div>
+        ))}
+        {notifications && notifications.old.map((notification) => (
+          <div className=' px-5 py-2 capitalize text-white/50 border-b  '>
+            <a href={notification.url} >
+              <div className="py-2 ">{notification.title}</div>
+              <p className="py-2 text-justify disabled:decoration-current text-[10px] ">{notification.description}</p>
+              <div className="text-right text-[8px]"><ReactTimeAgo date={notification.date} locale="en-US" /></div>
+            </a>
+          </div>
+        ))}
+      </div>
+      <Link to='/notifications'>
+        <div className=" px-3 py-3 text-white text-sm bg-[rgba(0,0,0,.2)] text-[10px] text-center select-none ">
+          View All
+        </div>
+      </Link>
+    </>
+  )
+}
 export default Navbar;
