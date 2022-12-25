0af80fb0ce54e1af93f6347f258798c64ce5c706
Bijo Mathew Jose <73273702+JackRiddlers@users.noreply.github.com>
2022-12-25 16:20:44 +0530

    Testimonials : Video Synchronized

--- src/Components/Testimonials/Card.jsx
+++ src/Components/Testimonials/Card.jsx
@@ -3,25 +3,37 @@ import Styles from "./card.module.css";
 
 const Card = ({ person }) => {
     return (
-        <div className={`${Styles.main} `}>
-            <section className={Styles.section}>
-                <div className={Styles.profile}>
-                    <img
-                        src={person.image_link} alt={person.name}
-                    />
+        <>
+            {/* <div className={`${Styles.main} `}>
+                <section className={Styles.section}>
+                    <div className={Styles.profile}>
+                        <img
+                            src={person.image_link} alt={person.name}
+                        />
+                    </div>
+                    <div className={Styles.info}>
+                        <h5>{person.name}</h5>
+                        <span>{person.about}</span>
+                    </div>
+                </section>
+                <div className={`${Styles.quote}`}>
+                    <blockquote className="text">
+                        {person.testimony}
+                    </blockquote>
                 </div>
-                <div className={Styles.info}>
-                    <h5>{person.name}</h5>
-                    <span>{person.about}</span>
-                </div>
-            </section>
-            <div className={`${Styles.quote}`}>
-                <blockquote className="text">
-                    {person.testimony}
-                </blockquote>
+                <span className={`${person.testimony.length > 200 ? "block" : "hidden"} text-sm text-orange-500 mt-3 relative self-end pr-5`}>Read more...</span>
+            </div> */}
+            <div className={Styles.video}>
+                <iframe
+                    className={Styles.ytvideo}
+                    src={person.video}
+                    title="YouTube video player"
+                    frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowfullscreen
+                ></iframe>
             </div>
-            <span className={`${person.testimony.length > 200 ? "block" : "hidden"} text-sm text-orange-500 mt-3 relative self-end pr-5`}>Read more...</span>
-        </div>
+        </>
     );
 };
 export default Card
