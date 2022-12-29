2fe0a26e352226bd961266be0668f4338eb53192
Aswin Asok <aswinasokofficial@gmail.com>
2022-12-29 22:10:59 +0530

    develop:404-page

--- src/Pages/404/NotFound.jsx
+++ src/Pages/404/NotFound.jsx
@@ -1,22 +1,27 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
-import nfgif from './assets/notfound/nf.gif'
+import React from "react";
+import { Link } from "react-router-dom";
+import nfgif from "./assets/notfound/nf.gif";
 import styles from "./NotFound.module.css";
-
 
 const NotFound = () => {
   return (
     <div className={styles.notfoundpage}>
       <img src={nfgif} alt="" className={styles.nf_img} />
       <h1 className={styles.h1}>404 PAGE NOT FOUND</h1>
-      <p className={styles.p}>The page you are looking for might have been removed  had its name changed or is temporarily unavailable.</p>
+      <p className={styles.p}>
+        The page you are looking for might have been removed, had its name
+        changed or is temporarily unavailable.
+      </p>
       <Link to="/">
-        <button className="bg-orange-400 text-white  px-6 py-2 rounded-md" style={{marginTop:"20px"}} >
+        <button
+          className="bg-orange-400 text-white  px-6 py-2 rounded-md"
+          style={{ marginTop: "20px" }}
+        >
           Back To Home
         </button>
       </Link>
-    </div >
-  )
-}
+    </div>
+  );
+};
 
-export default NotFound 
+export default NotFound;
