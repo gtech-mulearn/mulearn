242ed699e2854778ced9223df502e40fa9c8aca5
Jovit-Mathew236 <jovitmathew236@gmail.com>
2022-12-29 13:28:31 +0530

    develop:404-not-fouund-page

--- src/Pages/404/NotFound.jsx
+++ src/Pages/404/NotFound.jsx
@@ -7,11 +7,11 @@ import styles from "./NotFound.module.css";
 const NotFound = () => {
   return (
     <div className={styles.notfoundpage}>
-      <img src={nfgif} alt="" className={styles.fv_img} />
-      <h1>404 PAGE NOT FOUND</h1>
-      <p>The page you are looking for might have been removed  had its name changed or is temporarily unavailable.</p>
+      <img src={nfgif} alt="" className={styles.nf_img} />
+      <h1 className={styles.h1}>404 PAGE NOT FOUND</h1>
+      <p className={styles.p}>The page you are looking for might have been removed  had its name changed or is temporarily unavailable.</p>
       <Link to="/">
-        <button className="bg-orange-400 text-white  px-6 py-2 rounded-md " >
+        <button className="bg-orange-400 text-white  px-6 py-2 rounded-md" style={{marginTop:"20px"}} >
           Back To Home
         </button>
       </Link>
