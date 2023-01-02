c51cb6ef2dbb18e80320982619667ab777ce4b6f
Aswin Asok <aswinasokofficial@gmail.com>
2023-01-02 23:02:33 +0530

    community-page-data-loaded

--- src/Pages/CommunityLanding/CommunityLanding.jsx
+++ src/Pages/CommunityLanding/CommunityLanding.jsx
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "./CommunityLanding.module.css";
 import Navbar from "../../Components/Navbar/Navbar";
 import Footer from "../../Components/Footer/Footer";
 
+import { useParams } from "react-router-dom";
+
 const CommunityLanding = () => {
+  let { id } = useParams();
+
+  const data = require("./data.json").filter(function (community) {
+    return community.id === id;
+  });
+
+  console.log(data);
+
   return (
     <>
       <Navbar />
@@ -11,7 +21,7 @@ const CommunityLanding = () => {
         <div className={styles.first_view_container}>
           <div className={styles.first_view}>
             <div className={styles.fv_texts}>
-              <p className={styles.fv_heading}>TinkerHub Foundation</p>
+              <p className={styles.fv_heading}>{data[0].community}</p>
               {/* <p className={styles.fv_tagline}>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae
                 pariatur repellat aperiam sed enim, voluptatem saepe explicabo
@@ -31,54 +41,38 @@ const CommunityLanding = () => {
               />
             </div>
           </div>
-          <div className={styles.description_view_container}>
-            <div className={styles.description_view}>
-              <p className={styles.section_header}>
-                About TinkerHub Foundation
-              </p>
-              <p className={styles.section_content}>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Dignissimos dicta error provident, deleniti autem consectetur,
-                dolores at suscipit obcaecati eaque possimus. Iste voluptatem
-                hic tenetur animi odit eaque unde mollitia debitis? Tempora
-                veritatis eligendi officia.
-              </p>
+          {data[0].description.length > 0 && (
+            <div className={styles.description_view_container}>
+              <div className={styles.description_view}>
+                <p className={styles.section_header}>
+                  About {data[0].community}
+                </p>
+                <p className={styles.section_contentpara}>{data[0].description}</p>
+              </div>
             </div>
-          </div>
-          <div className={styles.description_view_container}>
-            <div className={styles.description_view}>
-              <p className={styles.section_header}>Our Speicalities</p>
-              <p className={styles.section_content}>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Dignissimos dicta error provident, deleniti autem consectetur,
-                dolores at suscipit obcaecati eaque possimus. Iste voluptatem
-                hic tenetur animi odit eaque unde mollitia debitis? Tempora
-                veritatis eligendi officia.
-              </p>
+          )}
+          {data[0].specialities.length > 0 && (
+            <div className={styles.description_view_container}>
+              <div className={styles.description_view}>
+                <p className={styles.section_header}>Our Speicalities</p>
+                <p className={styles.section_contentpara}>{data[0].specialities}</p>
+              </div>
             </div>
-          </div>
+          )}
           <div className={styles.second_view_container}>
             <div className={styles.second_view}>
-              <div className={styles.section}>
-                <p className={styles.section_header}>Our Mission</p>
-                <p className={styles.section_content}>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Dignissimos dicta error provident, deleniti autem consectetur,
-                  dolores at suscipit obcaecati eaque possimus. Iste voluptatem
-                  hic tenetur animi odit eaque unde mollitia debitis? Tempora
-                  veritatis eligendi officia.
-                </p>
-              </div>
-              <div className={styles.section}>
-                <p className={styles.section_header}>Our Vision</p>
-                <p className={styles.section_content}>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Dignissimos dicta error provident, deleniti autem consectetur,
-                  dolores at suscipit obcaecati eaque possimus. Iste voluptatem
-                  hic tenetur animi odit eaque unde mollitia debitis? Tempora
-                  veritatis eligendi officia.
-                </p>
-              </div>
+              {data[0].mission.length > 0 && (
+                <div className={styles.section}>
+                  <p className={styles.section_header}>Our Mission</p>
+                  <p className={styles.section_content}>{data[0].mission}</p>
+                </div>
+              )}
+              {data[0].vision.length > 0 && (
+                <div className={styles.section}>
+                  <p className={styles.section_header}>Our Vision</p>
+                  <p className={styles.section_content}>{data[0].vision}</p>
+                </div>
+              )}
             </div>
           </div>
         </div>
