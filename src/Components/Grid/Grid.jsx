2c287f88ad580547e5111c807d01a0af98a10ea2
Basith P <basithpofficial@gmail.com>
2023-02-08 19:46:39 +0530

    develop: add read more functioality for grid item

--- src/Components/Grid/Grid.jsx
+++ src/Components/Grid/Grid.jsx
@@ -15,7 +15,7 @@ const Grid = ({ data, selectedCategory }) => {
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4  gap-8 p-4">
         {filteredData.map((item) => (
           <GridItem key={item["Sl .No"]} item={item} />
-        ))}
+        )).reverse()}
       </div>
     );
   };
