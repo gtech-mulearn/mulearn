0b57cd5fb5da1df729d8b40a3ffc582c3b8fa936
Aswin Asok <aswinasokofficial@gmail.com>
2023-02-23 19:32:35 +0530

    dev:careers-page

--- src/Pages/Career/Career.jsx
+++ src/Pages/Career/Career.jsx
@@ -1,92 +1,59 @@
-import React, { Fragment } from "react"
-// import CareersCard from "../../Components/CareersCard/CareersCard";
+import React, { useEffect, useState } from "react"
 import Footer from "../../Components/Footer/Footer"
 import Navbar from "../../Components/Navbar/Navbar"
 import styles from "./Career.module.css"
-import ClosedCareers from "../../Components/ClosedCareers/ClosedCareers"
-import CareersCard from "../../Components/CareersCard/CareersCard"
-
-import ClosedCarrersData from "./data/data"
 
 const Career = () => {
+  let companies = require("./data/companies.json")
   return (
-    <Fragment>
+    <>
       <Navbar />
-      <main>
-        <div className={styles.first__section}>
-          <div className={styles.fstexts}>
-            <p className={styles.fsheading}>
-              µLearn <br />
-              Career Labs.
+      <div className={styles.main_container}>
+        <div className={styles.first_view_container}>
+          <img
+            src="/assets/careers/fvimg.gif"
+            alt=""
+            className={styles.fv_image}
+          />
+          <div className={styles.fv_texts}>
+            <p className={styles.fv_heading}>
+              µLearn <span>Career Labs</span>
             </p>
-            <p className={styles.fstagline}>
+            <p className={styles.fv_tagline}>
               In search of a job opportunity / internship? µLearn Career Labs
               helps you connect with opportunities from the industry.
             </p>
-          </div>
-          <div className={styles.fsimage}>
-            <img
-              src="/assets/careers/fsimg.gif"
-              className={styles.fsillustration}
-              alt=""
-            />
-          </div>
-        </div>
-
-        <div className={styles.second_section}>
-          <p className={styles.ssheading}>
-            <span className={styles.ssheadingspan}>µLearn X Yip</span> Hiring
-            Call
-          </p>
-          <p className={styles.ssheadingtagline}>
-            We want people with integrity, who are fair, impartial, honest, and
-            truthful. We are looking for dynamic and adaptable individuals who
-            are not afraid to think creatively and to be proactive, flexible,
-            and responsive.
-          </p>
-          <div className={styles.opportunities}>
-            <CareersCard
-              title="District Coordinator"
-              duration="1 Year"
-              payment="INR 15K - 20K/Month"
-              criteria="Click View More"
-              vacancy="03"
-              location="Respective Districts"
-              lastdate="24th February"
-              jdlink="https://mulearn.org/jd"
-              applylink="https://mulearn.org/jd"
-            />
-          </div>
-          <p className={styles.ssheadingposttagline}>
-            If you think you embody these values, we would love to see some of
-            them! Apply for a full-time/part-time/paid/volunteer position with
-            us here.
-          </p>
-        </div>
-
-        <div className={styles.closedcareerscontainer}>
-          <div className={styles.second_section}>
-            <p className={styles.ssheading}>Previous Hiring Calls</p>
-            <div className={styles.opportunities}>
-              {ClosedCarrersData.map((data) => (
-                <ClosedCareers
-                  company={data.Company}
-                  title={data.Title}
-                  duration={data.Duration}
-                  payment={data.Stipend || data.Package}
-                  criteria={data.Qualifications && data.Qualifications[0]}
-                  date={data.date}
-                  roles={data.Roles}
-                  jdlink={data.poster}
-                  location={data.location}
-                />
-              ))}
+            <div className={styles.counts}>
+              <p className={styles.hiring_count}>
+                <span className={styles.count}>340+</span>
+                <span className={styles.count_text}>Hired Candidates</span>
+              </p>
+              <p className={styles.hiring_count}>
+                <span className={styles.count}>20+</span>
+                <span className={styles.count_text}>
+                  Companies & Organisations
+                </span>
+              </p>
+              <p className={styles.hiring_count}>
+                <span className={styles.count}>30+</span>
+                <span className={styles.count_text}>Hiring Calls</span>
+              </p>
             </div>
           </div>
         </div>
-      </main>
+      </div>
+      <div className={styles.image_container}>
+        {companies.map((company) => (
+          <img
+            key={company.name}
+            src={company.logo}
+            alt={company.name}
+            className={styles.company_image}
+          />
+        ))}
+      </div>
       <Footer />
-    </Fragment>
+    </>
   )
 }
 
