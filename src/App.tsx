3fc796f899a4d594b24837706fbf67170d83cf12
Aswin Asok <aswinasokofficial@gmail.com>
2023-02-27 16:24:31 +0530

    dev-server:deployment-cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { useState } from "react"
-import reactLogo from "./assets/react.svg"
 import "./App.css"
 import { Route, Routes } from "react-router-dom"
 import Validate from "./Pages/Portal/User/Validate/Validate"
