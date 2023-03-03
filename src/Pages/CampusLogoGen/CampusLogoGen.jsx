c60b9c3ab849c632d13f28a69b36742024ffb4ab
Basith P <basithpofficial@gmail.com>
2023-03-03 18:34:33 +0530

    develop: add option to select logo type

--- src/Pages/CampusLogoGen/CampusLogoGen.jsx
+++ src/Pages/CampusLogoGen/CampusLogoGen.jsx
@@ -3,24 +3,11 @@ import LogoBgColors from "./LogoBgColors.json";
 
 const CampusLogoGenerator = () => {
   const canvasRef = useRef(null);
-  const [logoBgColor, setLogoBgColor] = useState("#000000");
+  const [logoType, setLogoType] = useState("Profile Pic");
+  const [logoBgColor, setLogoBgColor] = useState("#f5365c");
   const [text, setText] = useState("");
 
-  function handleTextChange(event) {
-    setText(event.target.value);
-  }
-
-  function handleSubmit(event) {
-    event.preventDefault();
-    const canvas = canvasRef.current;
-    const context = canvas.getContext("2d");
-    context.fillStyle = logoBgColor;
-    // Change the color of the logo here
-    context.fillText(text, 50, 50);
-    // Add the text to the canvas here
-    const dataURL = canvas.toDataURL();
-    // Use the data URL to display or download the final image
-  }
+  const LogoTypes = ["Profile Pic", "Transparent Bg"];
 
   return (
     <div className="flex">
@@ -31,16 +18,29 @@ const CampusLogoGenerator = () => {
           height={window.innerHeight / 2}
           className="border-2 border-gray-300 m-auto"
           style={{ backgroundColor: logoBgColor }}
-        />
+        ></canvas>
       </div>
       <form
-        onSubmit={handleSubmit}
+        // onSubmit={}
         className="w-1/2 max-w-sm h-screen p-8 flex flex-col"
       >
+        <label for="default-input" class="block mb-3 text-sm font-medium">
+          Logo Type
+        </label>
+        <div className="flex gap-4">
+          {LogoTypes.map((type) => (
+            <div
+              className={`${ logoType === type ? "bg-gray-800 text-white" : "border border-gray-300 text-gray-900" } px-4 py-2 rounded-lg cursor-pointer`}
+              onClick={() => setLogoType(type)}
+            >
+              {type}
+            </div>
+          ))}
+        </div>
+
         <label for="default-input" class="block mt-6 mb-3 text-sm font-medium">
           Background Color
         </label>
-
         <div className="flex justify-between">
           {LogoBgColors.map((color) => (
             <div
@@ -48,10 +48,9 @@ const CampusLogoGenerator = () => {
               style={{ backgroundColor: color }}
               onClick={() => setLogoBgColor(color)}
             ></div>
-          ))
-            }
+          ))}
         </div>
-        
+
         <label for="default-input" class="block mt-6 mb-3 text-sm font-medium">
           Campus Code
         </label>
@@ -59,6 +58,9 @@ const CampusLogoGenerator = () => {
           type="text"
           id="campusCode"
           class="mb-5 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-muorange focus:border-muorange block w-full p-2.5"
+          placeholder="Enter Campus Code"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
         />
 
         <button
