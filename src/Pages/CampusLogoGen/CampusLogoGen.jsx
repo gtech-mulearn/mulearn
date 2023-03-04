fd10730f4bf0352172d6e0f5c273995ebe28b0b7
Basith P <basithpofficial@gmail.com>
2023-03-04 08:56:53 +0530

    develop: add campus code to image canvas

--- src/Pages/CampusLogoGen/CampusLogoGen.jsx
+++ src/Pages/CampusLogoGen/CampusLogoGen.jsx
@@ -1,48 +1,68 @@
 import React, { useRef, useState } from "react";
-import LogoBgColors from "./LogoBgColors.json";
 
 const CampusLogoGenerator = () => {
-  const canvasRef = useRef(null);
+  const [campusCode, setCampusCode] = useState("");
   const [logoType, setLogoType] = useState("Profile Pic");
+  const [logoColor, setLogoColor] = useState("#ffffff");
   const [logoBgColor, setLogoBgColor] = useState("#f5365c");
-  const [text, setText] = useState("");
+  const [fileType, setFileType] = useState("PNG");
 
-  const LogoTypes = ["Profile Pic", "Transparent Bg"];
+  const logoTypes = ["Profile Pic", "Transparent Bg"];
+  const logoColors = ["#ffffff", "#000000"];
+  const logoBgColors = ["#f5365c", "#172b4d", "#fb6340", "#12bbda", "#5e72e4"];
+  const fileTypes = ["PNG", "JPG", "SVG"];
 
   return (
     <div className="flex">
-      <div className="w-full h-screen p-8 border-r-2">
-        <canvas
-          ref={canvasRef}
-          width={window.innerHeight / 2}
-          height={window.innerHeight / 2}
-          className="border-2 border-gray-300 m-auto"
-          style={{ backgroundColor: logoBgColor }}
-        ></canvas>
+      <div className="flex justify-center align-middle w-full h-full p-8 border-r-2">
+        <div className="w-80 h-80 text-white flex align-middle justify-center" style={{ backgroundColor: logoBgColor, color: logoColor }}>
+          {campusCode}
+        </div>
       </div>
       <form
         // onSubmit={}
         className="w-1/2 max-w-sm h-screen p-8 flex flex-col"
       >
-        <label for="default-input" class="block mb-3 text-sm font-medium">
-          Logo Type
-        </label>
-        <div className="flex gap-4">
-          {LogoTypes.map((type) => (
+        <label class="block mb-3 text-sm font-medium">Campus Code</label>
+        <input
+          type="text"
+          id="campusCode"
+          class="mb-8 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-muorange focus:border-muorange block w-full p-2.5"
+          placeholder="Enter Campus Code"
+          value={campusCode}
+          onChange={(e) => setCampusCode(e.target.value)}
+        />
+
+        <label class="block mb-3 text-sm font-medium">Logo Type</label>
+        <div className="flex gap-4 mb-8">
+          {logoTypes.map((type) => (
             <div
-              className={`${ logoType === type ? "bg-gray-800 text-white" : "border border-gray-300 text-gray-900" } px-4 py-2 rounded-lg cursor-pointer`}
-              onClick={() => setLogoType(type)}
+              className={`${
+                fileType === type
+                  ? "bg-gray-800 text-white"
+                  : "border border-gray-300 text-gray-900"
+              } px-4 py-2 rounded-lg cursor-pointer`}
+              onClick={() => setFileType(type)}
             >
               {type}
             </div>
           ))}
         </div>
 
-        <label for="default-input" class="block mt-6 mb-3 text-sm font-medium">
-          Background Color
-        </label>
-        <div className="flex justify-between">
-          {LogoBgColors.map((color) => (
+        <label class="block mb-3 text-sm font-medium">Logo Color</label>
+        <div className="flex mb-8 gap-4">
+          {logoColors.map((color) => (
+            <div
+              className="w-10 h-10 rounded-lg border border-gray-400"
+              style={{ backgroundColor: color }}
+              onClick={() => setLogoColor(color)}
+            ></div>
+          ))}
+        </div>
+
+        <label class="block mb-3 text-sm font-medium">Background Color</label>
+        <div className="flex justify-between mb-8">
+          {logoBgColors.map((color) => (
             <div
               className="w-10 h-10 rounded-lg border-gray-300"
               style={{ backgroundColor: color }}
@@ -51,20 +71,23 @@ const CampusLogoGenerator = () => {
           ))}
         </div>
 
-        <label for="default-input" class="block mt-6 mb-3 text-sm font-medium">
-          Campus Code
-        </label>
-        <input
-          type="text"
-          id="campusCode"
-          class="mb-5 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-muorange focus:border-muorange block w-full p-2.5"
-          placeholder="Enter Campus Code"
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-        />
-
+        <label class="block mb-3 text-sm font-medium">File Type</label>
+        <div className="flex gap-4 mb-8">
+          {fileTypes.map((type) => (
+            <div
+              className={`${
+                logoType === type
+                  ? "bg-gray-800 text-white"
+                  : "border border-gray-300 text-gray-900"
+              } px-4 py-2 rounded-lg cursor-pointer`}
+              onClick={() => setLogoType(type)}
+            >
+              {type}
+            </div>
+          ))}
+        </div>
         <button
-          type="button self-end"
+          type="button self-end  mt-6"
           class="text-white bg-muorange focus:ring-4 font-medium rounded-lg text-sm px-5 py-4"
         >
           Download
