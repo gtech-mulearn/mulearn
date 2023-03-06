0526187118c9a1078373f74bb899b7d5372fa068
the3plet <soorajp2002@gmail.com>
2023-03-06 23:46:25 +0530

    Revert "Team Page Update"
    This reverts commit 2282ac85d9ee6c2bb07d2b1ea0a9cfb29cc7428e.

--- src/Pages/Teams/Teams.jsx
+++ src/Pages/Teams/Teams.jsx
@@ -10,19 +10,12 @@ import techTeam from "./data/tech-contributors.js";
 
 const Teams = () => {
   const [teamName, setTeamfilter] = useState("all");
-  const [selectedYear, setSelectedYear] = useState("2022");
-  const [selectedOption, setSelectedOption] = useState("");
-
-  const handleSelectChange = (event) => {
-    setSelectedOption(event.target.value);
-  };
 
   let execom = require("./teamdata/execom.json");
   let core = require("./teamdata/core.json");
   let zonal = require("./teamdata/zonal.json");
   let district = require("./teamdata/district.json");
   let ca = require("./teamdata/ca.json");
-  let yip2023 = require("./yearwise/yip2023.json");
 
   const handleFilterChange = (e) => {
     setTeamfilter(e.target.value);
@@ -58,11 +51,8 @@ const Teams = () => {
       <div className={styles.select_wrapper}>
         <select
           className={styles.select}
-          value={teamName + selectedOption}
-          onChange={(e) => {
-            handleSelectChange(e);
-            handleFilterChange(e);
-          }}
+          value={teamName}
+          onChange={handleFilterChange}
         >
           <option value="all" selected>
             All
@@ -74,10 +64,6 @@ const Teams = () => {
         </select>
       </div>
 
-      {/* ------------Yearwise Tab------------ */}
-
-      {/* -------------------------- */}
-
       {(teamName === "all" || teamName === "execom") && (
         <section id={styles.execom} className={styles.team_group}>
           <p className={styles.team_title}>Executive Committee</p>
@@ -85,51 +71,6 @@ const Teams = () => {
             The Executive Members are those who serve as the community's
             skeleton and propel it forward from the rear.
           </p>
-
-          {selectedOption === "execom" && (
-            <div
-              style={{
-                textAlign: "center",
-                padding: "0.5rem 0rem",
-                margin: "0rem 0.5rem",
-              }}
-              className={`${styles.submenu_wrapper} submenu submenu:hover`}
-            >
-              <button
-                style={{
-                  border: "2px solid white",
-                  padding: "0rem 0.3rem",
-                  marginRight: "0.1rem",
-                  borderRadius: "5px",
-                }}
-                className={
-                  selectedYear === "2022"
-                    ? `${styles.submenu} ${styles.active_submenu}`
-                    : styles.submenu
-                }
-                onClick={() => setSelectedYear("2022")}
-              >
-                2022
-              </button>
-              <button
-                style={{
-                  border: "2px solid white",
-                  padding: "0rem 0.3rem",
-                  marginLeft: "0.1rem",
-                  borderRadius: "5px",
-                }}
-                className={
-                  selectedYear === "2023"
-                    ? `${styles.submenu} ${styles.active_submenu}`
-                    : styles.submenu
-                }
-                onClick={() => setSelectedYear("2023")}
-              >
-                2023
-              </button>
-            </div>
-          )}
-
           <div className={styles.members_list}>
             {execom.map((member) => {
               return (
@@ -145,138 +86,27 @@ const Teams = () => {
         </section>
       )}
 
-      {(teamName === "all" || teamName === "yip") &&
-        selectedYear === "2022" && (
-          <section className={styles.team_group}>
-            <p className={styles.team_title}>YIP Organization Team</p>
-            <p className={styles.team_desc}>
-              Here are the members of the crew and interns who helped to make
-              the YIP a big success.
-            </p>
-            {selectedOption === "yip" && (
-            <div
-              style={{
-                textAlign: "center",
-                padding: "0.5rem 0rem",
-                margin: "0rem 0.5rem",
-              }}
-              className={`${styles.submenu_wrapper} submenu submenu:hover`}
-            >
-              <button
-                style={{
-                  border: "2px solid white",
-                  padding: "0rem 0.3rem",
-                  marginRight: "0.1rem",
-                  borderRadius: "5px",
-                }}
-                className={
-                  selectedYear === "2022"
-                    ? `${styles.submenu} ${styles.active_submenu}`
-                    : styles.submenu
-                }
-                onClick={() => setSelectedYear("2022")}
-              >
-                2022
-              </button>
-              <button
-                style={{
-                  border: "2px solid white",
-                  padding: "0rem 0.3rem",
-                  marginLeft: "0.1rem",
-                  borderRadius: "5px",
-                }}
-                className={
-                  selectedYear === "2023"
-                    ? `${styles.submenu} ${styles.active_submenu}`
-                    : styles.submenu
-                }
-                onClick={() => setSelectedYear("2023")}
-              >
-                2023
-              </button>
-            </div>
-          )}
-            <div className={styles.members_list}>
-              {yipteam.map((member) => {
-                return (
-                  <TeamCard
-                    name={member.name}
-                    designation={member.designation}
-                    image={`/assets/team/yip-team/${member.name}.webp`}
-                    linkedIn={member.linkedin ? member.linkedin : ""}
-                  />
-                );
-              })}
-            </div>
-          </section>
-        )}
-
-      {(teamName === "all" || teamName === "yip") &&
-        selectedYear === "2023" && (
-          <section id={styles.yip2023} className={styles.team_group}>
-            <p className={styles.team_title}>YIP Organization Team</p>
-            <p className={styles.team_desc}>
-              Here are the members of the crew and interns who helped to make
-              the YIP a big success.
-            </p>
-            {selectedOption === "yip" && (
-            <div
-              style={{
-                textAlign: "center",
-                padding: "0.5rem 0rem",
-                margin: "0rem 0.5rem",
-              }}
-              className={`${styles.submenu_wrapper} submenu submenu:hover`}
-            >
-              <button
-                style={{
-                  border: "2px solid white",
-                  padding: "0rem 0.3rem",
-                  marginRight: "0.1rem",
-                  borderRadius: "5px",
-                }}
-                className={
-                  selectedYear === "2022"
-                    ? `${styles.submenu} ${styles.active_submenu}`
-                    : styles.submenu
-                }
-                onClick={() => setSelectedYear("2022")}
-              >
-                2022
-              </button>
-              <button
-                style={{
-                  border: "2px solid white",
-                  padding: "0rem 0.3rem",
-                  marginLeft: "0.1rem",
-                  borderRadius: "5px",
-                }}
-                className={
-                  selectedYear === "2023"
-                    ? `${styles.submenu} ${styles.active_submenu}`
-                    : styles.submenu
-                }
-                onClick={() => setSelectedYear("2023")}
-              >
-                2023
-              </button>
-            </div>
-          )}
-            <div className={styles.members_list}>
-              {yip2023.map((member) => {
-                return (
-                  <TeamCard
-                    name={member.name}
-                    designation={member.designation}
-                    image={
-                      "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Profile_pic_%281_of_1%29.jpg/320px-Profile_pic_%281_of_1%29.jpg"
-                    }
-                  />
-                );
-              })}
-            </div>
-          </section>
-        )}
+      {(teamName === "all" || teamName === "yip") && (
+        <section className={styles.team_group}>
+          <p className={styles.team_title}>YIP Organization Team</p>
+          <p className={styles.team_desc}>
+            Here are the members of the crew and interns who helped to make the
+            YIP a big success.
+          </p>
+          <div className={styles.members_list}>
+            {yipteam.map((member) => {
+              return (
+                <TeamCard
+                  name={member.name}
+                  designation={member.designation}
+                  image={`/assets/team/yip-team/${member.name}.webp`}
+                  linkedIn={member.linkedin ? member.linkedin : ""}
+                />
+              );
+            })}
+          </div>
+        </section>
+      )}
 
       {(teamName === "all" || teamName === "community") && (
         <section className={styles.team_group}>
@@ -287,49 +117,6 @@ const Teams = () => {
             students, faculty, mentors, and others.
           </p>
           <p className={styles.sub_team_title}>Core Team</p>
-          {selectedOption === "community" && (
-            <div
-              style={{
-                textAlign: "center",
-                padding: "0.5rem 0rem",
-                margin: "0rem 0.5rem",
-              }}
-              className={`${styles.submenu_wrapper} submenu submenu:hover`}
-            >
-              <button
-                style={{
-                  border: "2px solid white",
-                  padding: "0rem 0.3rem",
-                  marginRight: "0.1rem",
-                  borderRadius: "5px",
-                }}
-                className={
-                  selectedYear === "2022"
-                    ? `${styles.submenu} ${styles.active_submenu}`
-                    : styles.submenu
-                }
-                onClick={() => setSelectedYear("2022")}
-              >
-                2022
-              </button>
-              <button
-                style={{
-                  border: "2px solid white",
-                  padding: "0rem 0.3rem",
-                  marginLeft: "0.1rem",
-                  borderRadius: "5px",
-                }}
-                className={
-                  selectedYear === "2023"
-                    ? `${styles.submenu} ${styles.active_submenu}`
-                    : styles.submenu
-                }
-                onClick={() => setSelectedYear("2023")}
-              >
-                2023
-              </button>
-            </div>
-          )}
           <div className={styles.members_list}>
             {core.map((member) => {
               return (
@@ -387,49 +174,6 @@ const Teams = () => {
             Here are the members of the crew who contributed to developing the
             website and bot, collected resources and gave suggestions about UX.
           </p>
-          {selectedOption === "tech" && (
-            <div
-              style={{
-                textAlign: "center",
-                padding: "0.5rem 0rem",
-                margin: "0rem 0.5rem",
-              }}
-              className={`${styles.submenu_wrapper} submenu submenu:hover`}
-            >
-              <button
-                style={{
-                  border: "2px solid white",
-                  padding: "0rem 0.3rem",
-                  marginRight: "0.1rem",
-                  borderRadius: "5px",
-                }}
-                className={
-                  selectedYear === "2022"
-                    ? `${styles.submenu} ${styles.active_submenu}`
-                    : styles.submenu
-                }
-                onClick={() => setSelectedYear("2022")}
-              >
-                2022
-              </button>
-              <button
-                style={{
-                  border: "2px solid white",
-                  padding: "0rem 0.3rem",
-                  marginLeft: "0.1rem",
-                  borderRadius: "5px",
-                }}
-                className={
-                  selectedYear === "2023"
-                    ? `${styles.submenu} ${styles.active_submenu}`
-                    : styles.submenu
-                }
-                onClick={() => setSelectedYear("2023")}
-              >
-                2023
-              </button>
-            </div>
-          )}
           <div className={styles.members_list}>
             {techTeam.map((member) => {
               return (
