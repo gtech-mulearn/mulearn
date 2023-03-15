9d5cad512aa353c760686eb58bdbe708c72b65ee
Jovit-Mathew236 <jovitmathew236@gmail.com>
2023-03-15 23:27:50 +0530

    bug-fix

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Route, Routes } from "react-router-dom";
 import Validate from "./Pages/Portal/User/Validate/Validate";
 import Onboarding from "./Pages/Portal/User/Onboarding/Onboarding";
 import Profile from "./Pages/Portal/User/Profile/Profile";
+import Success from "./Pages/Portal/User/Onboarding/Success";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -13,6 +14,7 @@ function App() {
     <Routes>
       <Route path="user/validate/:token" element={<Validate />} />
       <Route path="user/onboarding" element={<Onboarding />} />
+      <Route path="user/onboarding/success" element={<Success />} />
       <Route path="user/profile" element={<Profile />} />
     </Routes>
   );
