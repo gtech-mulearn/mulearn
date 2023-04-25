00a9389c2a74ea13e02d103a702be68c3ee4ad8b
Aswin Asok <aswinasokofficial@gmail.com>
2023-04-26 02:05:09 +0530

    link-updation

--- src/App.js
+++ src/App.js
@@ -1,57 +1,57 @@
-import axios from "axios"
-import Home from "./Pages/Home/Home"
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import News from "./Pages/Gallery/News"
-import Calendar from "./Pages/Calendar/Calendar"
-import Gallery from "./Pages/Gallery/Gallery"
-import MuAnnouncements from "./Pages/Events/MuAnnouncements/MuAnnouncements"
-import ISR from "./Pages/Events/ISR/ISR"
-import Company from "./Pages/Company/Company"
-import Teams from "./Pages/Teams/Teams"
-import CommunityPartner from "./Pages/CommPartners/CommPartners"
-import ArtOfTeaching from "./Pages/ArtOfTeaching/ArtOfTeaching"
-import Career from "./Pages/Career/Career"
-import LeaderBoard from "./Pages/LeaderBoard/leaderBoard"
-import WikiSyllabus from "./Pages/WikiSyllabus/WikiSyllabus"
-import HacktoberFest from "./Pages/Events/HacktoberFest/HacktoberFest"
-import Yip from "./Pages/YIP/Yip"
-import CampusChapters from "./Pages/CampusChapters/CampusChapters"
-import BlogLanding from "./Pages/CampusChapters/Blogs/BlogLanding"
-import BlogTemplate from "./Pages/CampusChapters/Blogs/BlogTemplate/BlogTemplate"
-import Notifications from "./Pages/Notifications/Notifications"
-import MonthlyLeaderboard from "./Pages/LeaderBoard/MonthlyLeaderboard/MonthlyLeaderboard"
-import MentorConnect from "./Pages/Events/MentorConnect/MentorConnect"
-import SaltMangoTree from "./Pages/Events/SaltMangoTree/SaltMangoTree"
-import OpenMic from "./Pages/Events/OpenMic/OpenMic"
-import BuildForTeam from "./Pages/Events/BuildForTeam/BuildForTeam"
-import EnablersOnboarding from "./Pages/EnablersOnboarding/EnablersOnboarding"
-import HackathonKarma from "./Pages/HackathonKarma/HackathonKarma"
-import NotFound from "./Pages/404/NotFound"
-import EventsHome from "./Pages/Events/EventsHome/EventsHome"
-import CommunityLanding from "./Pages/CommunityLanding/CommunityLanding"
-import CampusLogoGenerator from "./Pages/CampusLogoGen/CampusLogoGen"
-import TermsAndCondition from "./Pages/TermsAndCondition/TermsAndCondition"
-import PrivacyPolicy from "./Pages/PrivacyPolicy/PrivacyPolicy"
+import axios from "axios";
+import Home from "./Pages/Home/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import News from "./Pages/Gallery/News";
+import Calendar from "./Pages/Calendar/Calendar";
+import Gallery from "./Pages/Gallery/Gallery";
+import MuAnnouncements from "./Pages/Events/MuAnnouncements/MuAnnouncements";
+import ISR from "./Pages/Events/ISR/ISR";
+import Company from "./Pages/Company/Company";
+import Teams from "./Pages/Teams/Teams";
+import CommunityPartner from "./Pages/CommPartners/CommPartners";
+import ArtOfTeaching from "./Pages/ArtOfTeaching/ArtOfTeaching";
+import Career from "./Pages/Career/Career";
+import LeaderBoard from "./Pages/LeaderBoard/leaderBoard";
+import WikiSyllabus from "./Pages/WikiSyllabus/WikiSyllabus";
+import HacktoberFest from "./Pages/Events/HacktoberFest/HacktoberFest";
+import Yip from "./Pages/YIP/Yip";
+import CampusChapters from "./Pages/CampusChapters/CampusChapters";
+import BlogLanding from "./Pages/CampusChapters/Blogs/BlogLanding";
+import BlogTemplate from "./Pages/CampusChapters/Blogs/BlogTemplate/BlogTemplate";
+import Notifications from "./Pages/Notifications/Notifications";
+import MonthlyLeaderboard from "./Pages/LeaderBoard/MonthlyLeaderboard/MonthlyLeaderboard";
+import MentorConnect from "./Pages/Events/MentorConnect/MentorConnect";
+import SaltMangoTree from "./Pages/Events/SaltMangoTree/SaltMangoTree";
+import OpenMic from "./Pages/Events/OpenMic/OpenMic";
+import BuildForTeam from "./Pages/Events/BuildForTeam/BuildForTeam";
+import EnablersOnboarding from "./Pages/EnablersOnboarding/EnablersOnboarding";
+import HackathonKarma from "./Pages/HackathonKarma/HackathonKarma";
+import NotFound from "./Pages/404/NotFound";
+import EventsHome from "./Pages/Events/EventsHome/EventsHome";
+import CommunityLanding from "./Pages/CommunityLanding/CommunityLanding";
+import CampusLogoGenerator from "./Pages/CampusLogoGen/CampusLogoGen";
+import TermsAndCondition from "./Pages/TermsAndCondition/TermsAndCondition";
+import PrivacyPolicy from "./Pages/PrivacyPolicy/PrivacyPolicy";
 
 //import redirects from "./redirects.json"
-import Redirection from "./Components/Redirection/Redirection"
-import Layout from "./Layout"
-import { useEffect, useState } from "react"
+import Redirection from "./Components/Redirection/Redirection";
+import Layout from "./Layout";
+import { useEffect, useState } from "react";
 
 function App() {
-  const[redirects, setRedirects] = useState([]);
-  useEffect(()=>{
+  const [redirects, setRedirects] = useState([]);
+  useEffect(() => {
     axios
-    .get(
-      "https://opensheet.elk.sh/1ylhC8QHDxBLheCtYliSasQNWjuOkb_N-A_BRh8aMpe4/redirections"
-    )
-    .then((response) => {
-      setRedirects(response.data)
-    })
-    .catch((error) => {
-      console.log(error)
-    })
-  })
+      .get(
+        "https://opensheet.elk.sh/1ylhC8QHDxBLheCtYliSasQNWjuOkb_N-A_BRh8aMpe4/redirections"
+      )
+      .then((response) => {
+        setRedirects(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  });
   return (
     <div className="App">
       <Router>
@@ -64,6 +64,13 @@ function App() {
               element={<Redirection link={redirect.redirect} />}
             />
           ))}
+          {/* //Redirect /yip to https://yip.kerala.gov.in/yipapp/index.php/idea2022 */}
+          <Route
+            path="/yip"
+            element={
+              <Redirection link="https://yip.kerala.gov.in/" />
+            }
+          />
           <Route path="/careers" element={<Career />} />
           <Route path="*" element={<NotFound />} />
           <Route path="/termsandconditions" element={<TermsAndCondition />} />
@@ -87,7 +94,7 @@ function App() {
             <Route path="/community-partners" element={<CommunityPartner />} />
             <Route path="/artofteaching" element={<ArtOfTeaching />} />
             <Route path="/wikisyllabus" element={<WikiSyllabus />} />
-            <Route path="/yip" element={<Yip />} />
+            {/* <Route path="/yip" element={<Yip />} /> */}
             <Route path="/team">
               <Route path="" element={<Teams />} />
             </Route>
@@ -115,7 +122,7 @@ function App() {
         </Routes>
       </Router>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
