a8bdd78d4735d0b9df08cdda7171adf23ac6b0e6
Basith P <basithpofficial@gmail.com>
2023-05-05 14:24:36 +0530

    fix: showing 404 on redirect

--- src/Components/Redirection/Redirection.jsx
+++ src/Components/Redirection/Redirection.jsx
@@ -1,10 +1,15 @@
-import React, { useEffect } from "react"
+import React, { useEffect } from "react";
 
 const Redirection = ({ link }) => {
   useEffect(() => {
-    window.location.href = link
-  }, [link])
-  return <div>Redirecting....</div>
-}
+    window.location.href = link;
+  }, [link]);
+  return (
+    <div className="flex justify-center items-center flex-col gap-8 h-screen">
+      <div className="animate-spin rounded-full h-20 w-20 border-t-2 border-l-2 border-gray-900"></div>
+      <p className="text-3xl font-medium">Redirecting...</p>
+    </div>
+  );
+};
 
-export default Redirection
+export default Redirection;
