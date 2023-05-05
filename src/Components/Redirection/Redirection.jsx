68a594ca8564dd9ecca1b409bf1fd18ec1493127
Aswin Asok <aswinasokofficial@gmail.com>
2023-05-05 21:39:06 +0530

    old-production:spinner-size

--- src/Components/Redirection/Redirection.jsx
+++ src/Components/Redirection/Redirection.jsx
@@ -6,7 +6,7 @@ const Redirection = ({ link }) => {
   }, [link]);
   return (
     <div className="flex justify-center items-center flex-col gap-8 h-screen">
-      <div className="animate-spin rounded-full h-15 w-15 border-t-2 border-l-2 border-orange-400"></div>
+      <div className="animate-spin rounded-full h-20 w-20 border-t-2 border-l-2 border-orange-400"></div>
       <p className="text-2xl font-medium text-orange-400">Redirecting...</p>
     </div>
   );
