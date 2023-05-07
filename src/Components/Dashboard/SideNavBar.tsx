fce74418344a2e74e0636f30a50646961088f999
Jovit-Mathew236 <jovitmathew236@gmail.com>
2023-05-07 20:30:35 +0530

    added connect discord box

--- src/Components/Dashboard/SideNavBar.tsx
+++ src/Components/Dashboard/SideNavBar.tsx
@@ -1,19 +1,20 @@
 import React, { useState } from "react";
 import styles from "./SideNavBar.module.css";
 import MulearnBrand from "./assets/MulearnBrand";
-import companyLogo from "./assets/images/profile.png";
-import {
-  MdHome,
-  MdSupervisorAccount,
-  MdSpaceDashboard,
-  MdClass,
-  MdBackupTable,
-  MdArticle,
-  MdWorkHistory,
-  MdSettings,
-  MdNotifications,
-} from "react-icons/md";
+// import companyLogo from "./assets/images/profile.png";
+// import {
+//   MdHome,
+//   MdSupervisorAccount,
+//   MdSpaceDashboard,
+//   MdClass,
+//   MdBackupTable,
+//   MdArticle,
+//   MdWorkHistory,
+//   MdSettings,
+//   MdNotifications,
+// } from "react-icons/md";
 import MuButton from "../MuCompenents/MuButton";
+import MuButtonLight from "../MuCompenents/MuButtonLight";
 
 const SideNavBar = (props: { component: any }) => {
   const [marginTop, setMarginTop] = useState("0px");
@@ -22,6 +23,8 @@ const SideNavBar = (props: { component: any }) => {
   const [transform3, setTransform3] = useState("0deg");
   const [display, setDisplay] = useState("block");
   const [display2, setDisplay2] = useState("unset");
+  // const [position, setPosition] = useState("unset");
+  // const [opacity, setOpacity] = useState(null);
   return (
     <div className={styles.fullpage}>
       <div
@@ -33,13 +36,27 @@ const SideNavBar = (props: { component: any }) => {
             <MulearnBrand />
           </div>
           <div className={styles.side_nav_bar_items}>
-            <MuButton text="Home" icon={<MdHome />} />
-            <MuButton text="Team" icon={<MdSupervisorAccount />} />
-            <MuButton text="Management" icon={<MdSpaceDashboard />} />
-            <MuButton text="Projects" icon={<MdClass />} />
-            <MuButton text="Tasks" icon={<MdArticle />} />
-            <MuButton text="Activity" icon={<MdBackupTable />} />
-            <MuButton text="History" icon={<MdWorkHistory />} />
+            {/* <MuButton text="Home" icon={<i className="fi fi-sr-home"></i>} />
+            <MuButton text="Team" icon={<i className="fi fi-sr-address-book"></i>} />
+            <MuButton text="Management" icon={<i className="fi fi-sr-layout-fluid"></i>} />
+            <MuButton text="Projects" icon={<i className="fi fi-sr-book-arrow-right"></i>} />
+            <MuButton text="Tasks" icon={<i className="fi fi-sr-box"></i>} />
+            <MuButton text="Activity" icon={<i className="fi fi-sr-copy-alt"></i>} />
+            <MuButton text="History" icon={<i className="fi fi-sr-search-alt"></i>} /> */}
+            <MuButton
+              text="Profile"
+              icon={<i className="fi fi-sr-clipboard-user"></i>}
+              style={{}}
+            />
+            <MuButton
+              text="Connect Discord"
+              icon={<i className="fi fi-sr-data-transfer"></i>}
+              style={{ background: "#014BB2", color: "#fff" }}
+            />
+            <MuButtonLight
+              text="Logout"
+              icon={<i className="fi fi-sr-key"></i>}
+            />
           </div>
         </div>
       </div>
@@ -56,8 +73,13 @@ const SideNavBar = (props: { component: any }) => {
                   setTransform2(transform2 === "0deg" ? "45deg" : "0deg");
                   setTransform3(transform3 === "0deg" ? "135deg" : "0deg");
                   setDisplay(display === "block" ? "none" : "block");
+                  // setPosition(position === "unset" ? "fixed" : "unset");
+                  // setOpacity(opacity === 1 ? 0 : 1);
                   setDisplay2(display2 === "none" ? "block" : "none");
+                  // setTimeout(() => {
+                  // }, 1000);
                 }}
+                // style={{ position: `${position}` }}
               >
                 <p
                   style={{ transform: `rotate(${transform2})` }}
@@ -80,11 +102,11 @@ const SideNavBar = (props: { component: any }) => {
                 <MulearnBrand />
               </div>
               <div className={styles.menu}>
-                <MdSettings style={{ fontSize: "20px" }} />
-                <MdNotifications style={{ fontSize: "20px" }} />
+                {/* <MdSettings style={{ fontSize: "30px" }} />
+                <MdNotifications style={{ fontSize: "30px" }} />
                 <div className={styles.profile}>
                   <img src={companyLogo} alt="" />
-                </div>
+                </div> */}
               </div>
             </div>
             <hr />
--- src/Components/MuCompenents/MuButtonLight.tsx
+++ src/Components/MuCompenents/MuButtonLight.tsx
@@ -1,15 +1,14 @@
-import React, { ReactFragment } from "react";
+import React from "react";
 import styles from "./Mucompnents.module.css";
 import { ReactJSXElement } from "@emotion/react/types/jsx-namespace";
 
-
-const MuButton = (props:{text:string, icon:ReactJSXElement, style: React.CSSProperties}) => {
+const MuButtonLight = (props: { text: string; icon: ReactJSXElement }) => {
   return (
-    <div className={styles.btn} style={props.style}>
+    <div className={styles.btn_light}>
       <div className={styles.btn_icon}>{props.icon}</div>
       <p>{props.text}</p>
     </div>
   );
 };
 
-export default MuButton;
+export default MuButtonLight;
