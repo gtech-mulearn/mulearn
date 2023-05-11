c36f4c8ce619d664ad3cffdd0e04e77f6871787b
Basith P <basithpofficial@gmail.com>
2023-05-11 20:07:17 +0530

    add: MuSidbar component

---
+++ src/Components/MuCompenents/MuSidebar/MuSidebar.tsx
@@ -0,0 +1,47 @@
+import React, { ReactNode } from 'react';
+import MulearnBrand from '../../Dashboard/assets/MulearnBrand';
+
+interface SidebarProps {
+    children: ReactNode;
+    showMulearnBrand?: boolean;
+}
+
+const MuSidebar = ({ showMulearnBrand, children }: SidebarProps) => {
+    return (
+        <div
+            style={{
+                display: 'flex',
+                flexDirection: 'column',
+                backgroundColor: 'rgb(235, 235, 245)',
+                height: '100%',
+                width: '250px',
+                padding: '10px',
+            }}
+        >
+            {((showMulearnBrand ?? true) &&
+                <div
+                    style={{
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        height: '100px',
+                        width: '100%',
+                        marginBottom: '20px',
+                    }}
+                >
+                    <MulearnBrand />
+                </div>)}
+            <div
+                style={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    gap: '10px',
+                }}
+            >
+                {children}
+            </div>
+        </div>
+    );
+};
+
+export default MuSidebar;
