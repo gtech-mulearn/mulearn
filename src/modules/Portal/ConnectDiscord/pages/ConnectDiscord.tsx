5f722b385aef3ab5e7248db8edaf4a18e1d91b40
Aswin Asok <aswinasokofficial@gmail.com>
2023-05-15 14:20:13 +0530

    folder-structuring:Connect-Discord-Migrated

--- src/modules/Portal/ConnectDiscord/pages/ConnectDiscord.tsx
+++ src/modules/Portal/ConnectDiscord/pages/ConnectDiscord.tsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
 import styles from "./ConnectDiscord.module.css";
 import cdimage from "./assets/images/connectdiscordpng1.png";
-import { getInfo } from "./helpers/apis";
-import { useNavigate } from "react-router-dom";
+import { getInfo } from "../services/apis";
+
 import { useToast } from "@chakra-ui/react";
 import { MdContentCopy } from "react-icons/md";
 import { BsDiscord } from "react-icons/bs";
