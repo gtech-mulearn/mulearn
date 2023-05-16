8cadcd760f2c42e655ade1a54ff1ae330f1415a1
Basith P <basithpofficial@gmail.com>
2023-05-16 20:29:58 +0530

    add: temporary 404 page

---
+++ src/components/NotFound.tsx
@@ -0,0 +1,28 @@
+import { Link } from "react-router-dom"
+
+// TODO: Redesign
+
+const NotFound = () => {
+    return (
+        <div style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            height: "100vh"
+        }}>
+            <h1 style={{ fontSize: "10rem" }}>404</h1>
+            <p>Page not found</p>
+            <Link to="/" style={{
+                marginTop: "1rem",
+                backgroundColor: "#014BB2",
+                color: "white",
+                padding: "0.5rem 1rem",
+                borderRadius: "0.5rem"
+
+            }}>Go back to home</Link>
+        </div>
+    )
+}
+
+export default NotFound
