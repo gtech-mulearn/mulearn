605b0bab0dcbb84f0cb00471969fbab65895e13a
Aswin Asok <aswinasokofficial@gmail.com>
2023-05-18 02:18:41 +0530

    dev:TableTop-params-conditional

--- src/components/MuComponents/TableTop/TableTop.tsx
+++ src/components/MuComponents/TableTop/TableTop.tsx
@@ -1,35 +1,34 @@
 import { PerPage } from "./PerPage";
-import { SearchBar } from "./SearchBar"
+import { SearchBar } from "./SearchBar";
 import { SortButton } from "./SortButton";
 import styles from "./tableTop.module.css";
 
-
 type Props = {
-	onSearchText: (data: string) => void;
-	onSortText: (data: string) => void;
-	onPerPageNumber: (data: number) => void;
-}
+    onSearchText?: (data: string) => void;
+    onSortText?: (data: string) => void;
+    onPerPageNumber?: (data: number) => void;
+};
 
 const TableTop = (props: Props) => {
-	const handleData = (search: string) => {
-    props.onSearchText(search)
-  };
-	const handleSort = (sort: string) => {
-		const sortVar = sort
-    props.onSortText(sortVar)
-  };
-	const handlePerPage = (selectedValue: number) => {
-    props.onPerPageNumber(selectedValue)
-  };
-	return (
-		<div className={styles.container}>
-			<div className={styles.body}>
-				<SearchBar onSearch={handleData}/>
-				<PerPage onPerPage={handlePerPage}/>
-				<SortButton onSort={handleSort} />
-			</div>
-		</div>
-	)
-}
+    const handleData = (search: string) => {
+        props.onSearchText && props.onSearchText(search);
+    };
+    const handleSort = (sort: string) => {
+        const sortVar = sort;
+        props.onSortText && props.onSortText(sortVar);
+    };
+    const handlePerPage = (selectedValue: number) => {
+        props.onPerPageNumber && props.onPerPageNumber(selectedValue);
+    };
+    return (
+        <div className={styles.container}>
+            <div className={styles.body}>
+                <SearchBar onSearch={handleData} />
+                <PerPage onPerPage={handlePerPage} />
+                <SortButton onSort={handleSort} />
+            </div>
+        </div>
+    );
+};
 
-export default TableTop
+export default TableTop;
