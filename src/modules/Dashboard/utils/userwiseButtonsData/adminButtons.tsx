46dd014fb92332b3dd3fcd21df12c13406104545
Aswin Asok <aswinasokofficial@gmail.com>
2023-05-18 16:57:40 +0530

    dev:util-changes

---
+++ src/modules/Dashboard/utils/userwiseButtonsData/adminButtons.tsx
@@ -0,0 +1,24 @@
+const adminButtons = [
+    {
+        url: "profile",
+        title: "Profile",
+        icon: <i className="fi fi-sr-clipboard-user"></i>
+    },
+    {
+        url: "campus-details",
+        title: "Campus Details",
+        icon: <i className="fi fi-sr-book-arrow-right"></i>
+    },
+    {
+        url: "interest-groups",
+        title: "Interest Groups",
+        icon: <i className="fi fi-sr-layout-fluid"></i>
+    },
+    {
+        url: "manage-users",
+        title: "Manage Users",
+        icon: <i className="fi fi-sr-users"></i>
+    },
+];
+
+export default adminButtons;
