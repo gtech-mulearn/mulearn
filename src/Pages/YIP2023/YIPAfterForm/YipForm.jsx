6481459df3e5c036aefcb4789846f2ebe5ca17cf
Aswin Asok <aswinasokofficial@gmail.com>
2023-05-23 18:22:46 +0530

    old-production:content-updations

---
+++ src/Pages/YIP2023/YIPAfterForm/YipForm.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import img from "../assets/yipform.svg";
+import Navbar from "../../../Components/Navbar/Navbar";
+import Footer from "../../../Components/Footer/Footer";
+import styles from "./YipForm.module.css";
+
+const YipForm = () => {
+  return (
+    <div>
+      <Navbar />
+      <div className={styles.content}>
+        <h2>
+          Engage more student participation from your campuses for a chance to
+          grab <span>exciting awards</span> for best performing colleges.
+        </h2>
+        <img src={img} alt="Team Work" />
+        <br />
+        <a href="/" target="_blank" rel="noopener noreferrer">
+          <button>Check It Out Now!</button>
+        </a>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default YipForm;
