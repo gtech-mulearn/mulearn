841c47d7f295bad26b03868a30e9f476e35961b1
Jenin82 <jenin8282@gmail.com>
2023-05-29 17:20:23 +0530

    feat(): toast msg added to interest group delete

--- src/modules/Dashboard/modules/InterestGroup/InterestGroupDelete.tsx
+++ src/modules/Dashboard/modules/InterestGroup/InterestGroupDelete.tsx
@@ -2,18 +2,20 @@ import { useEffect, useState } from "react";
 import { deleteInterestGroups, getIGDetails } from "./apis";
 import { useNavigate, useParams } from "react-router-dom";
 import Form from "../../../../components/MuComponents/Form/Form";
+import { useToast } from "@chakra-ui/react";
 
 type Props = {};
 
 const InterestGroupDelete = (props: Props) => {
     const [input, setInput] = useState("");
 	const { id } = useParams();
+    const toast = useToast();
 	useEffect(() => {
         getIGDetails(id, setInput);
     }, []);
 	const navigate = useNavigate();
     const handleSubmit = () => {
-        deleteInterestGroups(id);
+        deleteInterestGroups(id, toast);
 		navigate("/interest-groups");
 		
     };
