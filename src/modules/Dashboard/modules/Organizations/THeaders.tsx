7f738644099789d8fe547efc99e27d68598a57d1
Jenin82 <jenin8282@gmail.com>
2023-06-03 06:52:46 +0530

    org company & community tab table-json done

--- src/modules/Dashboard/modules/Organizations/THeaders.tsx
+++ src/modules/Dashboard/modules/Organizations/THeaders.tsx
@@ -4,37 +4,21 @@ export const columnsCollege = [
     { column: "code", Label: "Code", isSortable: false },
     { column: "affiliation", Label: "Affiliation", isSortable: false },
     { column: "district", Label: "District", isSortable: false },
-    { column: "zone", Label: "Zone", isSortable: false }
+    { column: "zone", Label: "Zone", isSortable: false },
 ];
 
 export const columnsCompanies = [
-    "title",
-    "code",
-    "district",
-    "zone",
-    "state",
-    "country"
-];
-
-export const editableCompaniesColumnNames = [
-    "Title",
-    "Code",
-    "District",
-    "Zone",
-    "State",
-    "Country"
+	{ column: "title", Label: "Title", isSortable: false },
+    { column: "code", Label: "Code", isSortable: false },
+    { column: "district", Label: "District", isSortable: false },
+    { column: "zone", Label: "Zone", isSortable: false },
+    { column: "state", Label: "State", isSortable: false },
+    { column: "country", Label: "Country", isSortable: false },
 ];
 
 export const columnsCommunities = [
-    "title",
-    "code",
-    "state",
-    "zone"
-];
-
-export const editableCommunityColumnNames = [
-    "Title",
-    "Code",
-    "State",
-    "Zone"
+	{ column: "title", Label: "Title", isSortable: false },
+    { column: "code", Label: "Code", isSortable: false },
+    { column: "state", Label: "State", isSortable: false },
+    { column: "zone", Label: "Zone", isSortable: false },
 ];
