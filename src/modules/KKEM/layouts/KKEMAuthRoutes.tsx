5f2677a7ab741857006de9416921d756f0d069b7
Arjun S <arjun8107@gmail.com>
2023-06-26 20:08:08 +0530

    fix(KKEM): Added MulearnBrad and some styling

--- src/modules/KKEM/layouts/KKEMAuthRoutes.tsx
+++ src/modules/KKEM/layouts/KKEMAuthRoutes.tsx
@@ -1,17 +1,25 @@
 import { useSearchParams } from "react-router-dom";
 import KKEMAuth from "../modules/Auth";
 import Landing from "../modules/Landing";
-
+import MulearnBrand from "../../Dashboard/assets/MulearnBrand";
+import styles from "./KKEMAuthRoutes.module.css";
 export default function KKEMAuthRoutes() {
     const [searchParams] = useSearchParams();
     const dwms_id = searchParams.get("dwms_id");
-    const muid = searchParams.get("muid");
+    const muid = searchParams.get("mu_id");
 
-    if (muid) {
-        return <Landing />;
-    } else if (dwms_id) {
-        return <KKEMAuth dwmsId={dwms_id} />;
-    } else {
-        return <Landing />;
-    }
+    return (
+        <div className={styles.container}>
+            <MulearnBrand />
+            {(() => {
+                if (muid) {
+                    return <Landing />;
+                } else if (dwms_id) {
+                    return <KKEMAuth dwmsId={dwms_id} />;
+                } else {
+                    return <Landing />;
+                }
+            })()}
+        </div>
+    );
 }
