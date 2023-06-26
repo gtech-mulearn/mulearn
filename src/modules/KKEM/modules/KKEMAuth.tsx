94376989ac13d3b5c4b40b14c919daa0eb8e1755
Arjun S <arjun8107@gmail.com>
2023-06-26 21:38:05 +0530

    feat(KKEM): Some simple style updates

--- src/modules/KKEM/modules/KKEMAuth.tsx
+++ src/modules/KKEM/modules/KKEMAuth.tsx
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom";
 import { userAuthConfirm } from "../services/auth";
 import { useEffect, useState } from "react";
+import styles from "./KKEMAuth.module.css";
 export default function KKEMAuth() {
     const { token } = useParams<{ token: string }>();
     const [success, setSuccess] = useState(false);
@@ -12,5 +13,11 @@ export default function KKEMAuth() {
             setSuccess(true);
         });
     }, [token]);
-    return success ? <p>Success</p> : <p>Failure</p>;
+    return success ? (
+        <p className={styles.content}>
+            Success! Your DWMS ID is successfully connected to MuLearn!
+        </p>
+    ) : (
+        <p className={styles.content}>Failure! Check console</p>
+    );
 }
