f3814b1e0543a46f8aa2c659e8bb12c9209afbfb
Arjun S <arjun8107@gmail.com>
2023-06-29 20:54:05 +0530

    fix(kkem): Added responsive navbar

--- src/modules/KKEM/components/Navbar.tsx
+++ src/modules/KKEM/components/Navbar.tsx
@@ -1,8 +1,14 @@
+import { useCallback, useEffect, useState } from "react";
 import kkemlogo from "../assets/kkemlogo.png";
 import XSvg from "../assets/x.svg";
 import mulearnLogo from "../assets/µ.svg";
 import styles from "./Navbar.module.css";
+import { RiMenu4Line, RiCloseLine } from "react-icons/ri";
 export default function Navbar() {
+    const [open, setOpen] = useState(false);
+    const clickHandler = useCallback(() => {
+        setOpen(prev => !prev);
+    }, []);
     return (
         <nav className={styles.navbar}>
             <div className={styles.container}>
@@ -11,11 +17,18 @@ export default function Navbar() {
                     <img src={XSvg} alt="X" />
                     <img src={kkemlogo} alt="KKEM Logo" />
                 </div>
-                <div className={styles.links}>
+                <div
+                    className={[styles.links, open ? styles.active : ""].join(
+                        " "
+                    )}
+                >
                     <a href="#about">About µLearn</a>
                     <a href="#">About K-KEM</a>
                     <a href="#skillExpress">Kerala Skill Express?</a>
                 </div>
+            </div>
+            <div className={styles.mobile} onClick={clickHandler}>
+                {open ? <RiCloseLine /> : <RiMenu4Line />}
             </div>
         </nav>
     );
