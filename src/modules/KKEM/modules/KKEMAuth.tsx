b0cf03dc500f7d9d6026a0bcb87891e186c934f8
Arjun S <arjun8107@gmail.com>
2023-06-29 23:39:57 +0530

    feat(kkem): Added Email auth endpoint styles

--- src/modules/KKEM/modules/KKEMAuth.tsx
+++ src/modules/KKEM/modules/KKEMAuth.tsx
@@ -1,7 +1,13 @@
 import { useParams } from "react-router-dom";
 import { userAuthConfirm } from "../services/auth";
 import { useEffect, useState } from "react";
+import kkemlogo from "../assets/kkemlogo.png";
+import XSvg from "../assets/x.svg";
+import mulearnLogo from "../assets/µ.svg";
+import Astronaut from "../assets/astronaut.png";
+import navStyles from "../components/Navbar.module.css";
 import styles from "./KKEMAuth.module.css";
+import Footer from "../components/Footer";
 export default function KKEMAuth() {
     const { token } = useParams<{ token: string }>();
     const [success, setSuccess] = useState(false);
@@ -17,17 +23,48 @@ export default function KKEMAuth() {
             controller.abort();
         };
     }, [token]);
-    return success ? <Success /> : <Failure />;
+    return (
+        <main>
+            <nav className={navStyles.navbar}>
+                <div className={navStyles.container}>
+                    <div className={navStyles.logos}>
+                        <img src={mulearnLogo} alt="Mulearn Logo" />
+                        <img src={XSvg} alt="X" />
+                        <img src={kkemlogo} alt="KKEM Logo" />
+                    </div>
+                </div>
+            </nav>
+            {success ? <Success /> : <Failure />}
+            <Footer />
+        </main>
+    );
 }
 
 function Success() {
     return (
-        <p className={styles.content}>
-            Success! Your DWMS ID is successfully connected to MuLearn!
-        </p>
+        <section className={styles.section}>
+            <div className={styles.content}>
+                <h1>Success!</h1>
+                <p>
+                    You have successfully authenticated with µLearn. You can now
+                    close this tab.
+                </p>
+            </div>
+            <img className={styles.image} src={Astronaut} alt="Astronaut" />
+        </section>
     );
 }
 
 function Failure() {
-    return <p className={styles.content}>Failure! Check console</p>;
+    return (
+        <section className={styles.section}>
+            <div className={styles.content}>
+                <h1 className={styles.failure}>Failure!</h1>
+                <p>
+                    You have failed to authenticate with µLearn. Please try
+                    again.
+                </p>
+            </div>
+        </section>
+    );
 }
