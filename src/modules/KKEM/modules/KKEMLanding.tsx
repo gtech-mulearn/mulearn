eee16869e6142304ebc0614c6bae86c09cab3813
Arjun S <arjun8107@gmail.com>
2023-06-29 17:51:07 +0530

    feat(kkem): Added mulearn about section and added fix for styling on larger screens

--- src/modules/KKEM/modules/KKEMLanding.tsx
+++ src/modules/KKEM/modules/KKEMLanding.tsx
@@ -3,6 +3,7 @@ import Navbar from "../components/Navbar";
 import SkillExpress from "../components/SkillExpress";
 import { useSearchParams } from "react-router-dom";
 import styles from "./KKEmLanding.module.css";
+import MulearnAbout from "../components/MulearnAbout";
 /**
  * Landing page for KKEM
  */
@@ -10,23 +11,26 @@ export default function Landing() {
     const [searchParams] = useSearchParams();
     const dwms_id = searchParams.get("dwms_id");
     return (
-        <>
+        <main className={styles.main}>
             <Navbar />
             {dwms_id && (
                 <section id="muId" className={styles.muidSection}>
-                    <KKEMAuth dwmsId={dwms_id} />
-                    <p className={styles.muidSectionText}>
-                        To get started, please enter your{" "}
-                        <strong>µLearn ID</strong>. If you don't have a µLearn
-                        ID yet, click the button below to visit the{" "}
-                        <strong>µLearn website and create one</strong>.
-                    </p>
-                    <a href="#" className={styles.muidLink}>
-                        Still without a Mu-Id? Grab one now
-                    </a>
+                    <div>
+                        <KKEMAuth dwmsId={dwms_id} />
+                        <p className={styles.muidSectionText}>
+                            To get started, please enter your{" "}
+                            <strong>µLearn ID</strong>. If you don't have a
+                            µLearn ID yet, click the button below to visit the{" "}
+                            <strong>µLearn website and create one</strong>.
+                        </p>
+                        <a href="#" className={styles.muidLink}>
+                            Still without a Mu-Id? Grab one now
+                        </a>
+                    </div>
                 </section>
             )}
             <SkillExpress />
-        </>
+            <MulearnAbout />
+        </main>
     );
 }
