80dda1239ee2fa79487644fce51b2c13897e764e
Amal C P <acp070146@gmail.com>
2023-07-06 21:16:54 +0530

    verify box ui change

--- src/components/MuComponents/Modal/Modal.tsx
+++ src/components/MuComponents/Modal/Modal.tsx
@@ -17,6 +17,23 @@ const Modal: FC<ModalProps> = ({ setIsOpen, id, heading, content, click }) => {
             <div className={styles.centered}>
                 <div className={styles.modal}>
                     <div className={styles.modalHeader}>
+                        <div className={styles.checkbtn}>
+                            <svg
+                                width="20"
+                                height="20"
+                                viewBox="0 0 24 24"
+                                fill="none"
+                                xmlns="http://www.w3.org/2000/svg"
+                            >
+                                <path
+                                    d="M7.5 12L10.5 15L16.5 9M22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12Z"
+                                    stroke="#039855"
+                                    stroke-width="2"
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                />
+                            </svg>
+                        </div>
                         <h5 className={styles.heading}>{heading}</h5>
                     </div>
                     <button
