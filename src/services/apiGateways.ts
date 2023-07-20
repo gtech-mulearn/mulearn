d699e749d2e5d8d20b7e00f7904eea6ec9ab9c5e
Jovit-Mathew236 <jovitmathew236@gmail.com>
2023-07-21 00:36:40 +0530

    [FIX] fixed reload issue when access token expire

--- src/services/apiGateways.ts
+++ src/services/apiGateways.ts
@@ -1,6 +1,6 @@
-import axios, { AxiosRequestConfig } from "axios";
+import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
 import { authRoutes } from "./urls";
-import { createStandaloneToast } from "@chakra-ui/react";
+import { createStandaloneToast, UseToastOptions } from "@chakra-ui/react";
 
 const { toast } = createStandaloneToast();
 
@@ -12,12 +12,12 @@ export const publicGateway = axios.create({
 });
 
 // <--- Comment below code before PR, this is for backend testing
-// export const publicGatewayAuth = axios.create({
-//     baseURL: import.meta.env.VITE_BACKEND_URL_AUTH as string,
-//     headers: {
-//         "Content-Type": "application/json"
-//     }
-// });
+export const publicGatewayAuth = axios.create({
+    baseURL: import.meta.env.VITE_BACKEND_URL_AUTH as string,
+    headers: {
+        "Content-Type": "application/json"
+    }
+});
 // --->
 
 export const privateGateway = axios.create({
@@ -30,10 +30,9 @@ export const privateGateway = axios.create({
 // Add a request interceptor
 privateGateway.interceptors.request.use(
     function (config) {
-        if (localStorage.getItem("accessToken") !== null) {
-            config.headers["Authorization"] = `Bearer ${localStorage.getItem(
-                "accessToken"
-            )}`;
+        const accessToken = localStorage.getItem("accessToken");
+        if (accessToken) {
+            config.headers["Authorization"] = `Bearer ${accessToken}`;
         }
 
         return config;
@@ -47,62 +46,59 @@ privateGateway.interceptors.request.use(
 // Add a response interceptor
 privateGateway.interceptors.response.use(
     function (response) {
-        // console.log(response);
         return response;
     },
-    function (error) {
-        //TODO: if error occurs and status isn't 1000 nothing will happened
-        
-
-        if (error.response.data.statusCode === 1000) {
+    async function (error) {
+        // TODO: if error occurs and status isn't 1000 nothing will happen
+        const accessToken = localStorage.getItem("accessToken");
+        if (error.response?.data?.statusCode === 1000) {
             // publicGatewayAuth
-            publicGateway
-                .post(authRoutes.getAccessToken, {
-                    refreshToken: localStorage.getItem("refreshToken")
-                })
-                .then(response => {
-                    localStorage.setItem(
-                        "accessToken",
-                        response.data.response.accessToken
-                    );
+            try {
+                const response = await publicGatewayAuth.post(
+                    authRoutes.getAccessToken,
+                    {
+                        refreshToken: accessToken
+                    }
+                );
+                localStorage.setItem(
+                    "accessToken",
+                    response.data.response.accessToken
+                );
 
-                    //retry the original request
-                    const config = error.config;
-                    config.headers[
-                        "Authorization"
-                    ] = `Bearer ${localStorage.getItem("accessToken")}`;
-                    return new Promise((resolve, reject) => {
-                        privateGateway
-                            .request(config)
-                            .then(response => {
-                                resolve(response);
-                            })
-                            .catch(error => {
-                                reject(error);
-                            });
-                    });
-                })
-                .catch(error => {
-                    console.log(error);
-                    toast.closeAll();
-                    toast({
-                        title: "Your session has expired.",
-                        description: "Please login again.",
-                        status: "error",
-                        duration: 5000,
-                        isClosable: true
-                    });
-
-                    //wait for 3 seconds
-
-                    setTimeout(() => {
-                        localStorage.clear();
-                        window.location.href = "/login";
-                    }, 3000);
+                // Retry the original request
+                const config = error.config;
+                config.headers["Authorization"] = `Bearer ${accessToken}`;
+                return await new Promise((resolve, reject) => {
+                    privateGateway
+                        .request(config)
+                        .then(response_1 => {
+                            resolve(response_1);
+                        })
+                        .catch(error_1 => {
+                            reject(error_1);
+                        });
                 });
+            } catch (error_2) {
+                console.log(error_2);
+                toast.closeAll();
+                toast({
+                    title: "Your session has expired.",
+                    description: "Please login again.",
+                    status: "error",
+                    duration: 5000,
+                    isClosable: true
+                });
+
+                // Wait for 3 seconds
+                setTimeout(() => {
+                    localStorage.clear();
+                    window.location.href = "/login";
+                }, 3000);
+                return await Promise.reject(error_2);
+            }
         }
 
-        // Any status codes that falls outside the range of 2xx cause this function to trigger
+        // Any status codes that fall outside the range of 2xx cause this function to trigger
         // Do something with response error
         return Promise.reject(error);
     }
