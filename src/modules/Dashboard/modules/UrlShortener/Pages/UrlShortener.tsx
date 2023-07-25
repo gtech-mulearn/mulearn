f5229a77a4eff57aa63ed18b067e98dae4d0279d
Jovit-Mathew236 <jovitmathew236@gmail.com>
2023-07-25 16:08:17 +0530

    [FEAT] added formik to urlshortner page

--- src/modules/Dashboard/modules/UrlShortener/Pages/UrlShortener.tsx
+++ src/modules/Dashboard/modules/UrlShortener/Pages/UrlShortener.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./UrlShortener.module.css";
 import {
     getShortenUrls,
@@ -6,35 +6,19 @@ import {
     editShortenUrl,
     deleteShortenUrl
 } from "../Services/apis";
-// import { useNavigate } from "react-router-dom";
 import TableTop from "@/MuLearnComponents/TableTop/TableTop";
 import Table from "@/MuLearnComponents/Table/Table";
 import THead from "@/MuLearnComponents/Table/THead";
 import Pagination from "@/MuLearnComponents/Pagination/Pagination";
-// import { roles } from "@/MuLearnServices/types";
-// import { hasRole } from "@/MuLearnServices/common_functions";
-// import { useFormik } from "formik";
+import { useFormik } from "formik";
 import { useToast } from "@chakra-ui/react";
 import { MuButtonLight } from "@/MuLearnComponents/MuButtons/MuButton";
 
-//TODO: Complete work rework
 const UrlShortener = () => {
-    // const columns = [];
-    // const firstFetch = useRef(true);
-
     // const [hasValidationError, setHasValidationError] = useState({
     //     error: false,
     //     message: ""
     // });
-
-    // const editableColumnNames = ["Title", "Short URL", "Long URL"];
-    // useEffect(() => {
-    //     if (firstFetch.current) {
-    //         if (!hasRole([roles.ADMIN])) navigate("/404");
-    //         getShortenUrls(setShortUrlData, 1, perPage, setTotalPages);
-    //     }
-    //     firstFetch.current = false;
-    // }, []);
 
     const columnOrder = [
         { column: "title", Label: "Title", isSortable: true },
@@ -57,10 +41,66 @@ const UrlShortener = () => {
     const [perPage, setPerPage] = useState(5);
     const [columns, setColumns] = useState(columnOrder);
     const [sort, setSort] = useState("");
-    const [id, setId] = useState("");
-    const [title, setTitle] = useState("");
-    const [longUrl, setLongUrl] = useState("");
-    const [shortUrl, setShortUrl] = useState("");
+
+    const formik = useFormik({
+        initialValues: {
+            id: "",
+            title: "",
+            longUrl: "",
+            shortUrl: ""
+        },
+        onSubmit: values => {
+            const urlCreateData = {
+                id: values.id,
+                title: values.title,
+                long_url: values.longUrl,
+                short_url: "mulearn.org/r/" + values.shortUrl
+            };
+            if (!editBtn) {
+                createShortenUrl(toast, urlCreateData);
+                setShortUrlData([...shortUrlData, urlCreateData]);
+                setTimeout(() => {
+                    getShortenUrls(setShortUrlData, 1, perPage, setTotalPages);
+                }, 500);
+                setEditBtn(false);
+            } else {
+                if (
+                    shortUrlData.some(
+                        item =>
+                            item.short_url ===
+                            "mulearn.org/r/" + values.shortUrl
+                    ) &&
+                    shortUrlData.some(item => item.title === values.title)
+                ) {
+                    toast({
+                        title: "Short URL already exists.",
+                        status: "error",
+                        duration: 3000,
+                        isClosable: true
+                    });
+                } else {
+                    editShortenUrl(values.id, toast, urlCreateData);
+                    setShortUrlData([
+                        ...shortUrlData.filter(item => item.id !== values.id),
+                        urlCreateData
+                    ]);
+                }
+            }
+        },
+        validate: (values: any) => {
+            let errors: any = {};
+            if (!values.title) {
+                errors.title = "Required";
+            }
+            if (!values.longUrl) {
+                errors.longUrl = "Required";
+            }
+            if (!values.shortUrl) {
+                errors.shortUrl = "Required";
+            }
+            return errors;
+        }
+    });
 
     const handleNextClick = () => {
         const nextPage = currentPage + 1;
@@ -113,16 +153,21 @@ const UrlShortener = () => {
                 column
             );
         }
-        //console.log(`Icon clicked for column: ${column}`);
     };
 
     const handleEdit = (id: string | number | boolean) => {
-        //console.log(formik.values.id);
-        setId(id.toString());
-        setTitle(shortUrlData.filter(item => item.id === id)[0].title);
-        setLongUrl(shortUrlData.filter(item => item.id === id)[0].long_url);
-        setShortUrl(
-            shortUrlData.filter(item => item.id === id)[0].short_url.slice(12)
+        formik.setFieldValue("id", id);
+        formik.setFieldValue(
+            "title",
+            shortUrlData.filter(item => item.id === id)[0].title
+        );
+        formik.setFieldValue(
+            "longUrl",
+            shortUrlData.filter(item => item.id === id)[0].long_url
+        );
+        formik.setFieldValue(
+            "shortUrl",
+            shortUrlData.filter(item => item.id === id)[0].short_url.slice(14)
         );
         setEditBtn(true);
     };
@@ -140,50 +185,58 @@ const UrlShortener = () => {
         <>
             <div className={styles.url_shortener_container}>
                 <div className={styles.create_new_url}>
-                    <form>
+                    <form onSubmit={formik.handleSubmit}>
                         <input
                             className={styles.title}
                             type="text"
                             name="title"
-                            onChange={e => setTitle(e.target.value)}
-                            value={title}
+                            onChange={formik.handleChange}
+                            value={formik.values.title}
+                            onBlur={formik.handleBlur}
                             placeholder="Title"
                             required
                         />
-                        {/* {formik.touched.title && formik.errors.title ? (
+                        {formik.touched.title && formik.errors.title ? (
                             <p className={styles.error_message}>
                                 {formik.errors.title}
                             </p>
-                        ) : null} */}
+                        ) : null}
                         <input
                             className={styles.long_url}
                             type="url"
-                            name="long_url"
-                            onChange={e => {
-                                setLongUrl(e.target.value);
-                                setEditBtn(false);
-                            }}
-                            value={longUrl}
+                            name="longUrl"
+                            onChange={formik.handleChange}
+                            value={formik.values.longUrl}
+                            onBlur={formik.handleBlur}
                             placeholder="Paste long url"
                             required
                         />
-                        {/* {formik.touched.long_url && formik.errors.long_url ? (
+                        {formik.touched.longUrl && formik.errors.longUrl ? (
                             <p className={styles.error_message}>
-                                {formik.errors.long_url}
+                                {formik.errors.longUrl}
                             </p>
-                        ) : null} */}
+                        ) : null}
                         <div className={styles.short_url_input_container}>
-                            <div className={styles.short_url_input}>
-                                <label htmlFor="">mulearn.org/</label>
-                                <input
-                                    className={styles.short_url}
-                                    type="text"
-                                    name="short_url"
-                                    onChange={e => setShortUrl(e.target.value)}
-                                    value={shortUrl}
-                                    placeholder="Enter short url"
-                                    required
-                                />
+                            <div className={styles.short_url_input_container_div}>
+                                <div className={styles.short_url_input}>
+                                    <label htmlFor="">mulearn.org/r/</label>
+                                    <input
+                                        className={styles.short_url}
+                                        type="text"
+                                        name="shortUrl"
+                                        onChange={formik.handleChange}
+                                        value={formik.values.shortUrl}
+                                        onBlur={formik.handleBlur}
+                                        placeholder="Enter short url"
+                                        required
+                                    />
+                                </div>
+                                {formik.touched.shortUrl &&
+                                formik.errors.shortUrl ? (
+                                    <p className={styles.error_message}>
+                                        {formik.errors.shortUrl}
+                                    </p>
+                                ) : null}
                             </div>
                             <div className={styles.form_btns}>
                                 <MuButtonLight
@@ -194,10 +247,7 @@ const UrlShortener = () => {
                                         margin: "20px 0px 0px"
                                     }}
                                     onClick={() => {
-                                        setTitle("");
-                                        setLongUrl("");
-                                        setShortUrl("");
-                                        setEditBtn(false);
+                                        formik.handleReset(formik.values);
                                     }}
                                 />
 
@@ -206,104 +256,16 @@ const UrlShortener = () => {
                                         className={styles.submit}
                                         type="submit"
                                         value="Create"
-                                        onClick={async e => {
-                                            e.preventDefault();
-                                            if (title && longUrl && shortUrl) {
-                                                const urlCreateData = {
-                                                    id: "",
-                                                    title: title,
-                                                    long_url: longUrl,
-                                                    short_url:
-                                                        "mulearn.org/" +
-                                                        shortUrl
-                                                };
-                                                await createShortenUrl(
-                                                    toast,
-                                                    urlCreateData
-                                                );
-                                                setShortUrlData([
-                                                    ...shortUrlData,
-                                                    urlCreateData
-                                                ]);
-                                                setTimeout(() => {
-                                                    getShortenUrls(
-                                                        setShortUrlData,
-                                                        1,
-                                                        perPage,
-                                                        setTotalPages
-                                                    );
-                                                }, 500);
-                                                setEditBtn(false);
-                                            } else {
-                                                console.log("error");
-                                            }
-                                        }}
                                     />
                                 ) : (
                                     <input
                                         className={styles.submit}
-                                        type="button"
+                                        type="submit"
                                         value="Edit"
-                                        onClick={e => {
-                                            e.preventDefault();
-
-                                            if (
-                                                shortUrlData.some(
-                                                    item =>
-                                                        item.short_url ===
-                                                        "mulearn.org/" +
-                                                            shortUrl
-                                                ) &&
-                                                shortUrlData.some(
-                                                    item => item.title === title
-                                                )
-                                            ) {
-                                                toast({
-                                                    title: "Short URL already exists.",
-                                                    status: "error",
-                                                    duration: 3000,
-                                                    isClosable: true
-                                                });
-                                            } else {
-                                                if (
-                                                    title &&
-                                                    longUrl &&
-                                                    shortUrl
-                                                ) {
-                                                    const urlEditData = {
-                                                        id: id,
-                                                        title: title,
-                                                        long_url: longUrl,
-                                                        short_url:
-                                                            "mulearn.org/" +
-                                                            shortUrl
-                                                    };
-                                                    editShortenUrl(
-                                                        id,
-                                                        toast,
-                                                        urlEditData
-                                                    );
-                                                    setShortUrlData([
-                                                        ...shortUrlData.filter(
-                                                            item =>
-                                                                item.id !== id
-                                                        ),
-                                                        urlEditData
-                                                    ]);
-                                                } else {
-                                                    console.log("error");
-                                                }
-                                            }
-                                        }}
                                     ></input>
                                 )}
                             </div>
                         </div>
-                        {/* {formik.touched.short_url && formik.errors.short_url ? (
-                            <p className={styles.error_message}>
-                                {formik.errors.short_url}
-                            </p>
-                        ) : null} */}
                     </form>
                 </div>
             </div>
