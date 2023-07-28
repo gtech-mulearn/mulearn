c64b6c431ba4f8247a038143931fa43deb1d882f
Rajat Sandeep <93584596+rajatsandeepsen@users.noreply.github.com>
2023-07-28 06:57:34 +0530

    fix: css nesting issues

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import postcssNesting from 'postcss-nesting';
 import path from "path";
 
 export default defineConfig({
+  css: {
+    postcss: {
+        plugins: [
+            postcssNesting
+        ],
+    },
+  },
   plugins: [react()],
   resolve: {
     alias: {
