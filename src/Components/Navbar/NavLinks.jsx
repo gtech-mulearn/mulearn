165af4901f7da0de58a9e54c48276fdc8b3772a0
Bijo Mathew Jose <bijomathewjose@gmail.com>
2023-08-03 16:19:20 +0530

    [ demote - feat ] : temp

--- src/Components/Navbar/NavLinks.jsx
+++ src/Components/Navbar/NavLinks.jsx
@@ -1,40 +1,40 @@
-import React, { useCallback, useEffect, useState } from "react";
-import { getLinks } from "./Mylinks";
+import React, { useState } from "react";
+import { links } from "./Mylinks";
 import { MobileView } from "./Mobile";
 import { MenuName, SubMenu } from "./Desktop";
 import "./Navbar.css"; /* Added CSS*/
-import SheetAPI from "../../Utils/SheetAPI";
+// import SheetAPI from "../../Utils/SheetAPI";
 
 const NavLinks = () => {
   const [currentLink, setCurrent] = useState("");
   const [visible, setVisibility] = useState(false);
   const [prev, setPrev] = useState("");
-  const [variable, setVariable] = useState([])
-  const [ig, setIg] = useState([])
+  // const [variable, setVariable] = useState([])
+  // const [ig, setIg] = useState([])
 
-  const Sheet = useCallback(() => {
-    SheetAPI('https://docs.google.com/spreadsheets/d/1C7MyDDpRCIq3bnXi-bdWQrUdYMJ0_2cBkpoJ7POQA6A/edit#gid=0', 'landing_pages', setVariable)
-  }, [])
-  useEffect(() => {
-    if (variable.length <= 0)
-      Sheet()
-    else {
-      // eslint-disable-next-line array-callback-return
-      variable.map(link => {
-        if (link.parent === 'null')
-          setIg((items) => ([...items, {
-            name: link?.heading,
-            link: `https://learn.mulearn.org/${link?.code}`,
-            foreign: true
-          }]))
-        else return {}
-      })
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [variable.length])
+  // const Sheet = useCallback(() => {
+  //   SheetAPI('https://docs.google.com/spreadsheets/d/1C7MyDDpRCIq3bnXi-bdWQrUdYMJ0_2cBkpoJ7POQA6A/edit#gid=0', 'landing_pages', setVariable)
+  // }, [])
+  // useEffect(() => {
+  //   if (variable.length <= 0)
+  //     Sheet()
+  //   else {
+  //     // eslint-disable-next-line array-callback-return
+  //     variable.map(link => {
+  //       if (link.parent === 'null')
+  //         setIg((items) => ([...items, {
+  //           name: link?.heading,
+  //           link: `https://learn.mulearn.org/${link?.code}`,
+  //           foreign: true
+  //         }]))
+  //       else return {}
+  //     })
+  //   }
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, [variable.length])
   return (
     <>
-      {getLinks(ig).map((link, index) => (
+      {links.map((link, index) => (
         <div className="p-4 text-left lg:cursor-pointer group lg:block hidden dashes" key={index} >
           <MenuName name={link.name} />
           <SubMenu submenu={link.submenu} sublinks={link.sublinks} />
--- src/Pages/MutechLeaderBoard/MutechLeaderBoard.jsx
+++ src/Pages/MutechLeaderBoard/MutechLeaderBoard.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef, useCallback } from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
 import Style from './MutechLeaderBoard.module.css'
 import Navbar from '../../Components/Navbar/Navbar'
 import OpenElkAPICall from './Utils/OpenElkAPICall';
