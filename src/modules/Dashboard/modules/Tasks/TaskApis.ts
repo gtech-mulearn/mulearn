31133e9680740d92300a48d05d8c0d0bb4cd2bd3
notS0LD13R <joelsebin@gmail.com>
2023-08-05 20:48:15 +0530

    changed fetch from sequential to promise.all for faster load

--- src/modules/Dashboard/modules/Tasks/TaskApis.ts
+++ src/modules/Dashboard/modules/Tasks/TaskApis.ts
@@ -219,9 +219,13 @@ export const getUUID = async () => {
     };
 
     const response: Partial<uuidType> = {};
+    const allResponse = await Promise.all(
+        Object.values(uuids)
+        .map(url=>privateGateway.get(url))
+    )
     for (let key in uuids) {
         response[key as keyof uuidType] = (
-            (await privateGateway.get(uuids[key])).data.response as Array<any>
+            (allResponse.shift()!).data.response as Array<any>
         ).sort((a, b) =>
             //check for name/title key and then compare
             (a.name !== undefined && a.name < b.name) ||
@@ -229,6 +233,16 @@ export const getUUID = async () => {
                 ? -1
                 : 1
         );
+        //sequential method where each fetch occurs after previous fetch
+        // response[key as keyof uuidType] = (
+        //     (await privateGateway.get(uuids[key])).data.response as Array<any>
+        // ).sort((a, b) =>
+        //     //check for name/title key and then compare
+        //     (a.name !== undefined && a.name < b.name) ||
+        //         (a.title !== undefined && a.title < b.title)
+        //         ? -1
+        //         : 1
+        // );
     }
     return response;
 };
