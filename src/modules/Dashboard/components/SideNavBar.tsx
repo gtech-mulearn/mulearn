9306f513772226c5f36d8ffdced92a6e3a9c7247
Basith P <basithpofficial@gmail.com>
2023-08-22 00:05:43 +0530

    fix: sidenav bar not hiding after a button is clicked | refator

--- src/modules/Dashboard/components/SideNavBar.tsx
+++ src/modules/Dashboard/components/SideNavBar.tsx
@@ -1,18 +1,9 @@
-import React, { useState, useEffect, useRef } from "react";
+import { useState, useEffect } from "react";
+
 import styles from "./SideNavBar.module.css";
+
 import MulearnBrand from "../assets/MulearnBrand";
-
-import { background, useToast } from "@chakra-ui/react";
-import { useNavigate } from "react-router-dom";
-
-import {
-    DropDownButtons,
-    MuButton,
-    MuButtonLight
-} from "@/MuLearnComponents/MuButtons/MuButton";
-import { fetchLocalStorage } from "@/MuLearnServices/common_functions";
-import MuLogOut from "../assets/svg/MuLogOut";
-import MuSettings from "../assets/svg/MuSettings";
+import SideNavBarBody from "./SideNavBarBody";
 
 type Props = {
     sidebarButtons: {
@@ -36,9 +27,9 @@ const SideNavBar = ({ sidebarButtons }: Props) => {
         window.innerWidth > 830 ? "flex" : "none"
     );
 
-    const myElementRef = useRef<HTMLDivElement>(null);
-    const elements = document.getElementById("right");
-    const element = elements as HTMLElement;
+    // const myElementRef = useRef<HTMLDivElement>(null);
+    // const elements = document.getElementById("right");
+    // const element = elements as HTMLElement;
 
     useEffect(() => {
         const handleResize = () =>
@@ -55,21 +46,23 @@ const SideNavBar = ({ sidebarButtons }: Props) => {
         setHamburgerIconDisplay(sideNavDisplay === "none" ? "none" : "block");
     };
 
-    const hideOrShowSideNavBar = () => {
-        animateHamburgerIcon();
-        setSideNavDisplay(sideNavDisplay === "flex" ? "none" : "flex");
+    const toggleSideNavBar = () => {
+        if (window.innerWidth <= 830) {
+            animateHamburgerIcon();
+            setSideNavDisplay(sideNavDisplay === "flex" ? "none" : "flex");
+        }
 
         // Right side content transition
-        element.style.transition = ".3s ease-in-out";
-        element.style.transform === "scale(1.1)"
-            ? (element.style.transform = "scale(1)")
-            : (element.style.transform = "scale(1.1)");
+        // element.style.transition = ".3s ease-in-out";
+        // element.style.transform === "scale(1.1)"
+        //     ? (element.style.transform = "scale(1)")
+        //     : (element.style.transform = "scale(1.1)");
     };
 
     return (
         <>
             {/* Hamburger Menu */}
-            <div className={styles.menu_btn} onClick={hideOrShowSideNavBar}>
+            <div className={styles.menu_btn} onClick={toggleSideNavBar}>
                 <p
                     style={{ transform: `rotate(${transform2})` }}
                     className={styles.lines}
@@ -99,7 +92,7 @@ const SideNavBar = ({ sidebarButtons }: Props) => {
                     </div>
                     <SideNavBarBody
                         sidebarButtons={sidebarButtons}
-                        toggleSideNavBar={hideOrShowSideNavBar}
+                        toggleSideNavBar={toggleSideNavBar}
                     />
                     <p className={styles.copyrightText}>
                         All Rights Reserved © µLearn Foundation{" "}
@@ -112,189 +105,3 @@ const SideNavBar = ({ sidebarButtons }: Props) => {
 };
 
 export default SideNavBar;
-
-type SideNavBarBodyProps = {
-    sidebarButtons: {
-        url: string;
-        title: string;
-        hasView: boolean;
-        roles?: Role[];
-        icon?: any;
-        children?: SideNavBarBodyProps["sidebarButtons"];
-    }[];
-    toggleSideNavBar: () => void;
-};
-
-const SideNavBarBody = ({
-    sidebarButtons,
-    toggleSideNavBar
-}: SideNavBarBodyProps) => {
-    const toast = useToast();
-    const navigate = useNavigate();
-
-    const [dropDownBtnDisplay, setDropDownBtnDisplay] = useState("0");
-    const [level2dropDownDisplay, setLevel2dropDownDisplay] = useState(""); // Title of the level 2 dropdown
-
-    const [connected, setConnected] = useState(false);
-
-    const userInfo = fetchLocalStorage<UserInfo>("userInfo");
-
-    useEffect(() => {
-        if (userInfo && userInfo.exist_in_guild) {
-            setConnected(userInfo.exist_in_guild);
-        }
-    }, [userInfo]);
-
-    return (
-        <div className={styles.side_nav_bar_items}>
-            <div className={styles.side_nav_bar_main_items}>
-                {sidebarButtons
-                    .filter(
-                        button =>
-                            button.hasView &&
-                            (!button.roles ||
-                                button.roles?.some(role =>
-                                    userInfo?.roles?.includes(role)
-                                ))
-                    )
-                    .map((button, i) =>
-                        button.children ? (
-                            <DropDownButtons
-                                key={i}
-                                text={button.title}
-                                icon={button.icon}
-                                onClick={() =>
-                                    setDropDownBtnDisplay(
-                                        dropDownBtnDisplay === "0"
-                                            ? "max-content"
-                                            : "0"
-                                    )
-                                }
-                                listOfDropBtn={button.children
-                                    .filter(
-                                        button =>
-                                            button.hasView &&
-                                            (!button.roles ||
-                                                button.roles?.some(role =>
-                                                    userInfo?.roles?.includes(
-                                                        role
-                                                    )
-                                                ))
-                                    )
-                                    .map((button, i) =>
-                                        button.children ? (
-                                            <DropDownButtons
-                                                key={i}
-                                                text={button.title}
-                                                icon={button.icon}
-                                                onClick={() =>
-                                                    setLevel2dropDownDisplay(
-                                                        level2dropDownDisplay ===
-                                                            button.title
-                                                            ? ""
-                                                            : button.title
-                                                    )
-                                                }
-                                                display={
-                                                    level2dropDownDisplay ===
-                                                    button.title
-                                                        ? "max-content"
-                                                        : "0"
-                                                }
-                                                listOfDropBtn={button.children
-                                                    .filter(
-                                                        button =>
-                                                            button.hasView &&
-                                                            (!button.roles ||
-                                                                button.roles?.some(
-                                                                    role =>
-                                                                        userInfo?.roles?.includes(
-                                                                            role
-                                                                        )
-                                                                ))
-                                                    )
-                                                    .map((button, i) => (
-                                                        <MuButton
-                                                            key={i}
-                                                            text={button.title}
-                                                            buttonUrl={
-                                                                button.url
-                                                            }
-                                                            onClick={() => {
-                                                                navigate(
-                                                                    button.url
-                                                                );
-                                                                window.innerWidth <=
-                                                                500
-                                                                    ? toggleSideNavBar()
-                                                                    : null;
-                                                            }}
-                                                        />
-                                                    ))}
-                                            />
-                                        ) : (
-                                            <MuButton
-                                                key={i}
-                                                text={button.title}
-                                                buttonUrl={button.url}
-                                                onClick={() => {
-                                                    navigate(button.url);
-                                                    window.innerWidth <= 500
-                                                        ? toggleSideNavBar()
-                                                        : null;
-                                                }}
-                                            />
-                                        )
-                                    )}
-                                display={dropDownBtnDisplay}
-                            />
-                        ) : (
-                            <MuButton
-                                key={i}
-                                text={button.title}
-                                icon={button.icon}
-                                buttonUrl={button.url}
-                                onClick={() => {
-                                    navigate(button.url);
-                                    window.innerWidth <= 500
-                                        ? toggleSideNavBar()
-                                        : null;
-                                }}
-                            />
-                        )
-                    )}
-            </div>
-            <div className={styles.bottomButtons}>
-                {/* <MuButton
-            text="Settings"
-            icon={<MuSettings />}
-            onClick={() => navigate("/dashboard/settings")}
-            style={{
-                color: "#9297AA",
-                backgroundColor: "#fff",
-                // marginBottom: "0px"
-            }}
-        /> */}
-                <MuButtonLight
-                    text="Logout"
-                    icon={<MuLogOut />}
-                    style={{
-                        backgroundColor: "#fff",
-                        color: "#FF7676"
-                    }}
-                    onClick={() => {
-                        localStorage.clear();
-                        toast({
-                            title: "Logged out",
-                            description: "Redirecting to login page.",
-                            status: "error",
-                            duration: 9000,
-                            isClosable: true
-                        });
-                        setTimeout(() => window.location.reload(), 900);
-                    }}
-                />
-            </div>
-        </div>
-    );
-};
