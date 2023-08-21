b267a77a7d6f9e9124729deaf073e748496b56a6
Basith P <basithpofficial@gmail.com>
2023-08-21 22:30:25 +0530

    refactor

--- src/modules/Dashboard/components/SideNavBar.tsx
+++ src/modules/Dashboard/components/SideNavBar.tsx
@@ -14,7 +14,6 @@ import { fetchLocalStorage } from "@/MuLearnServices/common_functions";
 import MuLogOut from "../assets/svg/MuLogOut";
 import MuSettings from "../assets/svg/MuSettings";
 
-
 type Props = {
     sidebarButtons: {
         url: string;
@@ -26,13 +25,16 @@ type Props = {
     }[];
 };
 
-const SideNavBar = (props: Props) => {
+const SideNavBar = ({ sidebarButtons }: Props) => {
     const toast = useToast();
     const navigate = useNavigate();
+
+    // for the hamburger icon
     const [marginTop, setMarginTop] = useState("0px");
     const [transform2, setTransform2] = useState("0deg");
     const [transform3, setTransform3] = useState("0deg");
     const [display, setDisplay] = useState("block");
+
     const [display2, setDisplay2] = useState("none");
     const [dropDownBtnDisplay, setDropDownBtnDisplay] = useState("0");
     const [level2dropDownDisplay, setLevel2dropDownDisplay] = useState(""); // Title of the level 2 dropdown
@@ -44,42 +46,32 @@ const SideNavBar = (props: Props) => {
         if (userInfo && userInfo.exist_in_guild) {
             setConnected(userInfo.exist_in_guild);
         }
-    }, []);
+    }, [userInfo]);
+
     const myElementRef = useRef<HTMLDivElement>(null);
     const elements = document.getElementById("right");
     const element = elements as HTMLElement;
-    const hideFunc = () => {
+
+    const animateHamburgerIcon = () => {
         setMarginTop(marginTop === "0px" ? "-15px" : "0px");
         setTransform2(transform2 === "0deg" ? "45deg" : "0deg");
         setTransform3(transform3 === "0deg" ? "135deg" : "0deg");
         setDisplay(display === "block" ? "none" : "block");
+    };
+
+    const hideOrShowSideNavBar = () => {
+        animateHamburgerIcon();
         // setOpacity(opacity === 1 ? 0 : 1);
-        setDisplay2(display2 === "block" ? "none" : "block");
-        element.style.transition = ".5s ease-in-out";
-        element.style.transform === "scale(1.1)"
-            ? (element.style.transform = "")
-            : (element.style.transform = "scale(1.1)");
+        // setDisplay2(display2 === "block" ? "none" : "block");
+        // element.style.transition = ".5s ease-in-out";
+        // element.style.transform === "scale(1.1)"
+        //     ? (element.style.transform = "")
+        //     : (element.style.transform = "scale(1.1)");
     };
 
     return (
         <>
-            <div
-                className={styles.menu_btn}
-                onClick={() => {
-                    setMarginTop(marginTop === "0px" ? "-15px" : "0px");
-                    setTransform2(transform2 === "0deg" ? "45deg" : "0deg");
-                    setTransform3(transform3 === "0deg" ? "135deg" : "0deg");
-                    setDisplay(display === "block" ? "none" : "block");
-                    // setOpacity(opacity === 1 ? 0 : 1);
-                    setDisplay2(display2 === "block" ? "none" : "block");
-                    // setTimeout(() => {
-                    // }, 1000);
-                    element.style.transition = ".5s ease-in-out";
-                    element.style.transform === "scale(1.1)"
-                        ? (element.style.transform = "")
-                        : (element.style.transform = "scale(1.1)");
-                }}
-            >
+            <div className={styles.menu_btn} onClick={hideOrShowSideNavBar}>
                 <p
                     style={{ transform: `rotate(${transform2})` }}
                     className={styles.lines}
@@ -115,7 +107,7 @@ const SideNavBar = (props: Props) => {
                     </div>
                     <div className={styles.side_nav_bar_items}>
                         <div className={styles.side_nav_bar_main_items}>
-                            {props.sidebarButtons
+                            {sidebarButtons
                                 .filter(
                                     button =>
                                         button.hasView &&
@@ -202,7 +194,7 @@ const SideNavBar = (props: Props) => {
                                                                                 );
                                                                                 window.innerWidth <=
                                                                                 500
-                                                                                    ? hideFunc()
+                                                                                    ? hideOrShowSideNavBar()
                                                                                     : null;
                                                                             }}
                                                                         />
@@ -222,7 +214,7 @@ const SideNavBar = (props: Props) => {
                                                                 );
                                                                 window.innerWidth <=
                                                                 500
-                                                                    ? hideFunc()
+                                                                    ? hideOrShowSideNavBar()
                                                                     : null;
                                                             }}
                                                         />
@@ -239,7 +231,7 @@ const SideNavBar = (props: Props) => {
                                             onClick={() => {
                                                 navigate(button.url);
                                                 window.innerWidth <= 500
-                                                    ? hideFunc()
+                                                    ? hideOrShowSideNavBar()
                                                     : null;
                                             }}
                                         />
@@ -259,10 +251,10 @@ const SideNavBar = (props: Props) => {
                             /> */}
                             <MuButtonLight
                                 text="Logout"
-                                icon={<MuLogOut/>}
+                                icon={<MuLogOut />}
                                 style={{
                                     backgroundColor: "#fff",
-                                    color: "#FF7676",
+                                    color: "#FF7676"
                                 }}
                                 onClick={() => {
                                     localStorage.clear();
@@ -283,7 +275,8 @@ const SideNavBar = (props: Props) => {
                         </div>
                     </div>
                     <p className={styles.copyrightText}>
-                        All Rights Reserved © µLearn Foundation {new Date().getFullYear()}
+                        All Rights Reserved © µLearn Foundation{" "}
+                        {new Date().getFullYear()}
                     </p>
                 </div>
             </div>
