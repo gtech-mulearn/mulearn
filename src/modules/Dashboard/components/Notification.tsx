df86b2b0a287449399f971c2e4e19fc103c165b6
Bijo Mathew Jose <bijomathewjose@gmail.com>
2023-08-23 22:42:49 +0530

    [feat] : notification:

---
+++ src/modules/Dashboard/components/Notification.tsx
@@ -0,0 +1,114 @@
+import { useEffect, useState } from 'react';
+import './Notification.css';
+import { clearAllNotifications, clearNotification, getNotifications, Notification as NotificationProps, requestApproval } from './api';
+import { IoIosClose } from 'react-icons/io';
+import dpm from '../assets/images/dpm.webp';
+import { filterNotification, getTimeAgo, isRequest } from './utils';
+
+
+
+const NotificationMessage = ({ profile, title, created_at, description, clear, id, url, update ,created_by}: NotificationMessageProps) => {
+    return (
+        <div className="notiMessageTab">
+            <img src={profile || dpm} alt="" />
+            <div className="notiMessageProfile">
+                <b>{title}</b>  
+                <span className="blueDot" onClick={() => {
+                    clear();
+                    clearNotification(id);
+                    update()
+                }}>
+                    <IoIosClose size={'18px'} />
+                </span>
+                <div className="day">
+                    {new Date(created_at).toLocaleDateString('en-US', { day: 'numeric', weekday: 'long', hour12: true, hour: 'numeric', minute: 'numeric' })}
+                    <b>{getTimeAgo(created_at, new Date())}</b>
+                </div>
+                <p>{description}</p>
+                {isRequest(title) &&
+                    <div className="btns">
+                        <button onClick={() => { 
+                            requestApproval(id,url,created_by, false); 
+                            update() 
+                            clear()
+                            }}>Decline</button>
+                        &nbsp;
+                        <button className="accept" onClick={() => { requestApproval(id,url,created_by, true); update() }}>Accept</button>
+                    </div>
+                }
+            </div>
+        </div>
+    );
+};
+
+const Notification = () => {
+    const [active, setActive] = useState(0);
+    const [notificationList, setNotificationList] = useState<NotificationProps[]>([]);
+
+    useEffect(() => {
+        getNotifications(setNotificationList);
+    }, []);
+
+    const links = [
+        { title: 'View All', count: notificationList.length },
+        { title: 'Requests', count: notificationList.filter((item: NotificationProps) => isRequest(item.title)).length },
+        { title: 'Followers', count: 0 },
+    ];
+
+    const filteredNotification = filterNotification(active, notificationList);
+
+    const clearElement = (cleared: number) => {
+        setNotificationList(list => list.filter((_, index) => index !== cleared));
+    };
+
+    const clearAll = () => {
+        setNotificationList([]);
+        clearAllNotifications();
+    };
+
+    return (
+        <div className="yourNotification">
+            <div className="notiTop">
+                <b>Your Notification</b>
+                <div className='clearAll' onClick={clearAll}>✖ Clear all</div>
+            </div>
+            <div className="notiTabs">
+                <ul>
+                    {links.map((item, index) => (
+                        <li key={index} className={active === index ? 'active' : 'inactive'} onClick={() => setActive(index)}>
+                            <p>{item.title}</p>
+                            <span>{item.count}</span>
+                        </li>
+                    ))}
+                    <span className='glider' style={{ transform: `translateX(${active * 100}%)`, margin: `${active === 0 ? 0 : active === links.length - 1 ? 25 : 15}px` }}></span>
+                </ul>
+            </div>
+            <div className="notiMessageContainer">
+                {filteredNotification.length > 0 ? (
+                    <div className="notiMessage">
+                        {filteredNotification.map((item, index) => (
+                            <div key={item.id}>
+                                <NotificationMessage
+                                    key={index}
+                                    {...item}
+                                    clear={() => clearElement(index)}
+                                    update={() => getNotifications(setNotificationList)}
+                                />
+                            </div>
+                        ))}
+                    </div>
+                ) : (
+                    <div className='nothing'>Nothing to show here</div>
+                )}
+            </div>
+        </div>
+    );
+};
+
+interface NotificationMessageProps extends NotificationProps {
+    clear: () => void;
+    profile?: string;
+    update: () => void;
+}
+
+export default Notification;
