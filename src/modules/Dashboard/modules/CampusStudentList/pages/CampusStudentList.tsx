9994179311d04103abded69a4b6686b6dc4bf7f3
Aswin Asok <aswinasokofficial@gmail.com>
2023-08-24 19:30:01 +0530

    fix(mulearn):transitiono-issue-fix

--- src/modules/Dashboard/modules/CampusStudentList/pages/CampusStudentList.tsx
+++ src/modules/Dashboard/modules/CampusStudentList/pages/CampusStudentList.tsx
@@ -203,7 +203,7 @@ const CampusStudentList = (props: Props) => {
                 </div>
             </div>}
             <div className={styles.graphs}>
-                <div className={styles.container}>
+                {/* <div className={styles.container}>
                     <h2>Weekly Karma Insights</h2>
                     <BarChart
                         data={barData}
@@ -212,7 +212,7 @@ const CampusStudentList = (props: Props) => {
                             colors: ['#91ABFF']
                         }}
                     />
-                </div>
+                </div> */}
                 <div className={styles.container}>
                     <h2>Student Statistics</h2>
                     <PieChart
