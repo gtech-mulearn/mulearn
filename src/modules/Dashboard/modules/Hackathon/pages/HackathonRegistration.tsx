02b1f4ccefef58161cfae5669e0ff19a15ebc0e9
Jenin82 <jenin8282@gmail.com>
2023-08-26 12:52:09 +0530

    fix(Hackathon): application form field issue fixed

--- src/modules/Dashboard/modules/Hackathon/pages/HackathonRegistration.tsx
+++ src/modules/Dashboard/modules/Hackathon/pages/HackathonRegistration.tsx
@@ -13,13 +13,17 @@ import { getApplicationForm, getHackDetails, submitHackApplication } from "../se
 import { capitalizeFirstLetter } from "../../../utils/common";
 import { HackApplicationSchema } from "../services/HackathonYup";
 import { HackList, HackathonApplication } from "../services/HackathonInterfaces";
+import Modal from "@/MuLearnComponents/Modal/Modal";
 
 type Props = {};
+
+// ... (previous imports)
 
 const HackathonRegistration = (props: Props) => {
     const toast = useToast();
     const { id } = useParams();
     const navigate = useNavigate();
+    const [modal, setModal] = useState(true); // Set modal to false initially
     const [data, setData] = useState<HackList>();
     const [application, setApplication] = useState<HackathonApplication[]>([]);
 
@@ -29,69 +33,119 @@ const HackathonRegistration = (props: Props) => {
     }, []);
 
     const handleSubmit = (values: any) => {
-        submitHackApplication(
-			values,
-			id,
-            navigate,
-            toast,
-		)
+        submitHackApplication(values, id, navigate, toast);
+    };
+
+    function getSystemFieldsNames() {
+        const systemFields = application.filter(
+            hack => hack.field_type === "system"
+        );
+        const systemFieldNames = systemFields.map(hack => hack.field_name);
+        return systemFieldNames.join(", ");
+    }
+
+    const handleModalClick = () => {
+        const hasNonSystemFields = application.some(
+            hack => hack.field_type !== "system"
+        );
+
+        if (hasNonSystemFields) {
+            setModal(false);
+        } else {
+			handleSubmit({
+                name: null,
+                gender: null,
+                email: null,
+                mobile: null,
+                bio: null,
+                college: null,
+                experience: null,
+                github: null,
+                linkedin: null
+            });
+            setModal(false);
+        }
     };
 
     return (
         <div className={styles.external_container}>
+            {modal && (
+                <Modal
+                    setIsOpen={setModal}
+                    id={""}
+                    heading={"Data access request"}
+                    content={`Are you sure you want to share your ${getSystemFieldsNames()} to apply in this hackathon ?`}
+                    click={handleModalClick}
+                />
+            )}
+                {/* Check if there's at least one non-"system" field */}
+                {application.some(hack => hack.field_type !== "system") && (
             <div className={styles.container}>
                 <h1 className={styles.text}>
                     Application Form for {data?.title}
                 </h1>
-                <Formik
-                    initialValues={{
-                        name: null,
-                        gender: null,
-                        email: null,
-                        mobile: null,
-                        bio: null,
-                        college: null,
-                        experience: null,
-                        github: null,
-                        linkedin: null
-                    }}
-                    /*
-					! TODO: Validation has issues with submitting so disabled for now.
-					*/
-                    // validationSchema={HackApplicationSchema}
-                    onSubmit={(values, { setSubmitting }) => {
-                        handleSubmit(values);
-                    }}
-                >
-                    {formik => (
-                        <Form className={styles.formContainer}>
-                            {application &&
-                                application.map((hack, index) => (
-                                    <div className={styles.inputContainer}>
-                                        <FormikTextInputWhite
-                                            label={capitalizeFirstLetter(
-                                                hack.field_name
-                                            )}
-                                            name={hack.field_name}
-                                            type="text"
-                                            placeholder="enter text here..."
-                                        />
-                                    </div>
-                                ))}
-                            <div className={styles.inputContainerBtn}>
-                                <PowerfulButton text="Decline" onButtonClick={() => {
-                                        navigate(
-                                            `/dashboard/hackathon/details/${id}`
-                                        );
-                                    }} backgroundColor="#EFF1F9" color="#456FF6" borderColor="#EFF1F28"/>
-                                
-                                <PowerfulButton text="Confirm" type={"submit"}/>
-                                
-                            </div>
-                        </Form>
-                    )}
-                </Formik>
+
+                    <Formik
+                        initialValues={{
+                            name: null,
+                            gender: null,
+                            email: null,
+                            mobile: null,
+                            bio: null,
+                            college: null,
+                            experience: null,
+                            github: null,
+                            linkedin: null
+                        }}
+                        onSubmit={(values, { setSubmitting }) => {
+                            handleSubmit(values);
+                        }}
+                    >
+                        {formik => (
+                            <Form className={styles.formContainer}>
+                                {application.map(
+                                    (hack, index) =>
+                                        hack.field_type !== "system" && (
+                                            <div
+                                                className={
+                                                    styles.inputContainer
+                                                }
+                                                key={index}
+                                            >
+                                                <FormikTextInputWhite
+                                                    label={capitalizeFirstLetter(
+                                                        hack.field_name
+                                                    )}
+                                                    name={hack.field_name}
+                                                    type="text"
+                                                    placeholder="enter text here..."
+                                                />
+                                            </div>
+                                        )
+                                )}
+                                <div className={styles.inputContainerBtn}>
+                                    <PowerfulButton
+                                        text="Decline"
+                                        onButtonClick={() => {
+                                            navigate(
+                                                `/dashboard/hackathon/details/${id}`
+                                            );
+                                        }}
+                                        backgroundColor="#EFF1F9"
+                                        color="#456FF6"
+                                        borderColor="#EFF1F28"
+                                    />
+                                    <PowerfulButton
+                                        text="Confirm"
+                                        type={"submit"} // Change to "button"
+                                        onButtonClick={() => {handleSubmit}}
+                                    />
+                                </div>
+                            </Form>
+                        )}
+                    </Formik>
             </div>
+                )}
         </div>
     );
 };
