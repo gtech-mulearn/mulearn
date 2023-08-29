6972126756b2029fecee9fe3de0fc8c01b3f615c
Aswin Asok <aswinasokofficial@gmail.com>
2023-08-29 15:35:21 +0530

    code-updations

--- src/Pages/Events/LearningFest/LearningFest.jsx
+++ src/Pages/Events/LearningFest/LearningFest.jsx
@@ -76,7 +76,7 @@ const LearningFest = () => {
             </p>
 
             <div className={styles.tracks_container}>
-              <img
+              {/* <img
                 src="/assets/events/learningfest/tracks/android.png"
                 alt=""
                 className={styles.track}
@@ -105,7 +105,7 @@ const LearningFest = () => {
                 src="/assets/events/learningfest/tracks/uiux.png"
                 alt=""
                 className={styles.track}
-              />
+              /> */}
               <img
                 src="/assets/events/learningfest/tracks/web.png"
                 alt=""
