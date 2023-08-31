150284b93ff9331980692ee1c39f07f542bfceb5
CyB3RTYp3 <rohanmanopkt@gmail.com>
2023-09-01 00:04:13 +0530

    feat(Onboarding):Rolepage Created and css added

--- src/modules/Common/Authentication/pages/Onboarding/Template.tsx
+++ src/modules/Common/Authentication/pages/Onboarding/Template.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styles from './Template.module.css'
 import OnboardingHeader from '../../components/Head/OnboardingHeader'
+import Rolepage from './rolepage'
 
 export default function Template() {
     return (
