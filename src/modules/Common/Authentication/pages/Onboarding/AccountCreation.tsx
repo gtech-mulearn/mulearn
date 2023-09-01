ceace4a27cf66a554524513f5612ed4dc453616d
CyB3RTYp3 <rohanmanopkt@gmail.com>
2023-09-01 18:38:31 +0530

    feat(Onboarding):AccountCreation and Rolepage created and fixed merge errors

--- src/modules/Common/Authentication/pages/Onboarding/AccountCreation.tsx
+++ src/modules/Common/Authentication/pages/Onboarding/AccountCreation.tsx
@@ -4,7 +4,9 @@ import styles from './accountCreation.module.css'
 export default function AccountCreation() {
   return (
     <div className={styles.accountCreationContainer}>
+
       <div className={styles.accountCreationInputs}>
+
         <input type="email" placeholder='Email id'/>
         <div className={styles.accountCreationName}>
             <input type="text" placeholder='First Name' />
@@ -12,13 +14,26 @@ export default function AccountCreation() {
         </div>
         <input type="number" placeholder='+91' />
         <div className={styles.accountCreationPassword}>
-        <input type="password" placeholder='Password' />eye
+        <input type="password" placeholder='Password' />
         </div>    
         <input type='password' placeholder='Confirm Password'/>
         <button>Create Account</button>
       </div>
+ 
+      <div className={styles.accountCreationAlternative}>
+        <div>
+          <hr/>
+          <p>OR</p>
+          <hr />
+        </div>            
+          <button>Sign in with google</button>
+            <div>
+                <p>Don't have an account?  <a href="">Sign In</a></p> 
+            </div>
+      </div>
 
     </div>
+
   )
 }
 
