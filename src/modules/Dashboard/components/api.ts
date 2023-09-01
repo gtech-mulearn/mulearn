0cc8ab05d467e74b483a5d70ccafc0d557ef8da8
Aswin Asok <aswinasokofficial@gmail.com>
2023-09-01 19:15:51 +0530

    Revert "#fix notification"

--- src/modules/Dashboard/components/api.ts
+++ src/modules/Dashboard/components/api.ts
@@ -1,7 +1,5 @@
 import { privateGateway } from "@/MuLearnServices/apiGateways";
 import { NotificationRoutes, dashboardRoutes } from "@/MuLearnServices/urls";
-import { useToast } from '@chakra-ui/react'
-const toast = useToast();
 
 export type Notification = {
     [K in "id" | "url" | "title" | "button" | "created_at" | "description"|"created_by"]: string;
@@ -13,49 +11,30 @@ export function getNotifications(setResponse: UseStateFunc<Notification[]>) {
             setResponse((response.data.response as []).reverse());
         })
         .catch((err) => {
-            toast({
-                title: "Error",
-                description: err.response.data.message,
-                status: "error",
-                duration: 3000,
-                isClosable: true
-            })
             console.error(err)
-
         })
 }
 export function clearAllNotifications() {
     privateGateway.delete(NotificationRoutes.deleteAllNotification)
-    .then(response => {})
-    .catch(err => {
-        toast({
-            title: "Error",
-            description: err.response.data.message,
-            status: "error",
-            duration: 3000,
-            isClosable: true
+        .then(response => {
+            console.log(response)
         })
-        console.error(err)})
+        .catch((err) => {
+            console.error(err)
+        })
 }
-export async function clearNotification(id: string) {
-    return await privateGateway.delete(`${NotificationRoutes.deleteNotification}${id}/`)
-        .then(response => {})
-        .catch(err =>{
-            toast({
-                title: "Error",
-                description: err.response.data.message,
-                status: "error",
-                duration: 3000,
-                isClosable: true
-            })
-            console.error(err)})
+export function clearNotification(id: string) {
+    privateGateway.delete(`${NotificationRoutes.deleteNotification}${id}/`)
+        .then(response => {
+            console.log(response)
+        })
+        .catch((err) => {
+            console.error(err)
+        })
 }
-export const requestApproval = (id:string,url: string,created_by: string, is_accepted: boolean,update: () => void) => {
+export const requestApproval = (id:string,url: string,created_by: string, is_accepted: boolean) => {
     const lcId=url.split('/')[7],userId=created_by
     const newUrl=`${dashboardRoutes.getCampusLearningCircles}${lcId}/${userId}/`
     privateGateway.patch(newUrl, { is_accepted: is_accepted ? '1' : '0' })
-    .then((res) => {
-        clearNotification(id)
-        .then(() => update())
-    })
+    clearNotification(id)
 };
