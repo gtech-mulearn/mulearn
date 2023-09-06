db317f57fca8fe5347104e1a7261e69df22296df
notS0LD13R <joelsebin@gmail.com>
2023-09-06 18:35:29 +0530

    added past events section

---
+++ src/modules/Public/KKEM/assets/past_events/index.ts
@@ -0,0 +1,14 @@
+import gtas from "./gtas.webp";
+import gtac from "./gtac.webp";
+import kf from "./kf.webp";
+import lf from "./lf.webp";
+import buh from "./buh.webp";
+const assets = {
+    gtas,
+    gtac,
+    kf,
+    lf,
+    buh
+};
+
+export default assets;
