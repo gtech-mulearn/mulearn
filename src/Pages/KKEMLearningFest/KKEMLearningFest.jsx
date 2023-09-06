f6e504907e42e1aac228deed6ee9e819fe3b240d
Jovit-Mathew236 <jovitmathew236@gmail.com>
2023-09-06 19:27:50 +0530

    [FEAT] added new faq section on kerala tech fest page

--- src/Pages/KKEMLearningFest/KKEMLearningFest.jsx
+++ src/Pages/KKEMLearningFest/KKEMLearningFest.jsx
@@ -4,12 +4,14 @@ import Hero from "./components/Hero/Hero";
 import Journey from "./components/Journey/Journey";
 import Footer from "./assets/footer.webp";
 import Cards from "./components/Cards/Cards"
+import Faq from "./components/FAQs/Faq";
 
 const LearningFest = () => {
   return <div className={styles.kkemLearningFest}>
     <Hero />
     <Journey />
     <Cards/>
+    <Faq />
     <img className={styles.kkemLearningFestFooter} src={Footer} alt="" />
   </div>;
 };
