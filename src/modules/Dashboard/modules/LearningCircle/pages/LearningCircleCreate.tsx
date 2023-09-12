d548ff8a8954a0d1d91589f3ede6e03cfd7e3c7a
Vishakh Abhayan <vishakhabhayan0@gmail.com>
2023-09-12 23:07:03 +0530

    feat(LC): Remove react lazy loader and change LC remove-button to cross icon

--- src/modules/Dashboard/modules/LearningCircle/pages/LearningCircleCreate.tsx
+++ src/modules/Dashboard/modules/LearningCircle/pages/LearningCircleCreate.tsx
@@ -2,10 +2,15 @@ import { useEffect, useState } from "react";
 import styles from "./LearningCircle.module.css";
 import * as Yup from "yup";
 import { Form, Formik } from "formik";
-import FormikReactSelect,{FormikSelect, FormikTextInput} from '@/MuLearnComponents/FormikComponents/FormikComponents'
-import { getInterestGroups,createCircle } from "../services/LearningCircleAPIs";
+import FormikReactSelect, {
+    FormikSelect,
+    FormikTextInput
+} from "@/MuLearnComponents/FormikComponents/FormikComponents";
+import {
+    getInterestGroups,
+    createCircle
+} from "../services/LearningCircleAPIs";
 import { useNavigate, useParams } from "react-router-dom";
-import LazyLoad from "react-lazy-load";
 import { PowerfulButton } from "@/MuLearnComponents/MuButtons/MuButton";
 
 type Props = {};
@@ -38,61 +43,57 @@ const LearningCircleCreate = (props: Props) => {
     return (
         <>
             <div className={styles.LearningCircleCreateContent}>
-                <LazyLoad height={762} offset={300}>
-                    <div className={styles.LearningCircleCreateMain}>
-                        <img
-                            src="https://i.ibb.co/RhT8b3z/image-24.png"
-                            alt="Loading..."
-                        />
+                <div className={styles.LearningCircleCreateMain}>
+                    <img
+                        src="https://i.ibb.co/RhT8b3z/image-24.png"
+                        alt="Loading..."
+                    />
 
-                        <div className={styles.LearningCircleCreateHeading}>
-                            <div>
-                                <b>Create a Learning circle</b>
-                                <p>Connect, Collaborate, and Learn Together!</p>
-                            </div>
-
-                            <Formik
-                                enableReinitialize={true}
-                                initialValues={{
-                                    circle_name: "",
-                                    interest_group: ""
-                                }}
-                                validationSchema={createLearningCircleSchema}
-                                onSubmit={(values, { resetForm }) => {
-                                    createCircle(
-                                        setId,
-                                        values.circle_name,
-                                        values.interest_group,
-                                        navigate
-                                    );
-                                    resetForm();
-                                }}
-                            >
-                                <Form>
-                                    <div
-                                        className={
-                                            styles.LearningCircleCreateForm
-                                        }
-                                    >
-                                        <FormikTextInput
-                                            type="text"
-                                            name="circle_name"
-                                            placeholder="Learning circle name"
-                                        />
-                                        <FormikReactSelect
-                                            isDisabled={!interestGroups}
-                                            name="interest_group"
-                                            placeholder="Interest group"
-                                            label=""
-                                            options={interestGroups!}
-                                        />
-                                    </div>
-                                      <button type="submit">Create</button>
-                                </Form>
-                            </Formik>
+                    <div className={styles.LearningCircleCreateHeading}>
+                        <div>
+                            <b>Create a Learning circle</b>
+                            <p>Connect, Collaborate, and Learn Together!</p>
                         </div>
+
+                        <Formik
+                            enableReinitialize={true}
+                            initialValues={{
+                                circle_name: "",
+                                interest_group: ""
+                            }}
+                            validationSchema={createLearningCircleSchema}
+                            onSubmit={(values, { resetForm }) => {
+                                createCircle(
+                                    setId,
+                                    values.circle_name,
+                                    values.interest_group,
+                                    navigate
+                                );
+                                resetForm();
+                            }}
+                        >
+                            <Form>
+                                <div
+                                    className={styles.LearningCircleCreateForm}
+                                >
+                                    <FormikTextInput
+                                        type="text"
+                                        name="circle_name"
+                                        placeholder="Learning circle name"
+                                    />
+                                    <FormikReactSelect
+                                        isDisabled={!interestGroups}
+                                        name="interest_group"
+                                        placeholder="Interest group"
+                                        label=""
+                                        options={interestGroups!}
+                                    />
+                                </div>
+                                <button type="submit">Create</button>
+                            </Form>
+                        </Formik>
                     </div>
-                </LazyLoad>
+                </div>
             </div>
         </>
     );
