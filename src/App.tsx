6b2fcab921395a198a853b07eeca8f3bbe51c109
Aswin Asok <aswinasokofficial@gmail.com>
2023-09-12 19:53:08 +0530

    fix(lc):temp-dynamic-imports

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,7 @@ import Profile from "./modules/Dashboard/modules/Profile/pages/Profile";
 const KarmaVoucher = lazy(
     () => import("./modules/Dashboard/modules/KarmaVoucher/KarmaVoucher")
 );
+
 const KarmaVoucherBulkImport = lazy(
     () =>
         import(
@@ -91,18 +92,7 @@ const KKEMLanding = lazy(
     () => import("./modules/Public/KKEM/modules/KKEMLanding")
 );
 const KKEMAuth = lazy(() => import("./modules/Public/KKEM/modules/KKEMAuth"));
-const LearningCircle = lazy(
-    () =>
-        import(
-            "./modules/Dashboard/modules/LearningCircle/pages/LearningCircle"
-        )
-);
-const LearningCircleCreate = lazy(
-    () =>
-        import(
-            "./modules/Dashboard/modules/LearningCircle/pages/LearningCircleCreate"
-        )
-);
+
 const ManageLocation = lazy(
     () => import("./modules/Dashboard/modules/ManageLocation/ManageLocation")
 );
@@ -117,17 +107,7 @@ const HackathonOrganizers = lazy(() =>
         "./modules/Dashboard/modules/Hackathon/pages/HackathonOrganizers"
     ).then(module => ({ default: module.HackathonOrganizers }))
 );
-const FindCircle = lazy(
-    () =>
-        import(
-            "./modules/Dashboard/modules/LearningCircle/pages/LearningCircleFind"
-        )
-);
-const LearningCircleLandingPage = lazy(() =>
-    import(
-        "./modules/Dashboard/modules/LearningCircle/pages/LearningCircleLandingPage"
-    ).then(module => ({ default: module.LearningCircleLandingPage }))
-);
+
 const Organizations = lazy(
     () => import("./modules/Dashboard/modules/Organizations/Organizations")
 );
@@ -183,6 +163,10 @@ import Rolepage from "./modules/Common/Authentication/pages/Onboarding/RolePage/
 import CollegePage from "./modules/Common/Authentication/pages/Onboarding/CollegePage/CollegePage";
 import CompanyPage from "./modules/Common/Authentication/pages/Onboarding/CompanyPage/CompanyPage";
 import SignIn from "./modules/Common/Authentication/pages/Onboarding/SignIn/SignIn";
+import { LearningCircleLandingPage } from "./modules/Dashboard/modules/LearningCircle/pages/LearningCircleLandingPage";
+import LearningCircle from "./modules/Dashboard/modules/LearningCircle/pages/LearningCircle";
+import LearningCircleCreate from "./modules/Dashboard/modules/LearningCircle/pages/LearningCircleCreate";
+import FindCircle from "./modules/Dashboard/modules/LearningCircle/pages/LearningCircleFind";
 
 const ConnectedDevices = lazy(
     () => import("./modules/Dashboard/modules/Settings/pages/ConnectedDevices")
