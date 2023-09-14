8e35d5ad96a48c166585324346178b974f772dfb
Alan0602 <alankanil1234@gmail.com>
2023-09-14 22:52:36 +0530

    feat(ErrorLog):set as download button

--- src/modules/Dashboard/modules/ErrorLog/ErrorLog.tsx
+++ src/modules/Dashboard/modules/ErrorLog/ErrorLog.tsx
@@ -1,14 +1,21 @@
 import { BsCloudDownload } from "react-icons/bs"
 import styles from "./ErrorLog.module.css"
+import { getCSV } from "./ErrorLogApi";
 
 const ErrorLog = () => {
+
+    const handleSubmit = async () => {
+       getCSV()
+    }
+
   return (
       <>
-          <button className={styles.errorLogButton}>
+          <button className={styles.errorLogButton} onClick={handleSubmit}>
               <div className={styles.errorLogBox}>
                 
                   <BsCloudDownload />
               </div>
+              Download ErrorLog
           </button>
       </>
   );
