dc722a728407416075a6a1f3634bc1e0ee670690
Aswin Asok <aswinasokofficial@gmail.com>
2023-09-17 14:10:01 +0530

    content-updations

--- src/Pages/CapTF/Level1.jsx
+++ src/Pages/CapTF/Level1.jsx
@@ -7,7 +7,7 @@ const Level1 = () => {
                 <body>
                     <img src="/assets/captf/img1.png" alt="" srcset="" style={{ maxWidth: "100%", margin: "auto" }} />
                     <br/>
-                    <p style={{ fontSize: "24px", fontWeight: "bold", marginTop: "20px" }}>pxohduq.ruj/vslghup + Muhammad Kutty Panaparambil Ismai's Son</p>
+                    <p style={{ fontSize: "24px", fontWeight: "bold", marginTop: "20px" }}>ghy.pxohduq.ruj/vslghup + Muhammad Kutty Panaparambil Ismai's Son</p>
                     <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank" rel="noopener noreferrer">
                         <button style={{ backgroundColor: "#e62429", color: "#fff", border: "none", borderRadius: "5px", padding: "10px 20px", fontSize: "18px", fontWeight: "bold", marginTop: "20px", cursor: "pointer" }}>Click Here</button>
                     </a>
