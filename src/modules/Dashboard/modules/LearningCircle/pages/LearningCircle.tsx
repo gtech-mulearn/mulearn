4d9acf48424d291e20c74f86c77b2aeeaac80001
Sujishna v P <sujishnaprijil@gmail.com>
2023-09-17 09:53:33 +0530

    minor changes in the learning circle members list

--- src/modules/Dashboard/modules/LearningCircle/pages/LearningCircle.tsx
+++ src/modules/Dashboard/modules/LearningCircle/pages/LearningCircle.tsx
@@ -636,7 +636,9 @@ const LearningCircle = (props: Props) => {
                                                         alt="Profile Picture"
                                                     />
                                                     <div>
+                                                        <div className={styles.username}>
                                                         <p>{member.username}</p>
+                                                        </div>
                                                         <span>
                                                             <img
                                                                 src="https://i.ibb.co/Dbhv9rS/karma.png"
