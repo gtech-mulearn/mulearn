4def9cf51f2f6cdac8f3e3f797e733afe218b0c5
Aswin Asok <aswinasokofficial@gmail.com>
2023-09-27 19:22:08 +0530

    fix(bug)

--- src/Pages/CapTF/Level1.jsx
+++ src/Pages/CapTF/Level1.jsx
@@ -1,20 +1,99 @@
 import React from "react";
+import notapdf from "./notapdf.fpd";
 
 const Level1 = () => {
-    return (
-        <div style={{ backgroundColor: "#1c1c1c", color: "#fff", textAlign: "center", fontFamily: "Arial, sans-serif", minHeight: "100vh", display: "flex", alignItems: "center", justifyContent: "center" }}>
-            <html>
-                <body>
-                    <img src="/assets/captf/img1.png" alt="" srcset="" style={{ maxWidth: "100%", margin: "auto" }} />
-                    <br/>
-                    <p style={{ fontSize: "24px", fontWeight: "bold", marginTop: "20px" }}>ghy.pxohduq.ruj/vslghup + Muhammad Kutty Panaparambil Ismai's Son</p>
-                    <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank" rel="noopener noreferrer">
-                        <button style={{ backgroundColor: "#e62429", color: "#fff", border: "none", borderRadius: "5px", padding: "10px 20px", fontSize: "18px", fontWeight: "bold", marginTop: "20px", cursor: "pointer" }}>Click Here</button>
-                    </a>
-                </body>
-            </html>
-        </div>
-    );
+  return (
+    <div
+      style={{
+        backgroundColor: "#1c1c1c",
+        color: "#fff",
+        textAlign: "center",
+        fontFamily: "Arial, sans-serif",
+        minHeight: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <html>
+        <body>
+          <img
+            src="/assets/captf/img1.png"
+            alt=""
+            srcset=""
+            style={{ maxWidth: "100%", margin: "auto" }}
+          />
+          <br />
+          <p
+            style={{ fontSize: "24px", fontWeight: "bold", marginTop: "20px" }}
+          >
+            pxohduq.ruj/{" "}
+            <a href={notapdf} target="_blank" rel="noopener noreferrer">
+              <span
+                style={{
+                  backgroundColor: "#e62429",
+                  color: "#fff",
+                  border: "none",
+                  borderRadius: "5px",
+                  padding: "10px 20px",
+                  fontSize: "18px",
+                  fontWeight: "bold",
+                  marginTop: "20px",
+                  cursor: "pointer",
+                }}
+              >
+                Find Me
+              </span>{" "}
+            </a>
+            + Muhammad Kutty Panaparambil Ismai's Son
+          </p>
+          <a
+            href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <button
+              style={{
+                backgroundColor: "#e62429",
+                color: "#fff",
+                border: "none",
+                borderRadius: "5px",
+                padding: "10px 20px",
+                fontSize: "18px",
+                fontWeight: "bold",
+                marginTop: "20px",
+                cursor: "pointer",
+              }}
+            >
+              For Finding Me(Real One)
+            </button>
+          </a>
+          <a
+            href="/assets/captf/RawMenCCLVI.pdf"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <button
+              style={{
+                backgroundColor: "#e62429",
+                color: "#fff",
+                border: "none",
+                borderRadius: "5px",
+                padding: "10px 20px",
+                marginLeft: "20px",
+                fontSize: "18px",
+                fontWeight: "bold",
+                marginTop: "20px",
+                cursor: "pointer",
+              }}
+            >
+              For Finding Me
+            </button>
+          </a>
+        </body>
+      </html>
+    </div>
+  );
 };
 
 export default Level1;
