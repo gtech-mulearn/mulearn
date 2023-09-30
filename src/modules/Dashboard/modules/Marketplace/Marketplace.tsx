c7e24b25933f7c6d4ab5f91b6c7ae4e501a05992
Amal C P <acp070146@gmail.com>
2023-09-30 21:26:24 +0530

    second section history start

--- src/modules/Dashboard/modules/Marketplace/Marketplace.tsx
+++ src/modules/Dashboard/modules/Marketplace/Marketplace.tsx
@@ -1,114 +1,72 @@
 import styles from "./Marketplace.module.css";
 import coin from "./assets/muCoin.svg";
 import i from "./assets/i.svg";
-import bgCard from "./assets/bgCard.svg";
-import phone from "./assets/phone.svg";
-import { History, RedNoti, LinkLogo, LockLogo } from "./assets/svg";
+import bg from "./assets/bgCard.svg";
+import { HintLogo } from "./assets/svg";
+
 type Props = {};
 
 export const Marketplace = (props: Props) => {
+    const hello = (p: any) => {
+        console.log("hello");
+    };
     const data = [
         {
-            coins: 20,
-            noti: 2,
-            bg: bgCard,
+            img: bg,
             title: "ChatGPT Pro",
-            descp: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.",
-            link: "https://dribbble.com/"
-        },
-        {
+            desp: "Lorem Ipsum bla bla description I guess",
             coins: 20,
-            noti: 2,
-            bg: bgCard,
-            title: "ChatGPT Pro",
-            descp: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.",
-            link: "https://dribbble.com/"
+            status: "Ordered",
+            address:
+                "102, Dream Nest Opp.Tikka Hut, Mallamar Chokkabettu, Surathkal Mangaluru-575014",
+            hints: "https://www.google.com"
         },
         {
+            img: bg,
+            title: "ChatGPT Pro",
+            desp: "Lorem Ipsum bla bla description I guess",
             coins: 20,
-            noti: 2,
-            bg: bgCard,
-            title: "ChatGPT Pro",
-            descp: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.",
-            link: "https://dribbble.com/"
+            status: "Ordered",
+            address:
+                "102, Dream Nest Opp.Tikka Hut, Mallamar Chokkabettu, Surathkal Mangaluru-575014",
+            hints: "https://www.google.com"
         },
         {
+            img: bg,
+            title: "ChatGPT Pro",
+            desp: "Lorem Ipsum bla bla description I guess",
             coins: 20,
-            noti: 2,
-            bg: bgCard,
-            title: "ChatGPT Pro",
-            descp: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.",
-            link: "https://dribbble.com/"
+            status: "Ordered",
+            address:
+                "102, Dream Nest Opp.Tikka Hut, Mallamar Chokkabettu, Surathkal Mangaluru-575014",
+            hints: "https://www.google.com"
         },
         {
+            img: bg,
+            title: "ChatGPT Pro",
+            desp: "Lorem Ipsum bla bla description I guess",
             coins: 20,
-            noti: 2,
-            bg: bgCard,
-            title: "ChatGPT Pro",
-            descp: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.",
-            link: "https://dribbble.com/"
+            status: "Ordered",
+            address:
+                "102, Dream Nest Opp.Tikka Hut, Mallamar Chokkabettu, Surathkal Mangaluru-575014",
+            hints: "https://www.google.com"
         },
         {
+            img: bg,
+            title: "ChatGPT Pro",
+            desp: "Lorem Ipsum bla bla description I guess",
             coins: 20,
-            noti: 2,
-            bg: bgCard,
-            title: "ChatGPT Pro",
-            descp: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.",
-            link: "https://dribbble.com/"
-        }
-    ];
-    const rewards = [
-        {
-            img: phone,
-            detail: "iphone",
-            coins: 20
-        },
-        {
-            img: phone,
-            detail: "iphone",
-            coins: 20
-        },
-        {
-            img: phone,
-            detail: "iphone",
-            coins: 20
-        },
-        {
-            img: phone,
-            detail: "iphone",
-            coins: 20
-        },
-        {
-            img: phone,
-            detail: "iphone",
-            coins: 20
-        },
-        {
-            img: phone,
-            detail: "iphone",
-            coins: 20
-        },
-        {
-            img: phone,
-            detail: "iphone",
-            coins: 20
-        },
-        {
-            img: phone,
-            detail: "iphone",
-            coins: 20
-        },
-        {
-            img: phone,
-            detail: "iphone",
-            coins: 20
+            status: "Ordered",
+            address:
+                "102, Dream Nest Opp.Tikka Hut, Mallamar Chokkabettu, Surathkal Mangaluru-575014",
+            hints: "https://www.google.com"
         }
     ];
     return (
         <div className={styles.wrapper}>
             <div className={styles.topSectionWrapper}>
                 <div className={styles.cont1}>
-                    <h1>Welcome to Marketplace</h1>
+                    <h1>Your past orders</h1>
                     <div>
                         <p>20</p>
                         <img src={coin} alt="" />
@@ -116,69 +74,61 @@ export const Marketplace = (props: Props) => {
                 </div>
                 <div className={styles.cont2}>
                     <div>
-                        <h2>Redeem rewards using your Mucoins</h2>
-                        <img src={i} alt="" />
+                        <h2>View your order details and history.</h2>
+                        <button>
+                            {" "}
+                            <img src={i} alt="" />
+                        </button>
                     </div>{" "}
-                    <a href="">
-                        <History />
-                    </a>
                 </div>
                 <div className={styles.cont3}>
-                    {data.map(({ coins, noti, bg, title, descp, link }, i) => (
-                        <div className={styles.cardIndividualWrapper}>
-                            <div className={styles.TopSet}>
-                                <img src={bgCard} alt="" />
-                                <div>
-                                    <div>
-                                        <p>{coins}</p>
-                                        <img src={coin} alt="" />
+                    {data.map(
+                        (
+                            { img, title, desp, coins, status, address, hints },
+                            i
+                        ) => (
+                            <div className={styles.innerContainer}>
+                                <img src={img} alt="" />
+                                <div className={styles.containerContent}>
+                                    <div className={styles.containerContent1}>
+                                        <div
+                                            style={{
+                                                display: "flex",
+                                                flexDirection: "column",
+                                                alignItems: "flex-start"
+                                            }}
+                                        >
+                                            <h1>ChatGPT Pro</h1>
+                                            <p>
+                                                Lorem Ipsum bla bla description
+                                                I guess
+                                            </p>
+                                        </div>
+
+                                        <div className={styles.coinss}>
+                                            <p>20</p>
+                                            <img src={coin} alt="" />
+                                        </div>
                                     </div>
                                     <div>
-                                        <RedNoti />
-                                        <p style={{ color: "red" }}>
-                                            {noti} remaining !!
-                                        </p>
+                                        <p>Status:</p>
+                                        <h1>{status}</h1>
+                                    </div>
+                                    <div
+                                        style={{
+                                            display: "flex"
+                                        }}
+                                    >
+                                        <p>to</p>
+                                        <p>{address}</p>
                                     </div>
                                 </div>
+                                <button>
+                                    <HintLogo />
+                                </button>
                             </div>
-                            <div className={styles.bottomSet}>
-                                <div>
-                                    <h1>{title}</h1>
-                                    <p>{descp}</p>
-                                </div>
-                                <a href={link}>
-                                    <p>Claim</p>
-                                    <LinkLogo />
-                                </a>
-                            </div>
-                        </div>
-                    ))}
-                </div>
-            </div>
-            <div className={styles.bottomSectionWrapper}>
-                <div className={styles.hiddenTreaasue}>
-                    {rewards.map(({ coins, img, detail }, i) => (
-                        <div className={styles.IndividualTreasure}>
-                            <img src={img} alt="" />
-                            <div>
-                                <p>{detail}</p>
-                                <div>
-                                    <p>{coins}</p>
-                                    <img src={coin} alt="" />
-                                </div>
-                            </div>
-                        </div>
-                    ))}
-                </div>
-                <div className={styles.Overlapper}>
-                    <div>
-                        <LockLogo />
-                        <p>
-                            Oops! You're short on Mucoins.Help friends hit Level
-                            4, earn Mucoins!
-                        </p>
-                        <a href="https://dribbble.com/">Invite</a>
-                    </div>
+                        )
+                    )}
                 </div>
             </div>
         </div>
