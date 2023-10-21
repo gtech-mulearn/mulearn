ce8f7c193d2319bb15591a0cc5120490188e56ea
Aswin Asok <aswinasokofficial@gmail.com>
2023-10-21 11:36:45 +0530

    fix(styles):pathway-enablers

--- src/Pages/EnablersPage/components/Footer/Footer.jsx
+++ src/Pages/EnablersPage/components/Footer/Footer.jsx
@@ -2,10 +2,14 @@ import React from 'react'
 import styles from './Footer.module.css'
 
 export default function Footer() {
+  const handleEmailClick = () => {
+    window.location.href = "mailto:info@mulearn.in";
+  };
+
   return (
     <div className={styles.footer}>
        <div>
-        <p>Contact Us for your queries: enablers@mulearn.in</p>
+        <p onClick={handleEmailClick}>Contact Us for your queries: info@mulearn.in</p>
        </div>
        <p>Copyright © 2023. All Rights Reserved. µLearn Foundation</p>
     </div>
