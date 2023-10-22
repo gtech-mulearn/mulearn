9020b4bc77d10b8741b93c5d7d8f41b452fc13d5
Jovit-Mathew236 <jovitmathew236@gmail.com>
2023-10-23 00:34:37 +0530

    [FEAT]minor and major changes in profiel v2

--- src/modules/Dashboard/modules/ProfileV2/components/BasicDetails/pages/BasicDetails.tsx
+++ src/modules/Dashboard/modules/ProfileV2/components/BasicDetails/pages/BasicDetails.tsx
@@ -4,7 +4,11 @@ import HeatmapComponent from "../../Heatmap/HeatmapComponent";
 import { useToast } from "@chakra-ui/react";
 import { editIgDetails, getAllIg, getIgDetails } from "../services/api";
 import { useParams } from "react-router-dom";
-import { PlanetGreen, PlanetOrange, PlanetRed } from "../../../assets/svg/Planets";
+import {
+    PlanetGreen,
+    PlanetOrange,
+    PlanetRed
+} from "../../../assets/svg/Planets";
 type Props = {
     userProfile: any;
     userLog: any;
@@ -20,35 +24,60 @@ const BasicDetails = (props: Props) => {
         // getIgDetails(toast, setIg);
         getAllIg(setAllIg);
     }, []);
+    useEffect(()=>{
+        setIg(props.userProfile.interest_groups)
+    })
     const ig_sorted = ig.sort((a: any, b: any) => {
         return a.name > b.name ? 1 : -1;
     });
+    
     return (
         <>
             <div className={styles.card_container}>
-                <div className={styles.card}>
-                    UX/UI Design
-                    <p>85%</p>
-                    <div className={styles.planet}>
-                        <PlanetGreen />
-                    </div>
-                </div>
-                <div className={styles.card}>
-                    UX/UI Design
-                    <p>85%</p>
-                    <div className={styles.planet}>
-                        <PlanetOrange />
-                    </div>
-                </div>
-                <div className={styles.card}>
-                    UX/UI Design
-                    <p>85%</p>
-                    <div className={styles.planet}>
-                        <PlanetRed />
-                    </div>
-                </div>
+                {props.userProfile.interest_groups.length != 0 ? (
+                    ig.map((data: any, i: number) => {
+                        return (
+                            <div key={i} className={styles.card}>
+                                {data.name}{" "}
+                                <p>
+                                    {" "}
+                                    {data.karma !== null
+                                        ? data.karma > 1000
+                                            ? (data.karma / 1000).toPrecision(
+                                                  2
+                                              ) + "K"
+                                            : data.karma
+                                            ? data.karma
+                                            : "0"
+                                        : "0"}
+                                </p>
+                                <div className={styles.planet}>
+                                    <PlanetGreen />
+                                </div>
+                            </div>
+                        );
+                    })
+                ) : (
+                    <p>
+                        No Interest Groups to Selected, You need you reach Level
+                        4 to Select
+                    </p>
+                )}
             </div>
-
+            {/*  <div className={styles.card}>
+                            UX/UI Design
+                            <p>85%</p>
+                            <div className={styles.planet}>
+                                <PlanetOrange />
+                            </div>
+                        </div>
+                        <div className={styles.card}>
+                            UX/UI Design
+                            <p>85%</p>
+                            <div className={styles.planet}>
+                                <PlanetRed />
+                            </div>
+                        </div> */}
             <div className={styles.heatmap}>
                 <HeatmapComponent
                     data={props.userLog}
