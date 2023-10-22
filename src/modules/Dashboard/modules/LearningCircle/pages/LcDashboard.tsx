83d2f12df02831c6be0d3ab692aa179744e2a33a
Amal C P <acp070146@gmail.com>
2023-10-22 19:27:04 +0530

    structure for nxt set LC is done

--- src/modules/Dashboard/modules/LearningCircle/pages/LcDashboard.tsx
+++ src/modules/Dashboard/modules/LearningCircle/pages/LcDashboard.tsx
@@ -1,10 +1,13 @@
 import { style } from "d3";
 import { EditLogo, RightArrow, ThreeDotssvg } from "../assets/svg";
 import styles from "./LcDashboard.module.css";
+import { Dispatch, SetStateAction, useState } from "react";
 
 type Props = {};
 
 const LcDashboard = (props: Props) => {
+    const [showReport, setShowReport] = useState("");
+
     return (
         <div className={styles.LCDashboardWrapper}>
             <div className={styles.TitleContainerWrapper}>
@@ -37,44 +40,128 @@ const LcDashboard = (props: Props) => {
                     <button className={styles.items}>Team</button>
                     <button className={styles.plusItem}>+</button>
                 </div>
-                <div className={styles.ContentWrapper}>
-                    <div className={styles.TopContainer}>
-                        <div className={styles.sectionOne}>
-                            <div className={styles.divOne}>
+                {showReport === "1" ? (
+                    <div className={styles.ReportWrapper}>
+                        <div>
+                            <div>
                                 <div>
-                                    <p>Next meeting on</p>
+                                    <p>Date</p>
+                                    <input type="text" />
+                                </div>
+                                <div>
+                                    <p>Time</p>
+                                    <input type="text" />
+                                </div>
+                            </div>
+                            <div>
+                                <p>Agenda</p>
+                                <input type="text" />
+                            </div>
+                            <div>
+                                <p>Attendees</p>
+                                <div>
+                                    <div>
+                                        <Attendees />
+                                        <Attendees />
+                                        <Attendees />
+                                        <Attendees />
+                                        <Attendees />
+                                    </div>
+                                    <button>+</button>
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <div>
+                                <p>Upload Meeting Images</p>
+                                <input type="text" />
+                            </div>
+                            <button>Submit</button>
+                        </div>
+                    </div>
+                ) : showReport === "2" ? (
+                    <div>
+                        <div>
+                            <div>
+                                <div>
                                     <h1>22 June 2023</h1>
                                     <p>Sunday</p>
                                 </div>
-                                <button>
-                                    <EditLogo />
-                                </button>
-                            </div>
-                            <div className={styles.secondDiv}>
                                 <div>
                                     <p>Venue: Multipurpose Hall</p>
                                     <p>Time: 12:45 PM</p>
                                 </div>
-                                <button>Report</button>
                             </div>
-                        </div>
-                        <div className={styles.sectionTwo}>
                             <div>
-                                <CheckBoxContainer />
+                                <h1>Agenda</h1>
+                                <p>
+                                    Lorem Ipsum is simply dummy text of the
+                                    printing and typesetting industry. Lorem
+                                    Ipsum has been the industry's standard dummy
+                                    text ever since the 1500 Lorem Ipsum is
+                                    simply dummy text of the printing and
+                                    typesetting industry. Lorem Ipsum has been
+                                    the industry's standard dummy text ever
+                                    since the 1500
+                                </p>
                             </div>
-                            <button>+</button>
                         </div>
-                    </div>
-                    <div className={styles.BottomContainer}>
-                        <p>Your past meetings</p>
                         <div>
-                            <HistoryDiv />
-                            <HistoryDiv />
-                            <HistoryDiv />
-                            <HistoryDiv />
+                            <div>
+                                <h1>Attendees</h1>
+                                <div>
+                                    <Attendees />
+                                </div>
+                            </div>
+                            <div>
+                                <img src="" alt="" />
+                                <img src="" alt="" />
+                            </div>
                         </div>
                     </div>
-                </div>
+                ) : (
+                    <div className={styles.ContentWrapper}>
+                        <div className={styles.TopContainer}>
+                            <div className={styles.sectionOne}>
+                                <div className={styles.divOne}>
+                                    <div>
+                                        <p>Next meeting on</p>
+                                        <h1>22 June 2023</h1>
+                                        <p>Sunday</p>
+                                    </div>
+                                    <button>
+                                        <EditLogo />
+                                    </button>
+                                </div>
+                                <div className={styles.secondDiv}>
+                                    <div>
+                                        <p>Venue: Multipurpose Hall</p>
+                                        <p>Time: 12:45 PM</p>
+                                    </div>
+                                    <button
+                                        onClick={() => {
+                                            setShowReport("1");
+                                        }}
+                                    >
+                                        Report
+                                    </button>
+                                </div>
+                            </div>
+                            <div className={styles.sectionTwo}>
+                                <div>
+                                    <CheckBoxContainer />
+                                </div>
+                                <button>+</button>
+                            </div>
+                        </div>
+                        <div className={styles.BottomContainer}>
+                            <p>Your past meetings</p>
+                            <div>
+                                <HistoryDiv setdata={setShowReport} />
+                            </div>
+                        </div>
+                    </div>
+                )}
             </div>
         </div>
     );
@@ -82,7 +169,11 @@ const LcDashboard = (props: Props) => {
 
 export default LcDashboard;
 
-export const HistoryDiv = () => {
+export const HistoryDiv = ({
+    setdata
+}: {
+    setdata: Dispatch<SetStateAction<string>>;
+}) => {
     return (
         <div className={styles.HistoryDivWrapper}>
             <div>
@@ -99,7 +190,11 @@ export const HistoryDiv = () => {
                 >
                     12.03pm
                 </p>
-                <button>
+                <button
+                    onClick={() => {
+                        setdata("2");
+                    }}
+                >
                     <RightArrow />
                 </button>
             </div>
@@ -116,6 +211,15 @@ export const CheckBoxContainer = () => {
                 value="Text that gets selected"
             />
             <label htmlFor="textInput">1. Study IA</label>
+        </div>
+    );
+};
+
+export const Attendees = () => {
+    return (
+        <div className={styles.AttendeesWrapperIndividual}>
+            <img src="" alt="" />
+            <p>Enric S Neelamkavil</p>
         </div>
     );
 };
