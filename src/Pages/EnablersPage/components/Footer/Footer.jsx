7e7fee46befedcecacd349a4075e3ab9aaee213d
Aswin Asok <aswinasokofficial@gmail.com>
2023-10-23 18:38:31 +0530

    fix(enablers):email-updated

--- src/Pages/EnablersPage/components/Footer/Footer.jsx
+++ src/Pages/EnablersPage/components/Footer/Footer.jsx
@@ -3,13 +3,13 @@ import styles from './Footer.module.css'
 
 export default function Footer() {
   const handleEmailClick = () => {
-    window.location.href = "mailto:info@mulearn.in";
+    window.location.href = "mailto:enabler@mulearn.org";
   };
 
   return (
     <div className={styles.footer}>
        <div>
-        <p onClick={handleEmailClick}>Contact Us for your queries: info@mulearn.in</p>
+        <p onClick={handleEmailClick}>Contact Us for your queries: enabler@mulearn.org</p>
        </div>
        <p>Copyright © 2023. All Rights Reserved. µLearn Foundation</p>
     </div>
