819e2fda77dd1e727ff34c4949e7a76c969b3736
notS0LD13R <joelsebin@gmail.com>
2023-10-31 22:43:51 +0530

    feat:added notification auto clear

--- src/modules/Dashboard/components/Notification.tsx
+++ src/modules/Dashboard/components/Notification.tsx
@@ -1,67 +1,129 @@
-import { useState } from 'react';
-import './Notification.css';
-import { clearAllNotifications, clearNotification, getNotifications, Notification as NotificationProps, requestApproval } from './api';
-import { IoIosClose } from 'react-icons/io';
-import { MdRefresh } from 'react-icons/md';
-import dpm from '../assets/images/dpm.webp';
-import { filterNotification, getTimeAgo, isRequest } from './utils';
-import { useToast } from '@chakra-ui/react'
+import { useState } from "react";
+import "./Notification.css";
+import {
+    clearAllNotifications,
+    clearNotification,
+    getNotifications,
+    Notification as NotificationProps,
+    requestApproval
+} from "./api";
+import { IoIosClose } from "react-icons/io";
+import { MdRefresh } from "react-icons/md";
+import dpm from "../assets/images/dpm.webp";
+import { filterNotification, getTimeAgo, isRequest } from "./utils";
+import { useToast } from "@chakra-ui/react";
 
 interface NotificationComponentProps {
     notificationList: NotificationProps[];
-    setNotificationList: React.Dispatch<React.SetStateAction<NotificationProps[]>>;
+    setNotificationList: React.Dispatch<
+        React.SetStateAction<NotificationProps[]>
+    >;
 }
 
-
-const NotificationMessage = ({ profile, title, created_at, description, clearElementFromView, id, url, updateList, created_by }: NotificationMessageProps) => {
-    const props = { toast: useToast(), updateList: updateList, clearElementFromView: clearElementFromView }
+const NotificationMessage = ({
+    profile,
+    title,
+    created_at,
+    description,
+    clearElementFromView,
+    id,
+    url,
+    updateList,
+    created_by
+}: NotificationMessageProps) => {
+    const props = {
+        toast: useToast(),
+        updateList: updateList,
+        clearElementFromView: clearElementFromView
+    };
     return (
         <div className="notiMessageTab">
             <img src={profile || dpm} alt="" />
             <div className="notiMessageProfile">
                 <b>{title}</b>
-                <span className="blueDot" onClick={() => clearNotification(id, props)}>
-                    <IoIosClose size={'18px'} />
+                <span
+                    className="blueDot"
+                    onClick={() => clearNotification(id, props)}
+                >
+                    <IoIosClose size={"18px"} />
                 </span>
                 <div className="day">
-                    {new Date(created_at).toLocaleDateString('en-US', { day: 'numeric', weekday: 'long', hour12: true, hour: 'numeric', minute: 'numeric' })}
+                    {new Date(created_at).toLocaleDateString("en-US", {
+                        day: "numeric",
+                        weekday: "long",
+                        hour12: true,
+                        hour: "numeric",
+                        minute: "numeric"
+                    })}
                     <b>{getTimeAgo(created_at, new Date())}</b>
                 </div>
                 <p>{description}</p>
-                {isRequest(title) &&
+                {isRequest(title) && (
                     <div className="btns">
-                        <button onClick={() => {
-                            requestApproval(id, url, created_by, false, props);
-                        }}>Decline</button>
+                        <button
+                            onClick={() => {
+                                requestApproval(
+                                    id,
+                                    url,
+                                    created_by,
+                                    false,
+                                    props
+                                );
+                                clearNotification(id, props);
+                            }}
+                        >
+                            Decline
+                        </button>
                         &nbsp;
-                        <button className="accept" onClick={() => {
-                            requestApproval(id, url, created_by, true, props)
-                        }}>Accept</button>
+                        <button
+                            className="accept"
+                            onClick={() => {
+                                requestApproval(
+                                    id,
+                                    url,
+                                    created_by,
+                                    true,
+                                    props
+                                );
+                                clearNotification(id, props);
+                            }}
+                        >
+                            Accept
+                        </button>
                     </div>
-                }
+                )}
             </div>
         </div>
     );
 };
 
-const NotificationTab = ({ notificationList, setNotificationList }: NotificationComponentProps) => {
+const NotificationTab = ({
+    notificationList,
+    setNotificationList
+}: NotificationComponentProps) => {
     const [active, setActive] = useState(0);
     const toast = useToast();
     const props = { toast: toast };
     const links = [
-        { title: 'View All', count: notificationList.length },
+        { title: "View All", count: notificationList.length }
         // { title: 'Requests', count: notificationList.filter((item: NotificationProps) => isRequest(item.title)).length },
         // { title: 'Followers', count: 0 },
     ];
 
     const filteredNotification = filterNotification(active, notificationList);
 
-    const clearElementFromView = (id: string) => setNotificationList(notificationList => notificationList.filter(item => item.id !== id))
+    const clearElementFromView = (id: string) =>
+        setNotificationList(notificationList =>
+            notificationList.filter(item => item.id !== id)
+        );
 
     const clearAll = () => {
         if (notificationList.length > 0) {
-            setNotificationList([])
-            clearAllNotifications({ toast: toast, setNotificationList: setNotificationList })
+            setNotificationList([]);
+            clearAllNotifications({
+                toast: toast,
+                setNotificationList: setNotificationList
+            });
         } else {
             toast({
                 title: "Error",
@@ -69,17 +131,23 @@ const NotificationTab = ({ notificationList, setNotificationList }: Notification
                 status: "error",
                 duration: 3000,
                 isClosable: true
-            })
+            });
         }
-    }
+    };
 
     return (
         <div className="yourNotification">
-
             <div className="notiTop">
                 <b>Your Notification</b>
-                <div className='clearAll' onClick={clearAll}>✖ Clear all</div>
-                <div className='clearAll' onClick={() => getNotifications(setNotificationList, props)}><MdRefresh size={20} /></div>
+                <div className="clearAll" onClick={clearAll}>
+                    ✖ Clear all
+                </div>
+                <div
+                    className="clearAll"
+                    onClick={() => getNotifications(setNotificationList, props)}
+                >
+                    <MdRefresh size={20} />
+                </div>
             </div>
             <div className="notiMessageContainer">
                 {filteredNotification.length > 0 ? (
@@ -89,14 +157,24 @@ const NotificationTab = ({ notificationList, setNotificationList }: Notification
                                 <NotificationMessage
                                     key={index}
                                     {...item}
-                                    clearElementFromView={() => clearElementFromView(item?.id)}
-                                    updateList={() => getNotifications(setNotificationList, props)}
+                                    clearElementFromView={() =>
+                                        clearElementFromView(item?.id)
+                                    }
+                                    updateList={() =>
+                                        getNotifications(
+                                            setNotificationList,
+                                            props
+                                        )
+                                    }
                                 />
                             </div>
                         ))}
                     </div>
                 ) : (
-                    <div className='nothing'>You're on track! <br/>No pending notifications at the moment.</div>
+                    <div className="nothing">
+                        You're on track! <br />
+                        No pending notifications at the moment.
+                    </div>
                 )}
             </div>
         </div>
--- src/modules/Dashboard/components/api.ts
+++ src/modules/Dashboard/components/api.ts
@@ -2,88 +2,114 @@ import { privateGateway } from "@/MuLearnServices/apiGateways";
 import { NotificationRoutes, dashboardRoutes } from "@/MuLearnServices/urls";
 
 export type Notification = {
-    [K in "id" | "url" | "title" | "button" | "created_at" | "description"|"created_by"]: string;
+    [K in
+        | "id"
+        | "url"
+        | "title"
+        | "button"
+        | "created_at"
+        | "description"
+        | "created_by"]: string;
 };
 
-export function getNotifications(setResponse: UseStateFunc<Notification[]>,props?: any) {
-    privateGateway.get(NotificationRoutes.getNotification)
+export function getNotifications(
+    setResponse: UseStateFunc<Notification[]>,
+    props?: any
+) {
+    privateGateway
+        .get(NotificationRoutes.getNotification)
         .then(response => {
             setResponse((response.data.response as []).reverse());
         })
-        .catch((err) => {
+        .catch(err => {
             props.toast({
                 title: "Error",
                 description: err.response.data.message,
                 status: "error",
                 duration: 3000,
                 isClosable: true
-            })
-            console.error(err)
-        })
+            });
+            console.error(err);
+        });
 }
 
-export function  clearAllNotifications(props:any) {
-    const {toast,setNotificationList}=props
-    privateGateway.delete(NotificationRoutes.deleteAllNotification)
-    .then(() => {
-        props.toast({
-            title: "Success",
-            description: "All notifications cleared",
-            status: "success",
-            duration: 3000,
-            isClosable: true
+export function clearAllNotifications(props: any) {
+    const { toast, setNotificationList } = props;
+    privateGateway
+        .delete(NotificationRoutes.deleteAllNotification)
+        .then(() => {
+            props.toast({
+                title: "Success",
+                description: "All notifications cleared",
+                status: "success",
+                duration: 3000,
+                isClosable: true
+            });
         })
-    })
-    .catch(err => {
-        getNotifications(setNotificationList,{toast})
-        toast({
-            title: "Error",
-            description: err.response.data.response,
-            status: "error",
-            duration: 3000,
-            isClosable: true
-        })})
-}
-export async function clearNotification(id: string,props: any) {
-    const {clearElementFromView,toast,updateList}=props
-    clearElementFromView()
-    return await privateGateway.delete(`${NotificationRoutes.deleteNotification}${id}/`)
-        .catch(err =>{
+        .catch(err => {
+            getNotifications(setNotificationList, { toast });
             toast({
                 title: "Error",
-                description: err.response.data.message,
+                description: err.response.data.response,
                 status: "error",
                 duration: 3000,
                 isClosable: true
-            })
-            console.error(err)
-        })
-        .finally(() => updateList())
+            });
+        });
 }
-export const requestApproval = (id:string,url: string,created_by: string, is_accepted: boolean,props: any) => {
-    const {toast,updateList,clearElementFromView}=props
+export async function clearNotification(id: string, props: any) {
+    const { clearElementFromView, toast, updateList } = props;
+    clearElementFromView();
+    return await privateGateway
+        .delete(`${NotificationRoutes.deleteNotification}${id}/`)
+        .catch(err => {
+            toast({
+                title: "Error",
+                description: err.response.data.message.general,
+                status: "error",
+                duration: 3000,
+                isClosable: true
+            });
+            console.error(err);
+        })
+        .finally(() => updateList());
+}
+export const requestApproval = (
+    id: string,
+    url: string,
+    created_by: string,
+    is_accepted: boolean,
+    props: any
+) => {
+    const { toast, updateList, clearElementFromView } = props;
 
-    const lcId=url.split('/')[7],userId=created_by
-    const newUrl=`${dashboardRoutes.getCampusLearningCircles}${lcId}/${userId}/`
-    privateGateway.patch(newUrl, { is_accepted: is_accepted ? '1' : '0' })
-    .then(() => {
-        toast({
-            title: "Success",
-            description: "Request Approved",
-            status: "success",
-            duration: 3000,
-            isClosable: true
+    const lcId = url.split("/")[7],
+        userId = created_by;
+    const newUrl = `${dashboardRoutes.getCampusLearningCircles}${lcId}/${userId}/`;
+    privateGateway
+        .patch(newUrl, { is_accepted: is_accepted ? "1" : "0" })
+        .then(() => {
+            toast({
+                title: "Success",
+                description: "Request Approved",
+                status: "success",
+                duration: 3000,
+                isClosable: true
+            });
+            clearNotification(id, {
+                toast: toast,
+                updateList: updateList,
+                clearElementFromView: clearElementFromView
+            });
         })
-        clearNotification(id,{toast:toast,updateList:updateList,clearElementFromView:clearElementFromView})
-    })
-    .catch(err => {
-        updateList()
-        toast({
-            title: "Error",
-            description: err.response.data.message,
-            status: "error",
-            duration: 3000,
-            isClosable: true
-        })
-    })
+        .catch(err => {
+            updateList();
+            toast({
+                title: "Error",
+                description: err.response.data.message.general,
+                status: "error",
+                duration: 3000,
+                isClosable: true
+            });
+        });
 };
