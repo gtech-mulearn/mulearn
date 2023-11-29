fa96874376e8e884f1de81b44857c59e237709cc
BlazeSN860 <virajprabhup@gmail.com>
2023-11-29 20:47:25 +0530

    fix(TaskApis): Added event to Api.
    1. Added even to Api payload.
    2. Removed '/' which caused payload issue.

--- src/modules/Dashboard/modules/Tasks/TaskApis.ts
+++ src/modules/Dashboard/modules/Tasks/TaskApis.ts
@@ -125,7 +125,8 @@ export const editTask = async (
                 level: level_id === "" ? null : level_id,
                 ig: ig_id === "" ? null : ig_id,
                 org: org_id === "" ? null : org_id,
-                discord_link: discord_link === "" ? null : discord_link
+                discord_link: discord_link === "" ? null : discord_link,
+                event: event
             }
         );
         toast({
@@ -182,7 +183,8 @@ export const createTask = async (
                 level: level_id === "" ? null : level_id,
                 ig: ig_id === "" ? null : ig_id,
                 org: org_id === "" ? null : org_id,
-                discord_link: discord_link
+                discord_link: discord_link,
+                event: event
             }
         );
         toast({
