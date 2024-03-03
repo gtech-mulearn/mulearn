7378e8bd16b2484e82ec129a1913ef845798601d
bijomathewjose <bijomathewjose@gmail.com>
2024-03-03 10:50:17 +0530

    Removed unnecessary hooks

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import styles from "../Home/Home.module.css";
 
 import Footer from "../../Components/Footer/Footer";
