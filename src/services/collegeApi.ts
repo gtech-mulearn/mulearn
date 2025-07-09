91c5fe523d586ccae39e3f9feca3cba300f7b2cf
S SIDHARTH <sidharthpunalur@gmail.com>
2025-07-09 21:20:39 +0530

    college change added

--- src/services/collegeApi.ts
+++ src/services/collegeApi.ts
@@ -1,11 +1,11 @@
 import { privateGateway } from "./apiGateways";
 import toast from "react-hot-toast";
 
-export const updateCollege = async (college_code: string) => {
+export const updateCollege = async (org_id: string) => {
     try {
         const response = await privateGateway.patch(
             "/api/v1/dashboard/college/change-college",
-            { college_code }
+            { org_id }
         );
         if (response.status === 200 && !response.data.hasError) {
             toast.success("College updated successfully!");
