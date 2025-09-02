9c49527f485b670ddc9b00aab18df1a011a8635b
Amith Abey Stephen <amithabeystephen@gmail.com>
2025-09-02 13:14:05 +0530

    /team q2 interns data aded

--- src/modules/Public/Team/components/Teams/Year2025.tsx
+++ src/modules/Public/Team/components/Teams/Year2025.tsx
@@ -2,12 +2,14 @@
 import TeamSection from "../TeamSection";
 
 import associates from "../../data/2025/associates.json";
-import PillersQ1 from "../../data/2025/pillersQ1.json";
+import PillersQ1 from "../../data/2025/pillersQ1.json"; 
+import PillersQ2 from "../../data/2025/pillarsQ2.json"; 
 export const Year2025 = () => {
   return (
     <>
       <TeamSection title="µLearn Associates" description="The Associates are the ones who are responsible for the smooth functioning of different teams and the community as a whole." members={associates} />
-      <TeamSection title="µLearn Pillers Q1 2025" description="The Interns are the ones who are responsible for the smooth functioning of different teams and the community as a whole." members={PillersQ1} />
+      <TeamSection title="µLearn Pillars Q1 2025" description="The Interns are the ones who are responsible for the smooth functioning of different teams and the community as a whole." members={PillersQ1} />
+      <TeamSection title="µLearn Pillars Q2 2025" description="The Interns are the ones who are responsible for the smooth functioning of different teams and the community as a whole." members={PillersQ2} />
     </> 
   );
 }
