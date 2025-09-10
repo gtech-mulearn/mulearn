3c79b5f967539b71da14a4c05081f1793e9bb530
Amith Abey Stephen <amithabeystephen@gmail.com>
2025-09-10 12:51:22 +0530

    add assocaites and interns 2024,2025

--- src/modules/Public/Team/components/Teams/Year2025.tsx
+++ src/modules/Public/Team/components/Teams/Year2025.tsx
@@ -8,8 +8,8 @@ export const Year2025 = () => {
   return (
     <>
       <TeamSection title="µLearn Associates" description="The Associates are the ones who are responsible for the smooth functioning of different teams and the community as a whole." members={associates} />
-      <TeamSection title="µLearn Pillars Q1 2025" description="The Interns are the ones who are responsible for the smooth functioning of different teams and the community as a whole." members={PillersQ1} />
       <TeamSection title="µLearn Pillars Q2 2025" description="The Interns are the ones who are responsible for the smooth functioning of different teams and the community as a whole." members={PillersQ2} />
+      <TeamSection title="µLearn Pillars Q1 2025" description="The Interns are the ones who are responsible for the smooth functioning of different teams and the community as a whole." members={PillersQ1} />
     </> 
   );
 }
